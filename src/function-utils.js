036fd84e39c639fb06323b2d554396fdc1560d20
Josh <joshua.froelich@gmail.com>
2014-07-25 17:19:51 -0400

    New function-utils
    Moved noop function utility out of general utilities

---
+++ src/function-utils.js
@@ -0,0 +1,10 @@
+// Copyright 2014 Josh Froelich. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file
+
+'use strict';
+
+// TODO: move this into function-utils.js
+// No operation singleton
+function noop() {
+}
--- src/utilities.js
+++ src/utilities.js
@@ -4,17 +4,16 @@
 
 'use strict';
 
-// No operation singleton
-function noop() {
-}
+var lucu = lucu || {};
+lucu.objectUtils = {};
 
-// Retrieves the value of the key property from obj
-function valueAt(obj, key) {
 
-  // I cannot find a way to undo the syntactic sugar :(
+// Retrieves the value of the key property from obj. I cannot find a more elegant
+// manner to undo the syntactic sugar
+lucu.objectUtils.valueAt = function(object, key) {
+  return object[key];
+};
 
-  return obj[key];
-}
 
 // Gets the values of the properties of an array-like object
 function objectValues(obj) {
@@ -23,9 +22,14 @@ function objectValues(obj) {
   // It would be nice if Object.keys had a 'restrictToOwnProps' parameter
 
   var keys = Object.keys(obj);
+
+  // NOTE: this is binding obj to 'this', not as the first argument. I am
+  // not certain that is correct
   var boundHasOwn = Object.prototype.hasOwnProperty.bind(obj);
   var ownKeys = keys.filter(boundHasOwn);
-  var boundValueAt = valueAt.bind(null,obj);
+
+  var boundValueAt = this.valueAt.bind(null,obj);
   var values = ownKeys.map(boundValueAt);
+
   return values;
 }
