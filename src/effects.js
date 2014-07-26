d70a73d93f54f9203c50e0290676129274490e9d
Josh <joshua.froelich@gmail.com>
2014-07-25 23:17:01 -0400

    Created effects.js
    moved fade into effects, updated deps

---
+++ src/effects.js
@@ -0,0 +1,37 @@
+// Copyright 2014 Josh Froelich. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file
+
+'use strict';
+
+/**
+ * Fade an element in/out
+ * Elements must have opacity defined as 0 or 1 for this to work
+ *
+ * TODO: this needs to be entirely refactored. it could be
+ * greatly simplified, it could make fewer assumptions about the element's
+ * state
+ */
+function fadeElement(element, duration, delay, callback) {
+
+  if(element.style.display == 'none') {
+    element.style.display = '';
+    element.style.opacity = '0';
+  }
+
+  if(!element.style.opacity)
+    element.style.opacity = element.style.display == 'none' ? '0' : '1';
+
+  if(callback)
+    element.addEventListener('webkitTransitionEnd', ended);
+
+  // property duration function delay
+  element.style.transition = 'opacity '+duration+'s ease '+delay+'s';
+  element.style.opacity = element.style.opacity == '1' ? '0' : '1';
+
+  function ended(event) {
+    this.removeEventListener('webkitTransitionEnd', ended);
+    if(callback)
+      callback(element);
+  }
+}
