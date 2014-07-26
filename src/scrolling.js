74320247383df197baaf2a3702ed07c45d541a52
Josh <joshua.froelich@gmail.com>
2014-07-25 23:13:41 -0400

    Created scrolling.js
    Moved smooth scroll out of dom utils, also rearranged html sources to
    include js files in alphabetical order

---
+++ src/scrolling.js
@@ -0,0 +1,51 @@
+// Copyright 2014 Josh Froelich. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file
+
+'use strict';
+
+/**
+ * Periodically scroll from the current position to a new position
+ *
+ * NOTE: the start timer is basically to debounce calls to this function
+ * whereas the interval timer is to track the interval and stop it when
+ * finished
+ *
+ * @param element {Element} the element to scroll
+ * @param delta {int} the amount of pixels by which to scroll per increment
+ * @param targetY {int} the desired vertical end position
+ */
+function smoothScrollToY(element, delta, targetY) {
+  var scrollYStartTimer;
+  var scrollYIntervalTimer;
+  var amountToScroll = 0;
+  var amountScrolled = 0;
+
+  return function() {
+    clearTimeout(scrollYStartTimer);
+    clearInterval(scrollYIntervalTimer);
+    scrollYStartTimer = setTimeout(start,5);
+  }();
+
+  function start() {
+    amountToScroll = Math.abs(targetY - element.scrollTop);
+    amountScrolled = 0;
+
+    if(amountToScroll == 0) {
+      return;
+    }
+
+    scrollYIntervalTimer = setInterval(scrollToY,20);
+  }
+
+  function scrollToY() {
+    var currentY = element.scrollTop;
+    element.scrollTop += delta;
+    amountScrolled += Math.abs(delta);
+
+    // If there was no change or we scrolled too far, then we are done.
+    if(currentY == element.scrollTop || amountScrolled >= amountToScroll) {
+      clearInterval(scrollYIntervalTimer);
+    }
+  }
+}
