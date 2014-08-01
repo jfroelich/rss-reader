95f92f56c49cf7bde4574ea2babcdd350610097e
Josh <joshua.froelich@gmail.com>
2014-08-01 19:11:38 -0400

    Comment cleanup

--- src/calamine/filter-comments.js
+++ src/calamine/filter-comments.js
@@ -8,19 +8,6 @@ var lucu = lucu || {};
 lucu.calamine = lucu.calamine || {};
 
 lucu.calamine.filterComments = function(doc) {
-
   var remover = lucu.node.remove;
-
-  // TEMP: testing alternate syntax for removal
-  // var remover = Node.prototype.remove;
-
-  // Ok, I think the above does not work because there is no
-  // such thing as Node.prototype.remove.  There is
-  // Element.prototype.remove, however, so other places can
-  // maybe use that
-
-
-
-
   lucu.node.forEach(doc.body, NodeFilter.SHOW_COMMENT, remover);
 };
