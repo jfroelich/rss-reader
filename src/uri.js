c5fc44f759eea869ec906deae4e268dd21a64c38
Josh <joshua.froelich@gmail.com>
2014-08-03 17:44:08 -0400

    Cleanup
    Removed the uri2 stuff, it was not working. Minor cleanup in other
    functions.

--- src/uri.js
+++ src/uri.js
@@ -108,102 +108,10 @@ lucu.uri.isValidString = function(string) {
 // Accepts a url string, returns a url string without the scheme
 lucu.uri.filterScheme = function(urlString) {
   var uriObject = lucu.uri.parse(urlString);
-  if(uriObject) {
-    delete uriObject.scheme;
-    return lucu.uri.uriToString(uriObject);
-  }
-};
-
-/////////////////////////////////////////////////////////////////
-
-// Under development. The goal of version 2 is to provide more accurate
-// URI handling. Performance is irrelevant. The goal is 100% accurate handling
-// by delegation to the browser's URL parsing (via setting HTMLAnchorElement.href)
-// Reference: https://github.com/inexorabletash/polyfill/blob/master/url.js
-// The trick for URL resolution is simply to create a document using
-// document.implementation, and to add a base tag to the head, then add the element,
-// set its href, then return its href.
-
-/*
-Relevant properties of an instance of an HTMLAnchorElement, and notes
-
-* set href  setting this parses the string and sets the other props.
-* host (e.g. www.example.com)
-* hostname (e.g. www.example.com)
-* get href (returns the full url, relative to base (resolved, canonical and normalized))
-* hash, includes the leading #
-* origin (e.g. http://www.example.com) does not include trailing slash
-* username (e.g. the username in http://username:www.example.com)
-* password
-* pathname - the path (includes leading slash)
-* protocol - the scheme, includes the trailing :
-* search - the query part, includes the leading ?
-
-NOTE: caveat is we must create the element with the base in order to resolve.
-
-
-*/
-
-
-lucu.uri2 = {};
-
-/*
-So there is a major problem here. This delegates to the base url
-of the document that included the uri.js script if the url is
-relative. That will fudge everything everytime this is used.
-
-There are 3 basic use cases:
-* Parsing for resolution. This would work fine in that case because we
-would expect and require the base parameter
-* Checking validity. Basically just checking if it looks like a url. This
-would not work fine, because the browser will always do things like
-substitute in the protocol of the caller (e.g. "chrome-extension:").
-* Filter scheme. Again it would not work well unless we modified the function
-to require the base parameter. Even then the browser still substitutes default
-values like the protocol, and we would have no accurate way of knowing.
-
-*/
-lucu.uri2.parse = function(string) {
-  if(!string) {
+  if(!uriObject) {
     return;
   }
 
-  var anchor = document.createElement('a');
-  anchor.href = string;
-
-  var result = {};
-
-  var protocol = anchor.protocol;
-  if(protocol) {
-    // Strip the trailing ':'
-    result.scheme = protocol.slice(0, -1);
-  }
-
-  var host = anchor.host;
-  if(host) {
-    result.host = host;
-  }
-
-  var path = anchor.pathname;
-  if(path) {
-    result.path = path;
-  }
-
-  var query = anchor.search;
-
-  // Ignore a single '?'
-  if(query && query.length > 1) {
-
-    // Strip the leading '?'
-    result.query = query.slice(1);
-  }
-
-  var fragment = anchor.hash;
-
-  // Ignore a single '#'
-  if(fragment && fragment.length > 1) {
-    result.fragment = fragment.slice(1);
-  }
-
-  return result;
+  delete uriObject.scheme;
+  return lucu.uri.uriToString(uriObject);
 };
