c850045fa94d59f61d598b2619bb38a165ce427b
Josh <joshua.froelich@gmail.com>
2014-09-18 12:16:26 -0400

    Simplification
    Deprecated separate functions and inlined most of the functions,
    refactored some of the scoring, improved some performance, and generally
    transitioned the module closer to its primary purpose

--- src/calamine.js
+++ src/calamine.js
@@ -16,10 +16,6 @@ if(!Map || !Set) {
   console.warn('Map/Set not supported, things will go wrong');
 }
 
-var forEach = Array.prototype.forEach;
-var reduce = Array.prototype.reduce;
-
-
 var INTRINSIC_BIAS = new Map([
   ['article', 2000],
   ['main', 100],
@@ -101,10 +97,7 @@ var ATTRIBUTE_BIAS = new Map([
   ['articleheadings', -50],
   ['attachment', 20],
   ['author', 20],
-
-  // Usually not the best element
   ['block', -5],
-
   ['blog', 20],
   ['body', 50],
   ['bodytd', 50],
@@ -215,172 +208,14 @@ var ATTRIBUTE_BIAS = new Map([
   ['zone', -50]
 ]);
 
-
-/**
- * Updates the element's score based on its index within
- * its parent. The closer to the start (the smaller the index),
- * the higher the score. The closer the middle (the mid index),
- * the higher the score.
- */
-function applyPositionScore(featuresMap, features, element) {
-  var siblingCount = element.parentElement.childElementCount - 1;
-  if(!siblingCount)
-    return;
-  var previous = element.previousElementSibling;
-  if(previous)
-    features.previousSiblingCount = featuresMap.get(previous).previousSiblingCount + 1;
-  var startRatio = features.previousSiblingCount / siblingCount;
-  features.score += 5 - 5 * startRatio;
-  var halfCount = siblingCount / 2;
-  var middleOffset = Math.abs(features.previousSiblingCount - halfCount);
-  features.score += 5 - 5 * middleOffset / halfCount;
-}
-
-function applySelectorBias(doc, features, selector, bias) {
-  var elements = doc.body.querySelectorAll(selector);
-  for(var i = 0, f, element, len = elements.length; i < len; i++) {
-    element = elements[i];
-    f = features.get(element);
-    f.score += bias;
-    features.set(element, f);
-  }
-}
-
-/**
- * Initializes the features map for all elements
- */
-function createFeatures(doc, elements) {
-  var features = new Map();
-
-  for(var i = 0, len = elements.length; i < len; i++) {
-    features.set(elements[i], {
-      score: 0, charCount: 0, anchorCharCount: 0, previousSiblingCount: 0
-    });
-  }
-
-  features.set(doc.documentElement,
-    {score: -Infinity, charCount: 0, anchorCharCount: 0});
-  features.set(doc.body,
-    {score: -Infinity, charCount: 0, anchorCharCount: 0});
-  return features;
-}
-
-/**
- * Extract anchor features. Based on text features
- */
-function deriveAnchorFeatures(featuresMap, anchor) {
-  var features = featuresMap.get(anchor);
-  var cc = features.charCount;
-  if(!cc) {
-    return;
-  }
-  features.anchorCharCount = cc;
-  featuresMap.set(anchor, features);
-  var parent = anchor, parentFeatures;
-  while(parent = parent.parentElement) {
-    parentFeatures = featuresMap.get(parent);
-    parentFeatures.anchorCharCount += cc;
-    featuresMap.set(parent, parentFeatures);
-  }
-}
-
-function deriveTextFeatures(doc, featuresMap) {
-  var it = doc.createNodeIterator(doc.body, NodeFilter.SHOW_TEXT);
-  var element, node, features, parent, pFeatures;
-  while(node = it.nextNode()) {
-    element = node.parentElement;
-    features = featuresMap.get(element);
-    features.charCount += node.nodeValue.length;
-    featuresMap.set(element, features);
-    if(features.charCount) {
-      parent = element;
-      while(parent = parent.parentElement) {
-        pFeatures = featuresMap.get(parent);
-        pFeatures.charCount += features.charCount;
-        featuresMap.set(parent, pFeatures);
-      }
-    }
-  }
-}
-
-var EXPOSE_PROPS = [
-  {key: 'SHOW_CHAR_COUNT', value: 'charCount'},
-  {key: 'SHOW_SCORE', value: 'score'}
-];
-
-// Exposing attributes for debugging
-function exposeAttributes(doc, featuresMap, options) {
-
-  var elements = doc.documentElement.getElementsByTagName('*');
-  for(var i = 0, value, e, features, len = elements.length; i < EXPOSE_PROPS.length;i++) {
-    if(options[EXPOSE_PROPS[i].key]) {
-      value = EXPOSE_PROPS[i].value;
-      for(var j = 0; j < len;j++) {
-        e = elements[j];
-        features = featuresMap.get(e) || {};
-        if(features[value]) {
-          e.setAttribute(value, features[value]);
-        }
-      }
-    }
-  }
-}
-
-/**
- * Compares the scores of two elements and returns the element with the higher
- * score. If equal the previous element is returned.
- * TODO: this needs a better name. what is it doing?
- */
-function getMaxScore(featuresMap, previous, current) {
-  return featuresMap.get(current).score > featuresMap.get(previous).score ?
-    current : previous;
-}
-
-var RE_TOKEN_SPLIT = /[\s-_]+/g;
-
-function remove(n) {
-  n.remove();
-}
-
-function identity(value) {
-  return value;
-}
-
-function tokenize(str) {
-  return new Set(str.split(RE_TOKEN_SPLIT).filter(identity));
-}
-
-function updateScoreWithAttributeBias(value) {
-  this.score += ATTRIBUTE_BIAS.get(value) || 0;
-}
-
-function applyAttributeScore(features, element) {
-
-  // Maybe I should use element.classList to tokenize
-  // instead? But that does not split by -_
-
-  tokenize([element.getAttribute('class') || '',
-    element.getAttribute('id') || '',
-    element.getAttribute('itemprop') || '',
-    element.getAttribute('name') || '',
-    element.getAttribute('role') || ''
-  ].join(' ').toLowerCase()).forEach(
-  updateScoreWithAttributeBias, features);
-}
-
 /**
  * Apply our 'model' to an element. We generate a score that is the
  * sum of several terms.
+ * TODO: make score a function of cc and density instead
+ * of searching classes
  */
 function scoreElement(featuresMap, element) {
   var features = featuresMap.get(element);
-  var localName = element.localName;
-
-  // Score elements based on the element itself
-  features.score += INTRINSIC_BIAS.get(localName) || 0;
-
-  // TODO: make score a function of cc and density instead
-  // of searching classes
   var cc = features.charCount;
   if(cc) {
     var density = (features.anchorCharCount || 0) / cc;
@@ -421,60 +256,7 @@ function scoreElement(featuresMap, element) {
     }
   }
 
-  // Interesting idea but maybe not important
-  // applyPositionScore(featuresMap, features, element);
-
-  applyAttributeScore(features, element);
-
-  // Interesting idea but maybe not important
-  //if(element.previousElementSibling) {
-    // Contiguity bias
-  //  var prevScore = featuresMap.get(element.previousElementSibling).score;
-  //  features.score += 20 * (prevScore > 0 ? 1 : -1);
-  //}
-
   featuresMap.set(element, features);
-
-  // TODO: move this into transformDocument iteration over
-  // elements NodeList
-  var descendantBias = DESCENDANT_BIAS.get(localName);
-  if(descendantBias) {
-    var pFeatures = featuresMap.get(element.parentElement);
-    pFeatures.score += descendantBias;
-    featuresMap.set(element.parentElement, pFeatures);
-  }
-}
-
-/**
- * TODO: is there some nicer way of updating the parent?
- */
-function scoreImage(featuresMap, image) {
-  var features = featuresMap.get(image);
-  var imageParent = image.parentElement;
-  var parentFeatures = featuresMap.get(imageParent);
-
-  // Penalize carousels. Each child image independently penalizes its container
-  parentFeatures.score += reduce.call(imageParent.childNodes,
-    function calculateImageSiblingPenalty(bias, node) {
-    return 'img' === node.localName && node !== image ? bias - 5 : bias;
-  }, 0);
-
-  // Reard auxillary descriptions
-  var alt = image.getAttribute('alt');
-  var title = image.getAttribute('title');
-  var caption = imageParent.localName == 'figure' &&
-    imageParent.querySelector('figcaption');
-  var supportingTextBias = alt || title || caption ? 30 : 0;
-  features.score += supportingTextBias;
-  parentFeatures.score +=  supportingTextBias;
-
-  // Reward large images
-  var area = image.width ? image.width * image.height : 0;
-  var areaBias = 0.0015 * Math.min(100000, area);
-  features.score += areaBias;
-  parentFeatures.score += areaBias;
-  featuresMap.set(image, features);
-  featuresMap.set(imageParent, parentFeatures);
 }
 
 /**
@@ -484,37 +266,177 @@ function scoreImage(featuresMap, image) {
 function transformDocument(doc, options) {
   options = options || {};
 
-  // Cannot blacklist 'aside' because Washington Times uses it as a wrapper
-  forEach.call(doc.body.querySelectorAll('nav, header, footer'), remove);
+  var forEach = Array.prototype.forEach;
 
-  // Feature extraction in preparation for later analysis
+  // Pre-filter
+  var removables = doc.body.querySelectorAll('nav, header, footer')
+  forEach.call(removables, function (n) {
+    n.remove();
+  });
+
+  // Initialize features map
+  var features = new Map();
+  features.set(doc.documentElement, {
+    score: -Infinity, charCount: 0, anchorCharCount: 0
+  });
+  features.set(doc.body, {
+    score: -Infinity, charCount: 0, anchorCharCount: 0
+  });
   var elements = doc.body.getElementsByTagName('*');
-  var features = createFeatures(doc, elements);
-  deriveTextFeatures(doc, features);
-  forEach.call(doc.body.querySelectorAll('a[href]'),
-    deriveAnchorFeatures.bind(this, features));
+  forEach.call(elements, function initFeatures(element) {
+    features.set(element, {
+      score: 0, charCount: 0, anchorCharCount: 0, previousSiblingCount: 0
+    });
+  });
 
-  applySelectorBias(doc, features, 'li *,ol *,ul *', -20);
-  applySelectorBias(doc, features, 'aside *, header *,footer *,nav *', -50);
+  // Derive text features from the bottom up
+  var it = doc.createNodeIterator(doc.body, NodeFilter.SHOW_TEXT);
+  var element, node, elementFeatures, parent, parentFeatures;
+  while(node = it.nextNode()) {
+    element = node.parentElement;
+    elementFeatures = features.get(element);
+    elementFeatures.charCount += node.nodeValue.length;
+    features.set(element, elementFeatures);
+    if(!elementFeatures.charCount) {
+      continue;
+    }
+    parent = element.parentElement;
+    while(parent) {
+      parentFeatures = features.get(parent);
+      parentFeatures.charCount += elementFeatures.charCount;
+      features.set(parent, parentFeatures);
+      parent = parent.parentElement;
+    }
+  }
 
-  // Scoring of images. Based of image size, alt/title text, and
-  // associated caption text. Also affects the score of each
-  // image's parent
+  // Derive non-nominal anchor features from the bottom up
+  var anchors = doc.body.querySelectorAll('a[href]');
+  forEach.call(anchors, function deriveAnchorFeatures(anchor) {
+    var anchorFeatures = features.get(anchor);
+    var cc = anchorFeatures.charCount;
+    if(!cc) {
+      return;
+    }
+    anchorFeatures.anchorCharCount = cc;
+    features.set(anchor, anchorFeatures);
+    var parentFeatures, parent = anchor.parentElement;
+    while(parent) {
+      parentFeatures = features.get(parent);
+      parentFeatures.anchorCharCount += cc;
+      features.set(parent, parentFeatures);
+      parent = parent.parentElement;
+    }
+  });
+
+  // Apply intrinsic bias (based on the type of element itself)
+  forEach.call(elements, function applyIntrinsicBias(element) {
+    var bias = INTRINSIC_BIAS.get(element.localName);
+    if(!bias) {
+      return;
+    }
+    var elementFeatures = features.get(element);
+    elementFeatures.score += bias;
+    features.set(element, elementFeatures);
+  });
+
+  // Penalize descendants of list elements
+  var listDescendants = doc.body.querySelectorAll('li *,ol *,ul *');
+  forEach.call(listDescendants, function biasLists(element) {
+    var elementFeatures = features.get(element);
+    elementFeatures.score -= 20;
+    features.set(element, elementFeatures);
+  });
+
+  // Penalize descendants of navigational elements
+  // NOTE: due to pre-filtering this is largely a no-op, but
+  // pre-filtering may be disabled in the future
+  var navDescendants = doc.body.querySelectorAll(
+    'aside *, header *, footer *, nav *');
+  forEach.call(navDescendants, function biasNavs(element) {
+    var elementFeatures = features.get(element);
+    elementFeatures.score -= 50;
+    features.set(element, elementFeatures);
+  });
+
+  // Score images and image parents
+  var reduce = Array.prototype.reduce;
   var images = doc.body.getElementsByTagName('img');
-  forEach.call(images, scoreImage.bind(this, features));
+  forEach.call(images, function scoreImage(image) {
+    var imageFeatures = features.get(image);
+    var parent = image.parentElement;
+    var parentFeatures = features.get(parent);
+    // Penalize carousels
+    parentFeatures.score += reduce.call(parent.childNodes,
+      function calculateImageSiblingPenalty(bias, node) {
+      return 'img' === node.localName && node !== image ? bias - 5 : bias;
+    }, 0);
+    // Reward auxillary descriptions
+    var alt = image.getAttribute('alt');
+    var title = image.getAttribute('title');
+    var caption = parent.localName == 'figure' &&
+      parent.querySelector('figcaption');
+    var supportingTextBias = alt || title || caption ? 30 : 0;
+    imageFeatures.score += supportingTextBias;
+    parentFeatures.score +=  supportingTextBias;
+    // Reward large images
+    var area = image.width ? image.width * image.height : 0;
+    var areaBias = 0.0015 * Math.min(100000, area);
+    imageFeatures.score += areaBias;
+    parentFeatures.score += areaBias;
+    features.set(image, imageFeatures);
+    features.set(parent, parentFeatures);
+  });
 
-  // Other biases, such as position, number of characters, and ratio
-  // of anchor-text characters to non-anchor-text characters.
+  // Bias elements based on attribute content
+  var RE_TOKEN_SPLIT = /[\s-_]+/g;
+  var attributeNames = ['id', 'class', 'itemprop', 'name', 'role'];
+  forEach.call(elements, function biasAttributes(element) {
+    var text = attributeNames.map(function getValue(name) {
+      return element.getAttribute(name);
+    }).join(' ').toLowerCase();
+    if(!text) return;
+    var tokens = text.split(RE_TOKEN_SPLIT).filter(function (value) {
+      return value;
+    });
+    if(!tokens.length) return;
+    var bias = 0;
+    (new Set(tokens)).forEach(function sumBias(token) {
+      bias += ATTRIBUTE_BIAS.get(token) || 0;
+    });
+    if(!bias) return;
+    var elementFeatures = features.get(element);
+    elementFeatures.score += bias;
+    features.set(element, elementFeatures);
+  });
+
+  // Bias the parents of ccertain elements
+  forEach.call(elements, function biasParent(element) {
+    var bias = DESCENDANT_BIAS.get(element.localName);
+    if(!bias) return;
+    var parent = element.parentElement;
+    var parentFeatures = features.get(parent);
+    parentFeatures.score += bias;
+    features.set(parent, parentFeatures);
+  });
+
   forEach.call(elements, scoreElement.bind(this, features));
 
-  // Find the best element
-  var result = reduce.call(elements,
-    getMaxScore.bind(this, features), doc.body);
+  // Expose attributes for debugging
+  var docElements = doc.documentElement.getElementsByTagName('*');
+  forEach.call(docElements, function expose(element) {
+    var meta = features.get(element);
+    if(options.SHOW_CHAR_COUNT && meta.charCount)
+      element.setAttribute('charCount', meta.charCount);
+    if(options.SHOW_SCORE && meta.score)
+      element.setAttribute('score', meta.score);
+  });
 
-  // Optionally expose some debugging information into the DOM
-  exposeAttributes(doc, features, options);
-
-  return result;
+  // Find and return the highest scoring element, defaulting to
+  // the body element
+  return reduce.call(elements, function (previous, current) {
+    return features.get(current).score > features.get(previous).score ?
+      current : previous;
+  }, doc.body);
 }
 
 // Public API
