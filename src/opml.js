885f3aa2b26e0b9bf82580e9f06f206e90fc24e2
Josh <joshua.froelich@gmail.com>
2014-11-06 09:27:39 -0500

    Ensure lucu namespace initialized

--- src/opml.js
+++ src/opml.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
+var lucu = lucu || {};
+
 /**
  * Returns an OPML XMLDocument object. Feeds should be an array of feed
  * objects. Feed objects should have properties title, url, description, and
