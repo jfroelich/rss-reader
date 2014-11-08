5fc3157fbd6506da71ab69f6a3c0e10f6c2c4a18
Josh <joshua.froelich@gmail.com>
2014-11-08 10:21:03 -0500

    Simplified code
    Deprecated IIFE and just use a plain function. Cleaned up some of the
    internal code and functions that were largely a remnant of an older
    deprecated approach. Cleaned up some comments. Inlined some functions
    that are only called once and that do not significantly detract from
    readability.

--- src/fetch-html.js
+++ src/fetch-html.js
@@ -2,9 +2,14 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
+
+
+var lucu = lucu || {};
+
 /**
- * Fetch HTML Document module
- * NOTE: this uses the responseURL as the base url
+ * Fetches an HTML document. Passes the document and the
+ * post-redirect url to the onComplete callback.
+ *
  * TODO: consider embedding iframe content
  * TODO: consider sandboxing iframes
  * TODO: resolve other types of urls: applet,audio,embed,iframe,object,
@@ -13,75 +18,9 @@
  * area.href, form.action, script.src
  * TODO: html compression? like enforce boolean attributes? see kangax lib
  * TODO: scrubbing (e.g. remove images without src attribute?)
- * TODO: there is no need to have a module. This can all be
- * a global function (or in the lucu namespace)
  */
-(function(exports) {
-'use strict';
-
-var filter = Array.prototype.filter;
-var forEach = Array.prototype.forEach;
-
-function getMimeType(request) {
-  return request && request.getResponseHeader('Content-Type');
-}
-
-function isTextHTML(contentType) {
-  return /text\/html/i.test(contentType);
-}
-
-var RE_RESOLVABLE_PROTOCOL = /^\s*http|https\s*:/i;
-
-function isResolvableAnchor(anchor) {
-  var href = anchor.getAttribute('href');
-  if(!href) return false;
-  href = href.trim();
-  if(!href) return false;
-
-  // fast case
-  if(href.charAt(0) == '#') return true;
-  if(/^\s*javascript\s*:/i.test(href)) return false;
-  if(/^\s*tel\s*:/i.test(href)) return false;
-  if(/^\s*mailto\s*:/i.test(href)) return false;
-  if(/^\s*ftp\s*:/i.test(href)) return false;
-  return true;
-}
-
-function resolveAnchor(baseURL, anchor) {
-  var href = anchor.getAttribute('href').trim();
-  try {
-    var resolvedURL = URI(href).absoluteTo(baseURL).toString();
-    anchor.setAttribute('href', resolvedURL);
-  } catch(e) {
-    console.debug('Medialize absoluteTo exception: %s %s', href, e);
-  }
-}
-
-function isResolvableImage(image) {
-  var src = image.getAttribute('src');
-  if(!src) return false;
-  src = src.trim();
-  if(!src) return false;
-  if(/^\s*data\s*:/i.test(src)) {
-    console.debug('Cannot resolve data: image %s', src);
-    return false;
-  }
-
-  return true;
-}
-
-function resolveImage(baseURL, image) {
-  var src = image.getAttribute('src').trim();
-  try {
-    var resolvedURL = URI(src).absoluteTo(baseURL).toString();
-    image.setAttribute('src', resolvedURL);
-  } catch(e) {
-    console.debug('Medialize absoluteTo exception: %s %s', src, e);
-  }
-}
-
-
-function fetchHTML(url, timeout, onComplete, onError) {
+lucu.fetchHTML = function(url, timeout, onComplete, onError) {
+  'use strict';
 
   var request = new XMLHttpRequest();
   request.timeout = timeout;
@@ -90,9 +29,10 @@ function fetchHTML(url, timeout, onComplete, onError) {
   request.onabort = onError;
 
   request.onload = function() {
-    var mime = getMimeType(this);
+    var mime = this.getResponseHeader('Content-Type');
 
-    if(!isTextHTML(mime)) {
+    var RE_TYPE_TEXT_HTML = /text\/html/i;
+    if(!RE_TYPE_TEXT_HTML.test(mime)) {
       onError({type: 'invalid-content-type', target: this, contentType: mime});
       return;
     }
@@ -102,26 +42,74 @@ function fetchHTML(url, timeout, onComplete, onError) {
       return;
     }
 
+    var document = this.responseXML;
+
     // Resolve anchors
     var baseURL = this.responseURL;
-    var anchors = this.responseXML.body.getElementsByTagName('a');
-    var resolvableAnchors = filter.call(anchors, isResolvableAnchor);
-    resolvableAnchors.forEach(resolveAnchor.bind(this, baseURL));
+    var anchors = document.body.getElementsByTagName('a');
+
+    var filter = Array.prototype.filter;
+
+    // Get a subset of only those anchors that we consider resolvable
+    var resolvableAnchors = filter.call(anchors, function (anchor) {
+      var href = anchor.getAttribute('href');
+      if(!href) return false;
+      href = href.trim();
+      if(!href) return false;
+
+      // fast case
+      if(href.charAt(0) == '#') return true;
+      if(/^\s*javascript\s*:/i.test(href)) return false;
+      if(/^\s*tel\s*:/i.test(href)) return false;
+      if(/^\s*mailto\s*:/i.test(href)) return false;
+      if(/^\s*ftp\s*:/i.test(href)) return false;
+      return true;
+    });
+
+    resolvableAnchors.forEach(function (anchor) {
+      var href = anchor.getAttribute('href').trim();
+      try {
+        var resolvedURL = URI(href).absoluteTo(baseURL).toString();
+        anchor.setAttribute('href', resolvedURL);
+      } catch(e) {
+        console.debug('Medialize absoluteTo exception: %s %s', href, e);
+      }
+    });
+
     // Resolve image src urls
-    var images = this.responseXML.body.getElementsByTagName('img');
-    var resolvableImages = filter.call(images, isResolvableImage);
-    resolvableImages.forEach(resolveImage.bind(this, baseURL));
-    // And we are done. Pass back responseURL so caller can consider
+    var images = document.body.getElementsByTagName('img');
+
+    // Get a subset of resolvable images
+    var resolvableImages = filter.call(images, function (image) {
+      var src = image.getAttribute('src');
+      if(!src) return false;
+      src = src.trim();
+      if(!src) return false;
+      if(/^\s*data\s*:/i.test(src)) {
+        //console.debug('Cannot resolve data: image %s', src);
+        return false;
+      }
+
+      return true;
+    });
+
+    resolvableImages.forEach(function (image) {
+      var src = image.getAttribute('src').trim();
+      try {
+        var resolvedURL = URI(src).absoluteTo(baseURL).toString();
+        image.setAttribute('src', resolvedURL);
+      } catch(e) {
+        console.debug('Medialize absoluteTo exception: %s %s', src, e);
+      }
+    });
+
+    // Pass back responseURL so caller can consider
     // doing something with the post-redirect url
-    onComplete(this.responseXML, this.responseURL);
+    onComplete(document, baseURL);
   };
 
   request.open('GET', url, true);
   request.responseType = 'document';
   request.send();
-}
 
-exports.lucu = lucu || {};
-exports.lucu.fetchHTML = fetchHTML;
-
-}(this));
+};
