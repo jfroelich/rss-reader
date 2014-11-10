8a359fffaa7c1560fe3a484137bf14e04f1e5af2
Josh <joshua.froelich@gmail.com>
2014-11-10 17:22:20 -0500

    Overhaul
    No longer an IIFE. Simplified the code

--- src/discover-feeds.js
+++ src/discover-feeds.js
@@ -4,66 +4,39 @@
 
 var lucu = lucu || {};
 
-
-// TODO: this really does not need an IIFE, this can just be a simple function
-
 /**
- * Discover module for searching for feeds
- *
+ * Use Google's seach service to find feeds. Sends a search query to Google
+ * and passes an array of results to the callback. Calls the fallback instead
+ * if an error occurs (e.g. offline, 404, 503, content type).
  */
-(function(exports) {
-'use strict';
+lucu.queryGoogleFeeds = function(query, timeout, callback, fallback) {
+  'use strict';
+  query = (query || '').trim();
+  if(!query) return callback('',[]);
+  fallback = fallback || function () {};
 
-/**
- * Use Google's find feed service to find feed URLs corresponding to a
- * google search query.
- *
- * TODO: document the props of a result object
- */
-function queryGoogleFeeds(query, timeout, onComplete, onError) {
-
-  if(!query) {
-    return onComplete('',[]);
-  }
-
-  query = query.trim();
-
-  if(!query) {
-    return onComplete('',[]);
-  }
-
-  onError = onError || function noop() {};
+  var base = 'https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q=';
+  var url = base + encodeURIComponent(query);
 
   var request = new XMLHttpRequest();
   request.timeout = timeout;
-  request.onerror = onError;
-  request.ontimeout = onError;
-  request.onabort = onError;
+  request.onerror = fallback;
+  request.ontimeout = fallback;
+  request.onabort = fallback;
   request.onload = function () {
     var data = this.response.responseData;
     data.entries = data.entries || [];
     data.query = data.query || '';
-    var entries = data.entries.map(sanitize);
-    onComplete(data.query, entries);
+    var entries = data.entries.map(function (entry) {
+      // Strip <br> elements from snippet
+      var snippet = entry.contentSnippet;
+      if(!snippet) return entry;
+      entry.contentSnippet = snippet.replace(/<br>/gi,'');
+      return entry;
+    });
+    callback(data.query, entries);
   };
-  request.open('GET', getURL(query), true);
+  request.open('GET', url, true);
   request.responseType = 'json';
   request.send();
-}
-
-function getURL(query) {
-  return 'https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q=' +
-    encodeURIComponent(query);
-}
-
-// Strip <br> elements from snippet
-function sanitize(entry) {
-  var snippet = entry.contentSnippet;
-  if(!snippet) return entry;
-  entry.contentSnippet = snippet.replace(/<br>/gi,'');
-  return entry;
-}
-
-exports.queryGoogleFeeds = queryGoogleFeeds;
-
-}(lucu));
+};
