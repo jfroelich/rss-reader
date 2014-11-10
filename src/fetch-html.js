fcc5f9de9bc78b549c8fc16bec352248b98eadb7
Josh <joshua.froelich@gmail.com>
2014-11-10 17:24:28 -0500

    Use new lucu.resolveElements function
    Other url-containing element attributes, such as blockquote.cite and
    track.src , are now resolved.  This change increases the coherency of
    the function. Also made some minor comment changes.

--- src/fetch-html.js
+++ src/fetch-html.js
@@ -2,22 +2,20 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
-
-
 var lucu = lucu || {};
 
 /**
  * Fetches an HTML document. Passes the document and the
  * post-redirect url to the onComplete callback.
  *
- * TODO: consider embedding iframe content
- * TODO: consider sandboxing iframes
- * TODO: resolve other types of urls: applet,audio,embed,iframe,object,
- * video, param?
- * blockquote.cite, track.src, link.href, base.href, source.src,
- * area.href, form.action, script.src
+ * TODO: use overrideMimeType to avoid the need for a fallback
+ * and to allow the normal onerror to be triggered. Then deprecate
+ * the content type check?
+ *
+ * TODO: consider embedding iframe content?
+ * TODO: consider sandboxing iframes?
  * TODO: html compression? like enforce boolean attributes? see kangax lib
- * TODO: scrubbing (e.g. remove images without src attribute?)
+ * TODO: scrubbing/html-tidy (e.g. remove images without src attribute?)
  */
 lucu.fetchHTML = function(url, timeout, onComplete, onError) {
   'use strict';
@@ -29,87 +27,33 @@ lucu.fetchHTML = function(url, timeout, onComplete, onError) {
   request.onabort = onError;
 
   request.onload = function() {
-    var mime = this.getResponseHeader('Content-Type');
+    var mimeType = this.getResponseHeader('Content-Type');
 
-    var RE_TYPE_TEXT_HTML = /text\/html/i;
-    if(!RE_TYPE_TEXT_HTML.test(mime)) {
-      onError({type: 'invalid-content-type', target: this, contentType: mime});
-      return;
-    }
-
-    if(!this.responseXML || !this.responseXML.body) {
-      onError({type: 'invalid-document', target: this});
+    if(!/text\/html/i.test(mimeType)) {
+      onError({type: 'invalid-content-type', target: this, contentType: mimeType});
       return;
     }
 
     var document = this.responseXML;
 
-    // Resolve anchors
+    if(!document || !document.body) {
+      onError({type: 'invalid-document', target: this});
+      return;
+    }
+
     var baseURL = this.responseURL;
-    var anchors = document.body.getElementsByTagName('a');
-
-    var filter = Array.prototype.filter;
-
-    // Get a subset of only those anchors that we consider resolvable
-    var resolvableAnchors = filter.call(anchors, function (anchor) {
-      var href = anchor.getAttribute('href');
-      if(!href) return false;
-      href = href.trim();
-      if(!href) return false;
-
-      // fast case
-      if(href.charAt(0) == '#') return true;
-      if(/^\s*javascript\s*:/i.test(href)) return false;
-      if(/^\s*tel\s*:/i.test(href)) return false;
-      if(/^\s*mailto\s*:/i.test(href)) return false;
-      if(/^\s*ftp\s*:/i.test(href)) return false;
-      return true;
-    });
-
-    resolvableAnchors.forEach(function (anchor) {
-      var href = anchor.getAttribute('href').trim();
-      try {
-        var resolvedURL = URI(href).absoluteTo(baseURL).toString();
-        anchor.setAttribute('href', resolvedURL);
-      } catch(e) {
-        console.debug('Medialize absoluteTo exception: %s %s', href, e);
-      }
-    });
-
-    // Resolve image src urls
-    var images = document.body.getElementsByTagName('img');
-
-    // Get a subset of resolvable images
-    var resolvableImages = filter.call(images, function (image) {
-      var src = image.getAttribute('src');
-      if(!src) return false;
-      src = src.trim();
-      if(!src) return false;
-      if(/^\s*data\s*:/i.test(src)) {
-        //console.debug('Cannot resolve data: image %s', src);
-        return false;
-      }
-
-      return true;
-    });
-
-    resolvableImages.forEach(function (image) {
-      var src = image.getAttribute('src').trim();
-      try {
-        var resolvedURL = URI(src).absoluteTo(baseURL).toString();
-        image.setAttribute('src', resolvedURL);
-      } catch(e) {
-        console.debug('Medialize absoluteTo exception: %s %s', src, e);
-      }
-    });
+    // TODO: is resolution integral to fetch? Or should this be the
+    // caller's responsibility?
+    lucu.resolveElements(document, baseURL);
 
     // Pass back responseURL so caller can consider
-    // doing something with the post-redirect url
+    // doing something with the post-redirect url.
+    // TODO: is there some attribute of document that can be set
+    // to baseURL instead of passing it as a separate value?
     onComplete(document, baseURL);
   };
 
   request.open('GET', url, true);
   request.responseType = 'document';
   request.send();
-
 };
