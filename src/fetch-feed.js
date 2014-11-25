c1385e8cdd0840d165b6f5718a021bd4f555c2bb
Josh <joshua.froelich@gmail.com>
2014-11-25 09:52:16 -0500

    Use native xml parsing
    Use overrideMimeType('application/xml') to force native parser to throw
    errors for non-xml urls instead of trying to manage it explicitly. This
    simplifies the code. This commit also cleans up some comments.

--- src/fetch-feed.js
+++ src/fetch-feed.js
@@ -5,24 +5,8 @@
 var lucu = lucu || {};
 
 /**
- * TODO: use overrideMimeType and simply allow the native mechanism
- * to trigger invalid content type errors. Unlike fetching html I think
- * i do want to use it here because we deserialize immediately and
- * deserializing will catch any errors.
- *
  * TODO: fetchFullArticles should not be a parameter and should not occur
  * here. Use the new lucu.augmentEntryContent function.
- * TODO: lucu.augmentEntryContent no longer avoids fetching the html
- * for entries that already exist in the database. So now we need
- * another separate function that filters out entries from the feed
- * that already exist.
- * TODO: entryTimeout is now a parameter to augmentEntryContent
- * so it does not need to be here
- * TODO: note that lucu.augmentEntryContent operates off the array
- * of entries, not the original feed
- * TODO: pass responseURL back to onComplete so that caller can
- * react to redirects?
- *
  *
  * Fetches the XML for a feed from a URL, then parses it into
  * a javascript object, and passes this along to a callback. If an error
@@ -38,39 +22,18 @@ var lucu = lucu || {};
  * error object that is passed to onError. I also think it really does
  * need error codes because identifying each error by string key is making
  * it difficult to respond to different errors differently.
- * TODO: this should not also do the xml to feed conversion? The coupling is
- * too tight because I want to be able to test fetching and transformation
- * separately. but separate web page fetching requires the feed be
- * converted first to identify links. Maybe what I am saying is I want
- * fetching to only be about fetching, and some more abstract controller
- * does the sequential cohesion by composing fetch+convert
- * TODO: is an approach that uses overrideMimeType better than
- * checking content type? will it just cause the native parsing errors I
- * was trying to avoid?
  * TODO: request.responseURL contains the redirected URL. Need to update the url
  * when that happens. Maybe I need to be storing both urls in the db?
- * TODO: entryTimeout should maybe be deprecated? Where should it really
- * be coming from?
- *
- * TODO: rather than call onerror, throw exception?
- *
- * @param params {object} an object literal that should contain props:
- * - url the remote url of the feed to fetch
- * - oncomplete - a callback to call when the feed is fetched, that is passed
- * a javascript object representing the contents of the feed
- * - onerror - a callback to call in case of an error, that is called instead
- * of oncomplete
- * - timeout - optional timeout before giving up on feed
- * - fetchFullArticles - if true, fetches full content of entry.link and
- * uses that instead of the feed content
- * - entryTimeout - optional timeout before giving up on fetching webpage for entry
  */
 lucu.fetchFeed = function(params) {
   'use strict';
 
   // TODO: once I move out the augment code, change params back to explicit
   // arguments
-
+  // TODO: remove duplicate entries?
+  // TODO: move the augment stuff completely out of the fetch-feed
+  // function and into its thing and then deprecate entryTimeout
+  // and shouldAugmentEntries parameter
   // NOTE: fetchFullArticles exists because fetch is doing two things instead
   // of one. I should never haved mixed together the augmentEntry code with
   // the fetch code. The caller can easily just pass the result of fetch to
@@ -94,61 +57,33 @@ lucu.fetchFeed = function(params) {
 
   var request = new XMLHttpRequest();
   request.timeout = timeout;
-  request.onerror = onError;
+  request.onerror = function(event) {
+    console.debug('fetch feed error');
+    console.dir(event);
+    onError(event);
+  };
   request.ontimeout = onError;
   request.onabort = onError;
   request.onload = function() {
-    var RE_FEED_TYPE = /(application|text)\/(atom|rdf|rss)?\+?xml/i;
-    var RE_HTML_TYPE = /text\/html|plain/i;
-    var mime = this.getResponseHeader('Content-Type');
-    var xmlDocument = null;
-
-    if(RE_FEED_TYPE.test(mime)) {
-      xmlDocument = this.responseXML;
-    } else if(RE_HTML_TYPE.test(mime)) {
-      // TODO: if we use overrideMimeType then the native utility
-      // should properly fail for us?
-      // Fallback
-      var parser = new DOMParser();
-      xmlDocument = parser.parseFromString(this.responseText,
-        'application/xml');
-      if(!xmlDocument || !xmlDocument.documentElement) {
-        return onError(e);
-      }
-      var parserError = xmlDocument.querySelector('parsererror');
-      if(parserError) {
-        return onError(parserError.textContent);
-      }
-
-    } else {
-      return onError({type:'invalid-content-type', target: this});
-    }
-
+    var xmlDocument = this.responseXML;
     if(!xmlDocument || !xmlDocument.documentElement) {
       return onError({type: 'invalid-xml', target: this});
     }
 
-    // TODO: should this bubble?
     try {
       var feed = lucu.deserializeFeed(xmlDocument);
     } catch(e) {
       return onError({type: 'invalid-xml', target: this, details: e});
     }
 
-    feed.entries = feed.entries.filter(function hasLink(entry) {
+    feed.entries = feed.entries.filter(function (entry) {
       return entry.link;
     });
 
-    feed.entries = feed.entries.map(function rewriteEntryLink(entry) {
+    feed.entries = feed.entries.map(function (entry) {
       entry.link = lucu.rewriteURL(entry.link);
       return entry;
     });
-
-    // TODO: remove duplicate entries?
-
-    // TODO: move the augment stuff completely out of the fetch-feed
-    // function and into its thing and then deprecate entryTimeout
-    // and shouldAugmentEntries parameter
 
     if(!fetchFullArticles) {
       return onComplete(feed);
@@ -199,6 +134,7 @@ lucu.fetchFeed = function(params) {
   };
 
   request.open('GET', url, true);
+  request.overrideMimeType('application/xml');
   request.send();
 
   function onDatabaseError(error) {
