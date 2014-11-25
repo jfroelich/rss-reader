1f59f2cf4ef91bc326ea76616f365151bff86258
Josh <joshua.froelich@gmail.com>
2014-11-25 09:50:41 -0500

    Minor comment changes

--- src/slideshow.js
+++ src/slideshow.js
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
-// TODO: use class 'slideRead' instead of custom attribute 'read'
-// TODO: use article element instead of a div
-
-
 'use strict';
 
 var currentSlide = null;
