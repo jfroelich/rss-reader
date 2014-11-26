a56054e81845769881c279191fa4344e68adfb50
Josh <joshua.froelich@gmail.com>
2014-11-26 14:48:14 -0500

    Created archive-feeds.js
    Moved archiveFeeds function from background.js into archive-feeds.js

--- src/background.js
+++ src/background.js
@@ -13,14 +13,10 @@
  */
 chrome.runtime.onMessage.addListener(function (message) {
 
-  if(!message) {
-    console.error('Received undefined message');
-    return;
-  }
-
   switch(message.type) {
     case 'entryRead':
-      // console.debug('Received message that entry %s was marked as read', message.entry.id);
+      // console.debug('Received message that entry %s was marked as read',
+      //   message.entry.id);
       updateBadge();
       break;
     case 'importFeedsCompleted':
@@ -54,11 +50,9 @@ chrome.runtime.onMessage.addListener(function (message) {
  * Sets the badge text to a count of unread entries
  */
 function updateBadge() {
-
   var request = indexedDB.open(lucu.DB_NAME, lucu.DB_VERSION);
   request.onerror = console.error;
   request.onblocked = console.error;
-  // Because this is the first call to open
   request.onupgradeneeded = lucu.upgradeDatabase;
   request.onsuccess = function (event) {
     var db = event.target.result;
@@ -114,6 +108,10 @@ chrome.browserAction.onClicked.addListener(function () {
 // TODO: is there a way to avoid this being called every time
 // the background page is loaded or reloaded, enabled/disabled?
 chrome.runtime.onInstalled.addListener(function onInstall() {
+
+  // TODO: trigger database update here, explicitly
+  // instead of relying on updateBadge
+
   // This also triggers database creation
   updateBadge();
 });
@@ -122,93 +120,13 @@ chrome.alarms.onAlarm.addListener(function onAlarm(alarm) {
   if('poll' == alarm.name) {
     lucu.pollFeeds(navigator);
   } else if('archive' == alarm.name) {
-    startArchive();
+    lucu.archiveFeeds();
   }
 });
 
 // TODO: customizable per feed?
 chrome.alarms.create('poll', {periodInMinutes: 20});
 chrome.alarms.create('archive', {periodInMinutes: 24 * 60});
-
-/**
- * Iterate over entries in storage and archive certain entries
- * if conditions are met.
- *
- * TODO: this function really belongs somewhere else, in the same
- * sense that lucu.poll is a separate module
- *
- * TODO: use caolan async.each or similar here.
- */
-function startArchive() {
-  var request = indexedDB.open(lucu.DB_NAME, lucu.DB_VERSION);
-  request.onerror = console.error;
-  request.onblocked = console.error;
-  request.onsuccess = function (event) {
-    var db = event.target.result;
-    var transaction = db.transaction('entry','readwrite');
-    var store = transaction.objectStore('entry');
-    var expiresMs = 30 * 24 * 60 * 60 * 1000;
-    var now = Date.now();
-    // We want to instead select only not-archived articles.
-    // Otherwise this just repeats itself on the first X entries.
-    // So we  pick an index that exists for articles where once
-    // archived the article would no longer appear in the index. The
-    // index on the feed id for each entry works nicely for this
-    // purpose.
-    var index = store.index('feed');
-    var request = index.openCursor();
-    var processed = 0, limit = 1000;
-    request.onsuccess = function() {
-      var cursor = this.result;
-      if(!cursor) return;
-      if(processed >= limit) {
-        return;
-      }
-
-      var entry = cursor.value;
-      var created = entry.created;
-      if(!created) {
-        console.debug('Unknown date created for entry %s, unable to archive',
-          JSON.stringify(entry));
-        processed++;
-        return cursor.continue();
-      }
-
-      // If this happens something is wrong
-      if(entry.archiveDate) {
-        console.warn('Entry already archived, not re-archiving %s', JSON.stringify(entry));
-        processed++;
-        return cursor.continue();
-      }
-
-      var age = now - created;
-      var shouldArchive = age > expiresMs;
-      // NOTE: in an old version this used to avoid archiving unread
-      // articles. For now this is ignored.
-      // NOTE: this used to keep feed id so that unsubscribe
-      // would delete archived articles as well
-      if(shouldArchive) {
-        delete entry.content;
-        delete entry.feed;
-        delete entry.feedLink;
-        delete entry.feedTitle;
-        delete entry.pubdate;
-        delete entry.readDate;
-        delete entry.title;
-        entry.archiveDate = now;
-        // console.debug('Archiving %s', entry.id);
-        cursor.update(entry);
-      }
-
-      processed++;
-      cursor.continue();
-    };
-
-    transaction.oncomplete = function () {
-      console.debug('Archived %s entries', processed);
-    };
-  };
-}
 
 function showNotification(message) {
   chrome.permissions.contains({permissions: ['notifications']},
