193132ab5579d60f11eef88952576a19d792d6f4
Josh <joshua.froelich@gmail.com>
2014-11-26 21:53:17 -0500

    Simplified url resolution
    Use a map instead of a bunch of if statements and arrays. Do a protocol
    exists check instead of avoiding resolution of various protocols (no
    idea why I was doing it any other way previously).

--- src/resolve-elements.js
+++ src/resolve-elements.js
@@ -4,83 +4,63 @@
 
 var lucu = lucu || {};
 
+/**
+ * TODO: support img srcset
+ * TODO: support style.backgroundImage?
+ * TODO: the new template tag?
+ * NOTE: not supporting applet
+ * NOTE: iframe.srcdoc?
+ * NOTE: ignores param values with URIs
+ * NOTE: stripping the base tag could lead to invalid urls
+ */
 lucu.resolveElements = function(document, baseURL) {
   'use strict';
-
-  // TODO: support img srcset
-  // TODO: support style.backgroundImage?
-  // the new template tag?
-
-  // NOTE: not supporting applet
-  // NOTE: iframe.srcdoc?
-  // NOTE: ignores param values with URIs
-
   var forEach = Array.prototype.forEach;
 
-  // Strip base tags from the document (includes <head>)
-  // NOTE: this technically could lead to invalid urls, I think, if the base
-  // is not equivalent to the url of the document. However, for now, it is
-  // better to avoid the whole app misbehaving on load of any document
-  // with a base tag.
-  var baseElements = document.documentElement.getElementsByTagName('base');
+  var baseElements = document.getElementsByTagName('base');
   forEach.call(baseElements, function(base) {
     if(!base) return;
-    // console.debug('removing %s', base.outerHTML);
     base.remove();
   });
 
-  var hrefAttributes = ['a','area','link'];
-  var srcAttributes = ['audio', 'embed', 'iframe', 'img', 'script',
-    'source', 'track', 'video'];
+  var name2attribute = new Map([
+    ['a', 'href'],
+    ['area', 'href'],
+    ['audio', 'src'],
+    ['blockquote', 'cite'],
+    ['embed', 'src'],
+    ['iframe', 'src'],
+    ['form', 'action'],
+    ['img', 'src'],
+    ['link', 'href'],
+    ['object', 'data'],
+    ['script', 'src'],
+    ['source', 'src'],
+    ['track', 'src'],
+    ['video', 'src']
+  ]);
+
+  // TODO: build this from the map, do not specify redundantly
   var resolvables = document.documentElement.querySelectorAll(
     'a, area, audio, blockquote, embed, iframe, form, img, link, '+
     'object, script, source, track, video');
   forEach.call(resolvables, function resolve(element) {
     var name = element.localName;
-    var attribute = null;
-
-    if(hrefAttributes.indexOf(name) != -1) {
-      attribute = 'href';
-    } else if(srcAttributes.indexOf(name) != -1) {
-      attribute = 'src';
-    } else if(name == 'form') {
-      attribute = 'action';
-    } else if(name == 'blockquote') {
-      attribute = 'cite';
-    } else if(name == 'object') {
-      // NOTE: this could be wrong, never tested
-      attribute = 'data';
-    }
-
+    var attribute = name2attribute.get(name);
     if(!attribute) return;
-
     var url = element.getAttribute(attribute);
     if(!url) return;
     url = url.trim();
     if(!url) return;
-
-    // TODO: use URI.js to parse the relative and get the
-    // protocol. Only resolve if no protocol. This avoids
-    // resolving URNs (e.g. data/ftp/etc) and avoids
-    // spelling errors and avoids the need to do these
-    // checks
-    // NOTE: these checks are unnecessary, all they
-    // do is avoid the exception thrown when calling absoluteTo
-    if(attribute == 'href' || attribute == 'action') {
-      if(/^\s*javascript\s*:/i.test(url)) return;
-      if(/^\s*tel\s*:/i.test(url)) return;
-      if(/^\s*mailto\s*:/i.test(url)) return;
-      if(/^\s*ftp\s*:/i.test(url)) return;
-    } else if(attribute == 'src') {
-      if(/^\s*data\s*:/i.test(url)) return;
-      if(/^\s*javascript\s*:/i.test(url)) return;
-    }
-
     try {
-      var resolved = URI(url).absoluteTo(baseURL).toString();
+      var uri = new URI(url);
+      // Suppress some medialize exceptions
+      if(uri.protocol()) return;
+      var resolved = uri.absoluteTo(baseURL).toString();
+      // console.debug('resolved %s as %s', url, resolved);
       element.setAttribute(attribute, resolved);
     } catch(e) {
-      console.debug('resolve error %s %s', element.outerHTML, e);
+      console.debug('%s %s', e, url);
     }
   });
 };
