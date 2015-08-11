130b1bc885f73e59e5b7e0a8dff7c5b56a70a1c6
Josh <joshua.froelich@gmail.com>
2015-08-11 13:51:08 -0400

    Created sanitize-feed lib
    Moved functions from feeds.js into sanitize-feed.js, rewrote some of
    the functions (mostly style changes for better clarity).

--- src/feeds.js
+++ src/feeds.js
@@ -57,31 +57,3 @@ lucu.updateFeed = function(db, localFeed, remoteFeed, oncomplete) {
     async.forEach(remoteFeed.entries, mergeEntry, oncomplete);
   };
 };
-
-/**
- * Returns a sanitized copy of dirtyFeed.
- *
- * TODO: html entities?
- */
-lucu.sanitizeFeed = function(feed) {
-  'use strict';
-  function sanitizeString(str) {
-    if(!str) return;
-    str = lucu.stripTags(str);
-    if(str) str = lucu.stripControls(str);
-    if(str) str = str.replace(/\s+/,' ');
-    if(str) str = str.trim();
-    return str;
-  }
-
-  var output = {};
-  var title = sanitizeString(feed.title);
-  if(title) output.title = title;
-  var description = sanitizeString(feed.description);
-  if(description) output.description = description;
-  var link = sanitizeString(feed.link);
-  if(link) output.link = link;
-  if(feed.date) output.date = feed.date;
-  return output;
-};
-
---
+++ src/sanitize-feed.js
@@ -0,0 +1,63 @@
+// Copyright 2014 Josh Froelich. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file
+
+var lucu = lucu || {};
+
+/**
+ * Returns a sanitized copy of dirtyFeed.
+ *
+ * TODO: html entities?
+ */
+lucu.sanitizeFeed = function(inputFeed) {
+  'use strict';
+
+  var ss = lucu.sanitizeFeedString;
+
+  var outputFeed = {};
+
+  var title = ss(inputFeed.title);
+  if(title) {
+  	outputFeed.title = title;
+  }
+
+  var description = ss(inputFeed.description);
+  if(description) {
+  	outputFeed.description = description;
+  }
+  
+  var link = ss(inputFeed.link);
+  if(link) {
+  	outputFeed.link = link;
+  }
+  
+  if(inputFeed.date) {
+  	outputFeed.date = inputFeed.date;
+  }
+  
+  return outputFeed;
+};
+
+lucu.sanitizeFeedString = function(string) {
+  'use strict';
+
+  if(!string) {
+  	return;
+  }
+
+  string = lucu.stripTags(string);
+
+  if(string) {
+  	string = lucu.stripControls(string);
+  }
+
+  if(string) {
+  	string = string.replace(/\s+/,' ');
+  }
+  
+  if(string) {
+  	string = string.trim();
+  }
+  
+  return string;
+};
