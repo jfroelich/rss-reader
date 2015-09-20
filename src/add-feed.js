c9eff5536ec1361afbcb16b3d8eb7535e2abd330
Josh <joshua.froelich@gmail.com>
2015-09-20 10:41:37 -0400

    Rename feeds.js to add-feed.js
    Now that feeds.js only contains a single function for a single purpose,
    rename it.

--- src/add-feed.js
+++ src/add-feed.js
@@ -4,6 +4,9 @@
 
 var lucu = lucu || {};
 
+// TODO: this file only contains add feed funciton, just rename file or 
+// move this function to the area that uses it
+
 // TODO: ensure no future pubdates?
 // TODO: use the async's approach to error handling. Only use
 // one callback. The first argument should be the error object.
@@ -35,25 +38,4 @@ lucu.addFeed = function(db, feed, onComplete, onerror) {
   request.onerror = onerror;
 };
 
-// TODO: the caller should pass in last modified date of the remote xml file
-// so we can avoid pointless updates?
-// TODO: this should not be changing the date updated unless something
-// actually changed. However, we do want to indicate that the feed was
-// checked
-lucu.updateFeed = function(db, localFeed, remoteFeed, oncomplete) {
-  var clean = lucu.sanitizeFeed(remoteFeed);
-  if(clean.title) localFeed.title = clean.title;
-  if(clean.description) localFeed.description = clean.description;
-  if(clean.link) localFeed.link = clean.link;
-  if(clean.date) localFeed.date = clean.date;
-  localFeed.fetched = remoteFeed.fetched;
-  localFeed.updated = Date.now();
-  var tx = db.transaction('feed','readwrite');
-  var store = tx.objectStore('feed');
-  var request = store.put(localFeed);
-  request.onerror = console.debug;
-  var mergeEntry = lucu.mergeEntry.bind(null, db, localFeed);
-  request.onsuccess = function() {
-    async.forEach(remoteFeed.entries, mergeEntry, oncomplete);
-  };
-};
+
