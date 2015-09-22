713a8b245091cdb1d961f2937cd0afa5e05973c3
Josh <joshua.froelich@gmail.com>
2015-09-22 17:23:37 -0400

    Created entry.js
    Consolidated merge-entry.js and mark-entry-read.js

--- src/entry.js
+++ src/entry.js
@@ -6,6 +6,7 @@ var lucu = lucu || {};
 
 lucu.entry = lucu.entry || {};
 
+
 // TODO: some entry link URLs from feeds are pre-chain-of-redirect-resolution, 
 // and are technically duplicates because each redirects to the same URL at the 
 // end of the redirect chain. Therefore we should be storing the terminal link,
@@ -81,4 +82,52 @@ lucu.entry.merge = function(db, feed, entry, callback) {
   addEntryRequest.onerror = function() {
     callback();
   };
+};
+
+
+lucu.entry.markRead = function(entryId, callback, fallback) {
+  'use strict';
+  // console.log('Marking %s as read', entryId);
+  var onConnect = lucu.entry.onMarkReadConnect.bind(null, entryId, callback);
+  lucu.database.connect(onConnect, fallback);
+};
+
+lucu.entry.onMarkReadConnect = function(entryId, callback, error, database) {
+  'use strict';
+  var transaction = database.transaction('entry', 'readwrite');
+  transaction.oncomplete = callback;
+  var entryStore = transaction.objectStore('entry');
+  var markReadRequest = entryStore.openCursor(entryId);
+  markReadRequest.onsuccess = lucu.entry.markReadUpdateEntry;
+};
+
+lucu.entry.markReadUpdateEntry = function(event) {
+  'use strict';
+  var cursor = event.target.result;
+  if(!cursor) return;
+
+  // Get the entry at the cursor. It may be possible that the entry somehow
+  // no longer exists, so escape early when that is the case.
+  var entry = cursor.value;
+  if(!entry) return;
+
+  // Suppress attempts to mark an entry as read if it is already read
+  if(!entry.hasOwnProperty('unread')) {
+  	// console.debug('Entry is already marked as read, suppressing');
+  	return;
+  }
+
+  // Modify the entry
+  delete entry.unread;
+  entry.readDate = Date.now();
+  cursor.update(entry);
+
+  // console.debug('Marked %o as read', entry);
+
+  // Update the unread count
+  lucu.badge.update();
+
+  // Notify observers that the entry was read
+  // TODO: is anything listening to this? maybe deprecate?
+  chrome.runtime.sendMessage({type: 'entryRead', entry: entry});
 };
