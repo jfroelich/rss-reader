61178b9fc29fd3b4e3b5014839d77b200d12149e
Josh <joshua.froelich@gmail.com>
2015-10-17 17:57:16 -0400

    Deleted fetch-feed.js

--- src/fetch-feed.js
+++
@@ -1,8 +0,0 @@
-// Copyright 2014 Josh Froelich. All rights reserved.
-// Use of this source code is governed by a MIT-style license
-// that can be found in the LICENSE file
-
-var lucu = lucu || {};
-
-lucu.fetch = {};
-
