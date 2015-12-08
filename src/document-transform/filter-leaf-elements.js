b3e5b211610b602682cad72c8471dd0bf1435f67
Josh <joshua.froelich@gmail.com>
2015-12-08 13:22:02 -0500

    Revised filterLeafElements
    Use recursion, perform fewer removal operations, improved performance,
    improved correctness

--- src/document-transform/filter-leaf-elements.js
+++ src/document-transform/filter-leaf-elements.js
@@ -2,125 +2,112 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
-// NOTE: this is incredibly sloppy code that barely works, but it is
-// a bit intricate, so I am leaving it this way for now, but it
-// requires substantial improvement.
+'use strict';
 
-// TODO: there is a specific edge case not being handled
-// where certain elements, e.g. anchors, that do not contain
-// any child nodes, should be considered empty. And this must
-// be recursive as well, up the tree.
-// In the case of <ul><li><a></a></li></ul>, the result should
-// be that the entire subtree is removed.
-// Because this case is not currently handled, and because we
-// remove other nodes, this leads to some funny looking junk
-// areas of content (e.g. a list of empty bullet points)
-// This gets trickier because the logic, in the current impl,
-// has to be in a couple places. In isLeafElement, an anchor without
-// a firstChild should be considered empty. That should be handled
-// right now but for some odd reason it is not. Then once any element
-// is removed and we check its parent, its parent should go through
-// the same logic, which does not seem to happen, even though
-// the logic is plainly there to do that.
+{ // BEGIN ANONYMOUS NAMESPACE
 
-// TODO: removes should happen only once on the shallowest
-// parent. If this were called on a live doc we would be causing
-// several unecessary reflows. For example, in the case of
-// <div><p></p><p></p></div>, there are 3 remove operations,
-// when only 1 needed to occur. To do this, this needs
-// to be fundamentally refactored. Removes should not occur
-// on the first pass over the elements. This, btw, would remove the
-// ugliness of using a map function with a side effect. Instead, start by
-// identifying all of the empty leaves. Then, for each leaf, traverse
-// upwards to find the actual element to remove. Be cautious
-// about simply checking that parent.childElementCount == 1 to find
-// a removable parent because it is false in the case that two
-// or more empty-leaves share the same parent. The criteria instead is
-// that a parent is removable if all of its children are removable.
-// So we need to go up 1, then query all direct children. But that is
-// kind of redundant since we already identified the children, so that
-// still might need improvement.
+// These element names are never considered leaves
+const exceptions = new Set([
+	'area',
+	'audio',
+	'br',
+	'canvas',
+	'col',
+	'hr',
+	'iframe',
+	'img',
+	'source',
+	'svg',
+	'track',
+	'video'
+]);
 
-// TODO: just add children that should be removed to the stack insead of
-// removing them and adding their parents to the stack.
-// Remove all the empty children and shove all the parents on the stack
+// Elements containing only these text node values are still leaves
+const trivialTexts = new Set([
+	'',
+	'\n',
+	'\n\t',
+	'\n\t\t',
+	'\n\t\t\t'
+]);
 
-// TODO: filtering leaves, singletons, and trimming probably
-// all has to occur together?
+// Prunes leaf elements from the document. Leaf elements are those
+// elements that do not contain sub elements, such as <p></p>, or elements
+// that only contain other leaf-like elements but are not leaf-like, such as
+// the outer paragraph in <p id="outer"><p id="nested-inner"></p></p>.
+// The document element (e.g. <html></html>) and the document body are never
+// considered leaves.
+// Certain elements are treated differently. For example, <img> is never
+// considered a leaf even though it has no nested elements or text.
+// Elements that contain only trivial text nodes are still considered leaves,
+// such as <p>\n</p>
+
+// TODO: this could still use improvement. it is revisiting and
+// re-evaluating children sometimes.
+// TODO: i would like to do this without recursion for better perf
+// TODO: does the resulting set of leaves contain leaves within
+// leaves? i want to avoid removing leaves within leaves.
+// TODO: test cases
+// TODO: i would like to do this without having a visitor function and
+// an isLeaf function that also visits, it feels wrong.
+// TODO: if we treat the document as a DAG, we can use graph principles,
+// and process the document as if it were a graph. maybe we need a graph
+// library.
 
 function filterLeafElements(document) {
-	'use strict';
-
-	const selector = [
-		'area',
-		'audio',
-		'br',
-		'canvas',
-		'col',
-		'hr',
-		'img',
-		'source',
-		'svg',
-		'track',
-		'video'
-	].join(',');
-
-	const elements = document.documentElement.getElementsByTagName('*');
-	const numElements = elements.length;
-
-	// if leaves is let or const declared array,
-	// chrome sometimes deopts with reason
-	// Unsupported phi use const variable
-	// https://v8.googlecode.com/svn/trunk/src/hydrogen.cc
-
 	const leaves = new Set();
 
-	for(let i = 0, element = null; i < numElements; i++) {
-		element = elements[i];
-		if(!element.firstChild && !element.matches(selector)) {
-			leaves.add(element);
-		}
-	}
+	visit(leaves,
+		document.body,
+		document.documentElement);
 
-	const body = document.body;
-	const documentElement = document.documentElement;
-	const stack = [];
-
-	var leafParent = null;
 	for(let leaf of leaves) {
-		leafParent = leaf.parentElement;
-		if(leafParent !== body && leafParent !== documentElement) {
-			stack.push(leaf.parentElement);
-		}
+		// console.debug('Leaf: ', leaf.outerHTML);
 		leaf.remove();
 	}
+}
 
-	let parent = null;
-	let grandParent = null;
+this.filterLeafElements = filterLeafElements;
 
-	while(stack.length) {
-		parent = stack.pop();
-
-		if(parent.firstChild) {
-			// There are other nodes in the parent after the child was removed,
-			// so do not remove the parent.
-			continue;
+function visit(leaves, bodyElement, element) {
+	const childNodes = element.childNodes;
+	const childNodeCount = childNodes.length;
+	for(let i = 0, cursor; i < childNodeCount; i++) {
+		cursor = childNodes[i];
+		if(cursor.nodeType === Node.ELEMENT_NODE) {
+			if(isLeaf(bodyElement, cursor)) {
+				leaves.add(cursor);
+			} else {
+				visit(leaves, bodyElement, cursor);
+			}
 		}
-
-		// Grab a reference to the grand parent before removal
-		// because after removal it is undefined
-		grandParent = parent.parentElement;
-
-		parent.remove();
-
-		// If there was no grand parent (how would that ever happen?)
-		// or the grand parent is the root, then do not add the new
-		// grand parent to the stack
-		if(!grandParent || grandParent === body ||
-			grandParent === documentElement) {
-			continue;
-		}
-
-		stack.push(grandParent);
 	}
 }
+
+function isLeaf(bodyElement, element) {
+	if(element === bodyElement)
+		return false;
+	if(exceptions.has(element.localName))
+		return false;
+
+	const childNodes = element.childNodes;
+	const childCount = childNodes.length;
+	for(let i = 0, child; i < childCount; i++) {
+		child = childNodes[i];
+		if(child.nodeType === Node.TEXT_NODE) {
+			if(!trivialTexts.has(child.nodeValue)) {
+				return false;
+			}
+		} else if(child.nodeType === Node.ELEMENT_NODE) {
+			if(!isLeaf(bodyElement, child)) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+} // END ANONYMOUS NAMESPACE
