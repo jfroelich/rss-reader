5a29338f56fb36cd5d8c4d8e48e24e7fb4dba2b4
Josh <joshua.froelich@gmail.com>
2015-12-10 12:20:38 -0500

    Minor style changes to analyze-document.js
    Move constants upward, use an if statement for finding bodyElement
    instead of returning early.

--- src/calamine/analyze-document.js
+++ src/calamine/analyze-document.js
@@ -6,69 +6,6 @@
 
 { // BEGIN ANONYMOUS NAMESPACE
 
-// TODO: use for..of destructuring when supported
-// TODO: refactor all extractors for body to just look for
-// best body, not to analyze bp, and move
-// TODO: split into two functions, one to find body,
-// and one to do second pass to class bp
-
-// Creates a boilerplate filtering function
-function analyzeDocument(document) {
-
-  const results = new CalamineResults();
-  results.document = document;
-
-  if(!document.querySelector('body')) {
-    results.bodyElement = null;
-    return results;
-  }
-
-  // TODO: rename classifyBoilerplate
-  //results.bodyElement = fastFindBodyElement(document);
-
-  // Looks for obvious best elements based on known content signatures
-  for(let i = 0, elements = null; i < NUM_SIGNATURES; i++) {
-    elements = document.body.querySelectorAll(BODY_SIGNATURES[i]);
-    if(elements.length === 1) {
-      results.bodyElement = elements[0];
-      break;
-    }
-  }
-
-  if(results.bodyElement) {
-    results.boilerplateElements = classifyBoilerplate(results.bodyElement);
-    return results;
-  }
-
-  results.textLengths = deriveTextLength(document);
-  results.anchorLengths = deriveAnchorLength(document, results.textLengths);
-  results.textScores = analyzeText(document, results.textLengths,
-    results.anchorLengths);
-  results.typeScores = analyzeTypes(document);
-  results.topologyScores = analyzeTopology(document);
-  results.imageParentScores = analyzeImages(document);
-  results.attributeScores = analyzeAttributes(document);
-  results.microdataScores = analyzeMicrodata(document);
-  results.updateElementScores();
-
-  // Set bodyElement to element with highest score
-  results.bodyElement = document.body;
-  let bestScore = results.scores.get(results.bodyElement);
-  for(let entry of results.scores) {
-    if(entry[1] > bestScore) {
-      results.bodyElement = entry[0];
-      bestScore = entry[1];
-    }
-  }
-
-  // TODO: rename classifyBoilerplate
-  results.boilerplateElements = classifyBoilerplate(results.bodyElement);
-  return results;
-}
-
-// Export global
-this.analyzeDocument = analyzeDocument;
-
 const BODY_SIGNATURES = [
   'article',
   '.hentry',
@@ -85,12 +22,12 @@ const BODY_SIGNATURES = [
   '[itemprop="articleBody"]',
   '[role="article"]',
   '[itemtype="http://schema.org/Article"]',
-  '[itemtype="http://schema.org/NewsArticle"]',
   '[itemtype="http://schema.org/BlogPosting"]',
   '[itemtype="http://schema.org/Blog"]',
-  '[itemtype="http://schema.org/WebPage"]',
+  '[itemtype="http://schema.org/NewsArticle"]',
   '[itemtype="http://schema.org/TechArticle"]',
   '[itemtype="http://schema.org/ScholarlyArticle"]',
+  '[itemtype="http://schema.org/WebPage"]',
   '#WNStoryBody',
 
   // todo: verify this one
@@ -98,5 +35,57 @@ const BODY_SIGNATURES = [
 ];
 
 const NUM_SIGNATURES = BODY_SIGNATURES.length;
+
+// Analyzes the document and returns a CalamineResults object
+function analyzeDocument(document) {
+
+  const results = new CalamineResults();
+  results.document = document;
+
+  if(!document.querySelector('body')) {
+    results.bodyElement = null;
+    return results;
+  }
+
+  // Looks for obvious best elements based on known content signatures
+  for(let i = 0, elements = null; i < NUM_SIGNATURES; i++) {
+    elements = document.body.querySelectorAll(BODY_SIGNATURES[i]);
+    if(elements.length === 1) {
+      results.bodyElement = elements[0];
+      break;
+    }
+  }
+
+  // If we didn't find an obvious body signature, do a full analysis
+  if(!results.bodyElement) {
+    results.bodyElement = document.body;
+    results.textLengths = deriveTextLength(document);
+    results.anchorLengths = deriveAnchorLength(document, results.textLengths);
+    results.textScores = analyzeText(document, results.textLengths,
+      results.anchorLengths);
+    results.typeScores = analyzeTypes(document);
+    results.topologyScores = analyzeTopology(document);
+    results.imageParentScores = analyzeImages(document);
+    results.attributeScores = analyzeAttributes(document);
+    results.updateElementScores();
+
+    // Set bodyElement to element with highest score
+    // TODO: use for..of destructuring when supported
+    let bestScore = results.scores.get(results.bodyElement);
+    for(let entry of results.scores) {
+      if(entry[1] > bestScore) {
+        results.bodyElement = entry[0];
+        bestScore = entry[1];
+      }
+    }
+  }
+
+  // TODO: rename classifyBoilerplate to something clearer
+  results.boilerplateElements = classifyBoilerplate(results.bodyElement);
+  return results;
+}
+
+// Export global
+this.analyzeDocument = analyzeDocument;
 
 } // END ANONYMOUS NAMESPACE
