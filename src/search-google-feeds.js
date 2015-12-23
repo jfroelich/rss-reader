c55bad6e688796736a37995037a84f5b0a0ba772
Josh <joshua.froelich@gmail.com>
2015-12-22 21:29:15 -0500

    Renamed google-feeds to search-google-feeds.js
    Removed the GoogleFeeds namespace, now this just provides a simple
    global function. Also changed some comments.

--- src/search-google-feeds.js
+++ src/search-google-feeds.js
@@ -2,15 +2,9 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
-// TODO: the google feeds api is now fully deprecated. remove the ability to
-// search by query from the options UI and delete this?
-// NOTE: actually, hold on, this still occassionally works
+// TODO: explicit dependency on StringUtils
 
 'use strict';
-
-// Lib for using the Google Feeds API
-// TODO: explicit dependency on StringUtils
-const GoogleFeeds = {};
 
 { // BEGIN ANONYMOUS NAMESPACE
 
@@ -18,9 +12,19 @@ const GoogleFeeds = {};
 const BASE_URL =
   'https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q=';
 
-// Sends a request to Google to search for feeds that correspond to
-// a general text query. Passes the results to the callback.
-GoogleFeeds.findFeed = function(query, timeout, callback) {
+// Sends an async request to Google to search for feeds that correspond to
+// a general text query. Passes the results to the callback. The callback
+// is passed an error argument, the query argument (as modified by Google),
+// and an array of results. The error argument is only defined if an error
+// occurred. If an error occurred, the other arguments may be undefined or null.
+// The results array contains result objects called entries. Each entry is a
+// basic js object containing the string properties url, link, title, and
+// contentSnippet. The title and content snippet may contain basic HTML such as
+// <b></b> around terms that were present in the query.
+// NOTE: Google formally deprecated this service. Around December 1st, 2015, I
+// first noticed that the queries stopped working. However, I have witnessed
+// the service occassionally work thereafter.
+function searchGoogleFeeds(query, timeout, callback) {
   const request = new XMLHttpRequest();
   request.timeout = timeout;
   request.onerror = callback;
@@ -31,7 +35,10 @@ GoogleFeeds.findFeed = function(query, timeout, callback) {
   request.open('GET', url, true);
   request.responseType = 'json';
   request.send();
-};
+}
+
+// Export a global
+this.searchGoogleFeeds = searchGoogleFeeds;
 
 // Cleans up the search results and passes them along
 // to the callback
