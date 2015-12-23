96c26a1b7abaeed40fcb2957fbc2cad066f759fc
Josh <joshua.froelich@gmail.com>
2015-12-22 21:27:54 -0500

    Improvements to fetch-html.js
    Cleaned up some comments, made the arguments to the callback more
    consistent, minor code style changes

--- src/fetch-html.js
+++ src/fetch-html.js
@@ -2,34 +2,62 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
+'use strict';
+
+{ // BEGIN ANONYMOUS NAMESPACE
+
 // Fetches an HTML document (async) and passes the callback an error, the
-// document, and the response url string.
-// TODO: unify the type of error. The native callbacks are events, but the
-// undefined document creates an Error object.
-// TODO: don't nest the function definition
-
+// document object, and the response url string. The error argument is falsy
+// when no error occurred.
 function fetchHTML(url, timeout, callback) {
-  'use strict';
-
   const request = new XMLHttpRequest();
   request.timeout = timeout;
-  request.ontimeout = callback;
-  request.onerror = callback;
-  request.onabort = callback;
 
-  request.onload = function _onload(event) {
-    const request = event.target;
-    const document = request.responseXML;
+  // TODO: can I unify onFetchHTML and onFetchError, and have the single
+  // function check something like event.type to determine whether it is
+  // an error callback or a success callback?
 
-    if(!document || !document.documentElement) {
-      callback(new Error('Undefined document'), document, request.responseURL);
-      return;
-    }
+  // TODO: is onerror a catch-all event that still is triggered when an abort
+  // or a timeout occurs (and is that exclusive or not)?
 
-    callback(null, document, request.responseURL);
-  };
+  const errorCallback = onFetchError.bind(request, url, callback);
+  request.ontimeout = errorCallback;
+  request.onerror = errorCallback;
+  request.onabort = errorCallback;
 
+  request.onload = onFetchHTML.bind(request, url, callback);
   request.open('GET', url, true);
+
+  // TODO: what is the default behavior? If responseType defaults
+  // to document and by default fetches HTML, do we even need to specify
+  // the type here?
+
   request.responseType = 'document';
   request.send();
 }
+
+this.fetchHTML = fetchHTML;
+
+function onFetchError(url, callback, event) {
+  const request = event.target;
+  callback(event, request.responseXML, request.responseURL);
+}
+
+function onFetchHTML(url, callback, event) {
+  const request = event.target;
+  const document = request.responseXML;
+  let error = null;
+
+  if(!document || !document.documentElement) {
+
+    // TODO: instead of creating an error object, this should create an event
+    // object so that it is minimally consistent with the other types of the
+    // first argument when the callback is called due to another type of error
+
+    error = new Error('Undefined document or document element for url ' + url);
+  }
+
+  callback(error, document, request.responseURL);
+}
+
+} // END ANONYMOUS NAMESPACE
