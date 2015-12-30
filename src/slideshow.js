ad8a04887c004bbc9165cc9eb81aa88cdf8b77d4
Josh <joshua.froelich@gmail.com>
2015-12-30 15:56:16 -0500

    Moved scrollElementTo into its own file
    Part of a series of changes of cleaning up slideshow.js

--- src/slideshow.js
+++ src/slideshow.js
@@ -279,8 +279,6 @@ function appendSlide(entry, isFirst) {
   document.getElementById('slideshow-container').appendChild(slide);
 }
 
-
-
 function showNextSlide() {
   if(countUnreadSlides() < 2) {
     appendSlides(function() {
@@ -397,57 +395,6 @@ function onKeyDown(event) {
 
 // this === window
 this.addEventListener('keydown', onKeyDown, false);
-
-
-// TODO: move scrollElementTo back into its own file
-// TODO: this should probably use an even more qualified name like
-// scrollElementToY, given that it does not handle horizontal movement
-// Or maybe name it verticalScrollElement
-// Or write a general scrollElementTo that supports both x and y, then
-// just use 0 for x.
-
-/**
- * NOTE: the start timer is basically to debounce calls to this function
- * whereas the interval timer is to track the interval and stop it when
- * finished
- * @param element {Element} the element to scroll
- * @param delta {int} the amount of pixels by which to scroll per increment
- * @param targetY {int} the desired vertical end position
- */
-function scrollElementTo(element, delta, targetY) {
-  let scrollYStartTimer;
-  let scrollYIntervalTimer;
-  let amountToScroll = 0;
-  let amountScrolled = 0;
-
-  return function() {
-    clearTimeout(scrollYStartTimer);
-    clearInterval(scrollYIntervalTimer);
-    scrollYStartTimer = setTimeout(start,5);
-  }();
-
-  function start() {
-    amountToScroll = Math.abs(targetY - element.scrollTop);
-    amountScrolled = 0;
-
-    if(amountToScroll === 0) {
-      return;
-    }
-
-    scrollYIntervalTimer = setInterval(scrollToY,20);
-  }
-
-  function scrollToY() {
-    const currentY = element.scrollTop;
-    element.scrollTop += delta;
-    amountScrolled += Math.abs(delta);
-
-    // If there was no change or we scrolled too far, then we are done.
-    if(currentY === element.scrollTop || amountScrolled >= amountToScroll) {
-      clearInterval(scrollYIntervalTimer);
-    }
-  }
-}
 
 function initSlideShow(event) {
   document.removeEventListener('DOMContentLoaded', initSlideShow);
