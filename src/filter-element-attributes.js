bd5d215f930f1f64f654a037b172c27720c463b8
Josh <joshua.froelich@gmail.com>
2016-01-01 22:51:21 -0500

    Added minor comment on responsive design to filterElementAttributes

--- src/filter-element-attributes.js
+++ src/filter-element-attributes.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
+// TODO: responsive design allows images to have a srcset, maybe permit
+
 // Removes attributes from elements in the document, except for href/src
 function filterElementAttributes(document) {
   'use strict';
