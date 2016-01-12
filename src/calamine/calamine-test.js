20f73aaf88826317a88033a0037942f49e38105e
Josh <joshua.froelich@gmail.com>
2016-01-12 14:54:10 -0500

    Changed copyright notice to 2016

--- src/calamine/calamine-test.js
+++ src/calamine/calamine-test.js
@@ -1,5 +1,7 @@
+// Copyright 2016 Josh Froelich. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file
 
-// todo: rename to calamine-test.js
 // todo: look into using a custom devtools panel like domdistiller
 
 'use strict';
