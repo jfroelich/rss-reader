135404b98b26b04d4d7636cabbfedd93617bf724
Josh <joshua.froelich@gmail.com>
2016-01-13 10:58:49 -0500

    Minor comment changes

--- src/html/parse-html.js
+++ src/html/parse-html.js
@@ -8,22 +8,22 @@
 // does the exact same thing. one issue though is whether domparser sets
 // the body inner html or the documentelement innerhtml. this would be something
 // to review.
+// NOTE: we do not need to check whether the html variable is defined because
+// setting the innerHTML property to undefined has no effect.
+// NOTE: doc does not have an innerHTML property, so we have to use
+// documentElement. Setting doc.innerHTML is basically only defining a new,
+// useless, expando property on the document instance.
 // TODO: review whether this throws an exception or does the funky embedded
 // parsererror element like what happens when parsing invalid xml
 // TODO: all functionality that involves parsing html in the app should be
 // using this function. I think there are a few places in other files that do
 // not use this.
+// TODO: if the document element has only one child and it is <html>, the
+// child should be unwrapped
 
 function parseHTML(html) {
   'use strict';
   const doc = document.implementation.createHTMLDocument();
-
-  // NOTE: we do not need to check whether the html variable is defined because
-  // setting the innerHTML property to undefined has no effect.
-  // NOTE: doc does not have an innerHTML property, so we have to use
-  // documentElement. Setting doc.innerHTML is basically only defining a new,
-  // useless, expando property on the document instance.
   doc.documentElement.innerHTML = html;
-
   return doc;
 }
--- src/opml/parse-opml.js
+++ src/opml/parse-opml.js
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
+// Requires: /xml/parse-xml.js
+
 // Parses an opml xml document string into a document. Throws an exception
 // if an error occurs.
-// Requires: parseXML
 function parseOPML(string) {
   'use strict';
 
@@ -12,6 +13,7 @@ function parseOPML(string) {
   const document = parseXML(string);
 
   if(!document.documentElement.matches('opml')) {
+    // TODO: use the new string template syntax?
     throw new Error('Invalid document element: ' +
       document.documentElement.localName);
   }
