be5d997a4deb74475de705debed3a91dc8fa9ead
Josh <joshua.froelich@gmail.com>
2016-03-25 10:51:15 -0400

    Restyling of implementation of resolve-urls.js

--- src/resolve-urls.js
+++ src/resolve-urls.js
@@ -2,84 +2,112 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file
 
+// TODO: do some additional testing after recent changes, i didn't see any
+// images appear in the output
+
 // TODO: resolve xlink type simple (on any attribute)
+// TODO: finish implementing serializeSrcSet
+// TODO: rather than do a separate check for srcset, it should somehow be
+// in the main map. but then that means we can't use a map because we
+// have dup keys. so maybe the map needs to instead contain arrays of
+// attribute names (similarly, support longdesc in iframe)
+// TODO: look into whether srcset can be anywhere or just in img/source els
+
+// Requires: /src/utils.js
 
 (function(exports) {
 'use strict';
 
-const ELEMENT_URL_ATTRIBUTE_MAP = new Map([
-  ['a', 'href'],
-  ['applet', 'codebase'],
-  ['area', 'href'],
-  ['audio', 'src'],
-  ['base', 'href'],
-  ['blockquote', 'cite'],
-  ['body', 'background'],
-  ['button', 'formaction'],
-  ['del', 'cite'],
-  ['embed', 'src'],
-  ['frame', 'src'],
-  ['head', 'profile'],
-  ['html', 'manifest'],
-  ['iframe', 'src'],
-  ['form', 'action'],
-  ['img', 'src'],
-  ['input', 'src'],
-  ['ins', 'cite'],
-  ['link', 'href'],
-  ['object', 'data'],
-  ['q', 'cite'],
-  ['script', 'src'],
-  ['source', 'src'],
-  ['track', 'src'],
-  ['video', 'src']
-]);
-
-const RESOLVE_SELECTOR = function() {
-  let keys = [];
-  ELEMENT_URL_ATTRIBUTE_MAP.forEach(function(value, key) {
-    keys.push(key + '[' + value +']');
-  });
-  return keys.join(',');
-}();
-
 function resolveURLs(document, baseURL) {
-  const forEach = Array.prototype.forEach;
-  const baseElementList = document.querySelectorAll('base');
-  const removeBaseElement = function(baseElement) {
-    baseElement.remove();
-  };
-  forEach.call(baseElementList, removeBaseElement);
-
-  const getNameOfAttributeWithURL = function(element) {
-    return ELEMENT_URL_ATTRIBUTE_MAP.get(element.localName);
-  };
-
-  const resolvables = document.querySelectorAll(RESOLVE_SELECTOR);
-  forEach.call(resolvables, function(element) {
-    const attribute = getNameOfAttributeWithURL(element);
-    const url = element.getAttribute(attribute).trim();
-    const resolved = utils.resolveURL(baseURL, url);
-    if(resolved && resolved !== url) {
-      element.setAttribute(attribute, resolved);
-    }
-
-    if((element.localName === 'img' || element.localName === 'source') &&
-      element.hasAttribute('srcset')) {
-      resolveImageSrcSet(baseURL, element);
-    }
-  });
+  removeBaseElements(document);
+  modifyResolvableAttributes(document, baseURL);
 }
 
-// Access an image element's srcset attribute, parses it into an array of
+exports.resolveURLs = resolveURLs;
+
+// Remove base. base is blacklisted in sanitize-document.js but the integrity
+// of this module if used independently requires this behavior.
+function removeBaseElements(document) {
+  for(let bases = document.querySelectorAll('base'), i = 0,
+    len = bases.length; i < len; i++) {
+    bases[i].remove();
+  }
+}
+
+function modifyResolvableAttributes(document, baseURL) {
+  const elements = document.querySelectorAll(RESOLVE_SELECTOR);
+  const resolveURL = utils.resolveURL;
+  for(let i = 0, len = elements.length, element, attribute, originalURL,
+    resolvedURL; i < len; i++) {
+    element = elements[i];
+    attribute = getURLAttribute(element);
+    originalURL = element.getAttribute(attribute).trim();
+    resolvedURL = resolveURL(baseURL, originalURL);
+    if(resolvedURL && resolvedURL !== originalURL) {
+      element.setAttribute(attribute, resolvedURL);
+    }
+
+    // Resolve srcsets
+    if((element.localName === 'img' || element.localName === 'source') &&
+      element.hasAttribute('srcset')) {
+      resolveSrcSetAttribute(baseURL, element);
+    }
+  }
+}
+
+const URL_ATTRIBUTE = {
+  'a': 'href',
+  'applet': 'codebase',
+  'area': 'href',
+  'audio': 'src',
+  // base elements are always removed, but i am leaving this in here
+  // for completeness
+  'base': 'href',
+  'blockquote': 'cite',
+  'body': 'background',
+  'button': 'formaction',
+  'del': 'cite',
+  'embed': 'src',
+  'frame': 'src',
+  'head': 'profile',
+  'html': 'manifest',
+  'iframe': 'src',
+  'form': 'action',
+  'img': 'src',
+  'input': 'src',
+  'ins': 'cite',
+  'link': 'href',
+  'object': 'data',
+  'q': 'cite',
+  'script': 'src',
+  'source': 'src',
+  'track': 'src',
+  'video': 'src'
+};
+
+const RESOLVE_SELECTOR = Object.keys(URL_ATTRIBUTE).map(function(key) {
+  return key + '[' + URL_ATTRIBUTE[key] +']';
+}).join(',');
+
+console.debug(RESOLVE_SELECTOR);
+
+function getURLAttribute(element) {
+  return URL_ATTRIBUTE[element.localName];
+}
+
+function removeElement(element) {
+  element.remove();
+}
+
+// Access an element's srcset attribute, parses it into an array of
 // descriptors, resolves the url for each descriptor, and then composes the
 // descriptors array back into a string and modifies the element
-function resolveImageSrcSet(baseURL, image) {
+function resolveSrcSetAttribute(baseURL, element) {
 
-  const source = image.getAttribute('srcset');
+  const source = element.getAttribute('srcset');
   let descriptors = parseSrcset(source) || [];
   let numURLsChanged = 0;
-  let resolvedDescriptors = descriptors.map(function(descriptor) {
+  let resolvedDescriptors = descriptors.map(function transform(descriptor) {
     const resolvedURL = utils.resolveURL(baseURL, descriptor.url);
     let newURL = descriptor.url;
     if(resolvedURL && resolvedURL !== descriptor.url) {
@@ -98,7 +126,7 @@ function resolveImageSrcSet(baseURL, image) {
 
   const newSource = serializeSrcSet(resolvedDescriptors);
   console.debug('Changing srcset %s to %s', source, newSource);
-  image.setAttribute('srcset', newSource);
+  element.setAttribute('srcset', newSource);
 }
 
 // Returns a string representing serialized descriptors, which is a suitable
@@ -106,7 +134,6 @@ function resolveImageSrcSet(baseURL, image) {
 // TODO: THIS IS INCOMPLETE, because I do not yet include the other dimensions
 // back into the string, and I am getting image errors in the output
 // TODO: support d,w,h
-// TODO: do a no-op if the urls were already absolute
 function serializeSrcSet(descriptors) {
 
   const resolvedDescriptors = [];
@@ -132,9 +159,7 @@ function serializeSrcSet(descriptors) {
   }
 
   // i believe a comma is what joins? have not researched
-  return resolvedDescriptors.join(', ');
+  return resolvedDescriptors.join(',');
 }
-
-exports.resolveURLs = resolveURLs;
 
 }(this));
