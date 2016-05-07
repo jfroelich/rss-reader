fcc1ab4fc9667ac5110fb4a7f4fbcfae5f9efd67
Josh <joshua.froelich@gmail.com>
2016-05-07 00:09:15 -0400

    Cleaned up notrack.js a bit
    Made the code more terse and simple, added fallback to documentElement
    as root, changed some comments

--- src/notrack.js
+++ src/notrack.js
@@ -4,44 +4,31 @@
 
 'use strict';
 
-// Lib for helping reduce the number of tracking elements in an HTML
-// document.
-
-// TODO: I am not seeing any of the last 4 urls here being filtered. Maybe
-// I am looking for the wrong thing.
-
-const NO_TRACK_IMAGE_SRC_URLS = [
-  'http://b.scorecardresearch.com',
-  'https://b.scorecardresearch.com',
-  'http://pagead2.googlesyndication.com',
-  'https://pagead2.googlesyndication.com',
-  'http://pubads.g.doubleclick.net',
-  'https://pubads.g.doubleclick.net'
-];
-
-function no_track_make_image_selector_part(urlString) {
-  return 'IMG[src^="' + urlString + '"]';
-}
-
-const NO_TRACK_IMAGE_SELECTOR_PARTS = NO_TRACK_IMAGE_SRC_URLS.map(
-  no_track_make_image_selector_part);
-const NO_TRACK_IMAGE_SELECTOR = NO_TRACK_IMAGE_SELECTOR_PARTS.join(',');
-
 // Remove elements with unwanted urls
 // This is separate from sanity functions primarily because of when this is
 // called. This is done before doing image dimension fetches. Otherwise the
 // pings would still occur.
+// TODO: this needs a lot of improvement
+// TODO: I am not seeing any of the last 4 urls here being filtered. Maybe
+// I am looking for the wrong thing.
 function no_track_filter_elements(document) {
+
+  const NO_TRACK_IMAGE_SELECTORS = [
+    'img[src^="http://b.scorecardresearch.com"]',
+    'img[src^="https://b.scorecardresearch.com"]',
+    'img[src^="http://pagead2.googlesyndication.com"]',
+    'img[src^="https://pagead2.googlesyndication.com"]',
+    'img[src^="http://pubads.g.doubleclick.net"]',
+    'img[src^="https://pubads.g.doubleclick.net"]'
+  ];
+
+  const NO_TRACK_IMAGE_SELECTOR = NO_TRACK_IMAGE_SELECTORS.join(',');
+
   // I only look for tracking elements within the body, because I assume
   // that out-of-body information is removed separately in the sanity
   // functions.
-
-  const bodyElement = document.body;
-  if(!bodyElement) {
-    return;
-  }
-
-  const imageNodeList = bodyElement.querySelectorAll(NO_TRACK_IMAGE_SELECTOR);
+  const rootElement = document.body || document.documentElement;
+  const imageNodeList = rootElement.querySelectorAll(NO_TRACK_IMAGE_SELECTOR);
   const listLength = imageNodeList.length;
   for(let i = 0, imageElement; i < listLength; i++) {
     imageElement = imageNodeList[i];
