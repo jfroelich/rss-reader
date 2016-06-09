b08ba9a5a078d1bef41b53f72d28296332462a5a
Josh <joshua.froelich@gmail.com>
2016-06-09 10:49:21 -0400

    Revised handling of lazily loaded images
    Accomplished several of the TODOs for transformLazilyLoadedImages,
    revised the function, reduced the function’s DRYness

--- src/poll.js
+++ src/poll.js
@@ -420,7 +420,6 @@ FeedPoller.filterTrackingImages = function(document) {
 };
 
 // Modify the src values of images that appear to be lazily loaded.
-// TODO: maybe skip an image if image.closest('picture') ?
 FeedPoller.transformLazilyLoadedImages = function(document) {
   const rootElement = document.body || document.documentElement;
   if(!rootElement) {
@@ -434,60 +433,39 @@ FeedPoller.transformLazilyLoadedImages = function(document) {
   }
 };
 
-// TODO: reduce the DRYness of this function
-// TODO: support this case better. There is a problem here because
-// the tiny image filter is picking this up and removing it.
-// <img data-thumb="url" data-full-size-image="url" data-lar
-// ge-size-image="url" data-trigger-notification="1" data-scalable="fa
-// lse" alt="" data-src="url" data-tc-lazyload="deferred" src="url" width=
-// "1" height="1">
 FeedPoller.transformLazilyLoadedImage = function(image) {
-  if(!image.hasAttribute('src') && image.hasAttribute('load-src')) {
-    image.setAttribute('src', image.getAttribute('load-src'));
+  const src = (image.getAttribute('src') || '').trim();
+  if(src) {
     return;
   }
 
-  if(image.hasAttribute('data-src') &&
-    image.classList.contains('lazy-image')) {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    return;
-  }
+  const NAMES = [
+    'load-src',
+    'data-src',
+    'data-original-desktop',
+    'data-baseurl',
+    'data-lazy',
+    'data-img-src',
+    'data-original',
+    'data-adaptive-img'
+  ];
 
-  if(!image.hasAttribute('src') && image.hasAttribute('data-src')) {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    return;
-  }
+  let before = '';
 
-  // TODO: responsive design conflicts with the approach this takes,
-  // this needs to be handled instead by the srcset handler?
-  if(!image.hasAttribute('src') &&
-    image.hasAttribute('data-original-desktop')) {
-    image.setAttribute('src', image.getAttribute('data-original-desktop'));
-    return;
-  }
-
-  if(!image.hasAttribute('src') && image.hasAttribute('data-baseurl')) {
-    image.setAttribute('src', image.getAttribute('data-baseurl'));
-    return;
-  }
-
-  if(!image.hasAttribute('src') && image.hasAttribute('data-lazy')) {
-    image.setAttribute('src', image.getAttribute('data-lazy'));
-    return;
-  }
-
-  if(!image.hasAttribute('src') && image.hasAttribute('data-img-src')) {
-    image.setAttribute('src', image.getAttribute('data-img-src'));
-    return;
-  }
-
-  if(!image.hasAttribute('src') && image.hasAttribute('data-original')) {
-    image.setAttribute('src', image.getAttribute('data-original'));
-    return;
-  }
-
-  if(!image.hasAttribute('src') && image.hasAttribute('data-adaptive-img')) {
-    image.setAttribute('src', image.getAttribute('data-adaptive-img'));
-    return;
+  const numNames = NAMES.length;
+  for(let i = 0, name, altSrc; i < numNames; i++) {
+    name = NAMES[i];
+    if(image.hasAttribute(name)) {
+      altSrc = image.getAttribute(name);
+      altSrc = altSrc || '';
+      altSrc = altSrc.trim();
+      if(altSrc && /:\/\//.test(altSrc)) {
+        before = image.outerHTML;
+        image.removeAttribute(name);
+        image.setAttribute('src', altSrc);
+        // console.debug('Transformed', before, 'to', image.outerHTML);
+        break;
+      }
+    }
   }
 };
