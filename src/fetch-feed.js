abd194a7b2b62604f5973c90d4fe8945130a8c4d
Josh <joshua.froelich@gmail.com>
2016-06-10 18:17:19 -0400

    Minor cleanup of fetchFeed.js
    Cleaned up comments, renamed some variables, reordered some of the
    statements

--- src/fetch-feed.js
+++ src/fetch-feed.js
@@ -4,39 +4,21 @@
 
 'use strict';
 
-// NOTE: this does not assume url is defined? It just fails in that case. The
-// thing is, does it fail by calling request.onerror or does it cause a
-// javascript error? If it is a js error then maybe I need to guard.
-// TODO: I think this should always just pass back event and let caller test
-// against it, instead the whole only-defined-event-if-error thing
-// TODO: the type of error passed back as first argument to the final callback
-// should be consistent. Perhaps should mimic an event object and use that
-// in all cases
-function fetchFeed(urlString, timeout, callback) {
+// Fetches the XML of a feed and parses it into a feed object
+// TODO: maybe require a URL object as a parameter instead of a string
+function fetchFeed(urlString, timeoutMillis, callback) {
   const request = new XMLHttpRequest();
-  request.timeout = timeout;
+  request.timeout = timeoutMillis;
   request.onerror = onError;
   request.ontimeout = onError;
   request.onabort = onError;
   request.onload = onLoad;
-  request.open('GET', urlString, true);
+  const isAsyncRequest = true;
+  request.open('GET', urlString, isAsyncRequest);
   request.responseType = 'document';
   request.send();
 
-  function onError(event) {
-    const fetchEvent = {
-      'type': event.type,
-      'feed': null,
-      'requestURLString': urlString,
-      'responseURLString': event.target.responseURL
-    };
-
-    callback(fetchEvent);
-  }
-
   function onLoad(event) {
-    const document = event.target.responseXML;
-
     const fetchEvent = {
       'type': event.type,
       'feed': null,
@@ -44,10 +26,12 @@ function fetchFeed(urlString, timeout, callback) {
       'responseURLString': event.target.responseURL
     };
 
-    // This can happen, for example, when fetching a PDF.
+    // Document may be undefined when trying to fetch something that is not
+    // document, such as PDF file.
     // TODO: would document or documentElement ever actually be undefined?
     // If an error occurs, wouldn't that mean that onLoad doesn't even
     // get called? I need to look into this more.
+    const document = event.target.responseXML;
     if(!document || !document.documentElement) {
       fetchEvent.type = 'invaliddocument';
       callback(fetchEvent);
@@ -71,6 +55,16 @@ function fetchFeed(urlString, timeout, callback) {
       fetchEvent.details = exception;
     }
 
+    callback(fetchEvent);
+  }
+
+  function onError(event) {
+    const fetchEvent = {
+      'type': event.type,
+      'feed': null,
+      'requestURLString': urlString,
+      'responseURLString': event.target.responseURL
+    };
     callback(fetchEvent);
   }
 }
