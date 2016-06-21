c98cd8deca35f3dd0145d483ce13704a3cced4bf
Josh <joshua.froelich@gmail.com>
2016-06-21 08:23:22 -0400

    Moved archival out of background
    background wasn’t coherent enough

---
+++ src/archive-entries.js
@@ -0,0 +1,103 @@
+// Copyright 2016 Josh Froelich. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file
+
+'use strict';
+
+// TODO: I would prefer not to hardcode expiration period, I would prefer it
+// was defined externally, somewhere else
+// TODO: i have a more general idea about dealing with the situation where I
+// subscribe to a lot of feeds and then do not bother to read everything and
+// so the unread count starts to pile up. I could phase out various entries
+// over time. Basically you would have a window in which to read entries, and
+// after some point if the entry is still unread, it will never be read because
+// it is probably out dated or no longer of interest, at which point it makes
+// sense to remove it. So maybe I also want to check for unread entries that
+// are very old and do something to them. From this perspective maybe it does
+// make sense to use two flags (archiveState and readState) instead of one
+// flag, because this way I can differentiate entries that were never read
+// from those that were read once those entries are archived. Also, I still
+// have some reservations about this, because I don't like the anxiety it
+// causes me simply for not constantly reading. I don't want to feel like I
+// just magically completely missed out on an important article. Another note,
+// is that if I ever implement the history view, I have to be careful about
+// what data I should retain.
+// TODO: Do I need to also set readState? Perhaps that is implicit
+// in archiveState? Moreover, if that is implicit, why am I
+// using two separate flags instead of just one flag with 3 states?
+// Using one flag would simplify the index keypath and store one less
+// field in the entry store (both pre and post archive transform).
+// I suppose I would also need to think about other things like count
+// of unread and other areas that use the flag. Maybe I should just be
+// using one flag.
+// TODO: if I ever plan to implement a history view where I can see
+// articles read, I should consider maintaining entry.title so that it
+// can appear in the history view. Maybe I also want to add in a
+// 'blurb' that is like truncateHTMLString that shows a bit of the full
+// text of each entry.
+function archiveEntries() {
+  console.log('Archiving entries...');
+  let processedEntryCount = 0, archivedEntryCount = 0;
+  const EXPIRES_AFTER_MS = 10 * 24 * 60 * 60 * 1000;
+  db.open(onConnect);
+
+  function onConnect(event) {
+    // Exit early if we failed to connect (e.g. blocked)
+    if(event.type !== 'success') {
+      console.debug(event);
+      onComplete();
+      return;
+    }
+
+    // Open a cursor over read, unarchived entries
+    const connection = event.target.result;
+    const transaction = connection.transaction('entry', 'readwrite');
+    transaction.oncomplete = onComplete;
+    const store = transaction.objectStore('entry');
+    const index = store.index('archiveState-readState');
+    const keyPath = [db.EntryFlags.UNARCHIVED, db.EntryFlags.READ];
+    const request = index.openCursor(keyPath);
+    request.onsuccess = processEntryAtCursor;
+  }
+
+  // Check if the entry at the current cursor position should be archived, and
+  // if so, archive it, and then proceed to the next entry.
+  // TODO: access dateCreated once field is renamed
+  // TODO: the object will be a Date object once I make other changes so
+  // the calculation here needs to change to compare two date objects
+  function processEntryAtCursor(event) {
+    const request = event.target;
+    const cursor = request.result;
+
+    if(!cursor) {
+      return;
+    }
+
+    processedEntryCount++;
+    const entry = cursor.value;
+    const ageInMillis = Date.now() - entry.created;
+    if(ageInMillis > EXPIRES_AFTER_MS) {
+      const archivedEntry = Object.create(null);
+      archivedEntry.id = entry.id;
+      archivedEntry.feed = entry.feed;
+      archivedEntry.urls = entry.urls;
+      archivedEntry.dateArchived = new Date();
+      archivedEntry.archiveState = db.EntryFlags.ARCHIVED;
+      cursor.update(archivedEntry);
+
+      const archiveMessage = {
+        'type': 'archiveEntryRequested',
+        'entryId': entry.id
+      };
+      chrome.runtime.sendMessage(archiveMessage);
+
+      archivedEntryCount++;
+    }
+    cursor.continue();
+  }
+
+  function onComplete(event) {
+    console.log('Archived %s of %s entries', archivedEntryCount,
+      processedEntryCount);
+  }
+}
--- src/background.js
+++ src/background.js
@@ -38,7 +38,7 @@ Background.onAlarm = function(alarm) {
     // Theory 1: something to do with creating the alarm per page load
     // Theory 2: erroneously not persisted, never have page open long enough
     localStorage.ARCHIVE_ALARM_WOKEUP = '' + Date.now();
-    Background.archiveEntries();
+    archiveEntries();
   } else if(alarm.name === 'pollAlarm') {
     FeedPoller.start();
   }
@@ -78,103 +78,3 @@ Background.onBadgeClick = function() {
 };
 
 chrome.browserAction.onClicked.addListener(Background.onBadgeClick);
-
-// Archives certain entries
-Background.archiveEntries = function() {
-  console.log('Archiving entries...');
-
-  // Tracking variables that are shared by the nested helper functions
-  // and used to log stats
-  let processedEntryCount = 0, archivedEntryCount = 0;
-
-  // An entry is considered archivable if it was created more than 10 days
-  // ago, among other factors.
-  // TODO: it would be cool if this could read in a property set in
-  // manifest.js regarding the expiration period instead of hard coding a
-  // preference.
-  const EXPIRES_AFTER_MS = 10 * 24 * 60 * 60 * 1000;
-
-  db.open(onConnect);
-
-  function onConnect(event) {
-    // NOTE: there can be normal cases where this happens. For example, if
-    // the request is blocked
-    // TODO: should this still call on complete? Should oncomplete accept
-    // a parameter indicating success or failure?
-    // TODO: rather than log the event, log the error. Is that event.error?
-    if(event.type !== 'success') {
-      console.debug(event);
-      return;
-    }
-
-    const connection = event.target.result;
-    const transaction = connection.transaction('entry', 'readwrite');
-    transaction.oncomplete = onComplete;
-    const store = transaction.objectStore('entry');
-    const index = store.index('archiveState-readState');
-    const keyPath = [db.EntryFlags.UNARCHIVED, db.EntryFlags.READ];
-    const request = index.openCursor(keyPath);
-    request.onsuccess = processEntryAtCursor;
-  }
-
-  // Check if the entry at the current cursor position should be archived, and
-  // if so, archive it, and then proceed to the next entry.
-  // TODO: access dateCreated once field is renamed
-  // TODO: the object will be a Date object once I make other changes so
-  // the calculation here needs to change to compare two date objects
-  function processEntryAtCursor(event) {
-    const request = event.target;
-    const cursor = request.result;
-    if(cursor) {
-      processedEntryCount++;
-      const entry = cursor.value;
-      const ageInMillis = Date.now() - entry.created;
-      if(ageInMillis > EXPIRES_AFTER_MS) {
-
-        const archivedEntry = Object.create(null);
-        archivedEntry.id = entry.id;
-        archivedEntry.feed = entry.feed;
-        archivedEntry.urls = entry.urls;
-        archivedEntry.dateArchived = new Date();
-        archivedEntry.archiveState = db.EntryFlags.ARCHIVED;
-
-        // TODO: Do I need to also set readState? Perhaps that is implicit
-        // in archiveState? Moreover, if that is implicit, why am I
-        // using two separate flags instead of just one flag with 3 states? Using
-        // one flag would simplify the index keypath and store one less field in
-        // the entry store (both pre and post archive transform).
-        // I suppose I would also need to think about other things like count
-        // of unread and other areas that use the flag. Maybe I should just be
-        // using one flag.
-        // TODO: if I ever plan to implement a history view where I can see
-        // articles read, I should consider maintaining entry.title so that it can
-        // appear in the history view. Maybe I also want to add in a 'blurb' that
-        // is like truncateHTMLString that shows a bit of the full text of each
-        // entry.
-
-        // Note that IDBCursor.prototype.update is async. I am not waiting for
-        // it to complete here before continuing.
-        cursor.update(archivedEntry);
-
-        // NOTE: this is called prior to the update request completing, meaning
-        // listeners are notified while the request is still pending, and even
-        // if it may somehow be unsuccessful
-        // TODO: use a type like 'archiveEntryRequested' to signal that it is
-        // a pending operation
-        const archiveMessage = {
-          'type': 'archivedEntry',
-          'entryId': entry.id
-        };
-        chrome.runtime.sendMessage(archiveMessage);
-
-        archivedEntryCount++;
-      }
-      cursor.continue();
-    }
-  }
-
-  function onComplete(event) {
-    console.log('Archived %s of %s entries', archivedEntryCount,
-      processedEntryCount);
-  }
-};
