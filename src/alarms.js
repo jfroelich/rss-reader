0a865ed1356a40ecfb406c257e1f3521dfbf9e57
Josh <joshua.froelich@gmail.com>
2016-06-22 09:20:56 -0400

    Minor comment changes in alarms.js

--- src/alarms.js
+++ src/alarms.js
@@ -5,14 +5,15 @@
 'use strict';
 
 // Called by Chrome when an alarm wakes up
+// TODO: there is a strange bug, archiveAlarm is never called
+// Theory 1: something to do with creating the alarm per page load
+// Theory 2: erroneously not persisted, never have page open long enough
 function onAlarm(alarm) {
   if(alarm.name === 'archiveAlarm') {
-    // TODO: test whether there is ever called, testing with a temporary
-    // local storage variable
-    // NOTE: I am not seeing it. This is never getting called.
-    // Theory 1: something to do with creating the alarm per page load
-    // Theory 2: erroneously not persisted, never have page open long enough
+
+    // temp, for debugging
     localStorage.ARCHIVE_ALARM_WOKEUP = '' + Date.now();
+
     archiveEntries();
   } else if(alarm.name === 'pollAlarm') {
     FeedPoller.start();
