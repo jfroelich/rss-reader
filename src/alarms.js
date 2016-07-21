002f1259ed6daab28f83399ad52cc9405bd60881
Josh <joshua.froelich@gmail.com>
2016-07-21 19:26:01 -0400

    onAlarm now creates a PollingService instance

--- src/alarms.js
+++ src/alarms.js
@@ -6,21 +6,28 @@
 
 // Called by Chrome when an alarm wakes up
 // TODO: there is a strange bug, archiveAlarm is never called
-// Theory 1: something to do with creating the alarm per page load
-// Theory 2: erroneously not persisted, never have page open long enough
-// Theory 3: creating pollAlarm overwrites archiveAlarm (there can only be one)
+// It looks like chrome is not picking up on alarm name changes, it keeps
+// calling alarm names i no longer use
+// ok it looks like it serializes the alarm when registered, so even if not
+// re-registering, it still eventually gets sent to onAlarm. that's why i am
+// still seeing unknown alarms
 // TODO: think of a way to only bind the listener once. Maybe there is a way
 // to lookup a registered alarm and only register if unregistered.
 function onAlarm(alarm) {
+  console.debug('Received alarm:', alarm.name);
   const archiveService = new ArchiveService();
   archiveService.log = new LoggingService();
   archiveService.log.level = LoggingService.LEVEL_INFO;
+
+  const pollingService = new PollingService();
+  pollingService.log = new LoggingService();
+  pollingService.log.level = LoggingService.LEVEL_INFO;
 
   if(alarm.name === 'archive') {
     console.debug('Calling archiveService.start() from alarm wakeup');
     archiveService.start();
   } else if(alarm.name === 'poll') {
-    PollingService.start();
+    pollingService.start();
   } else {
     console.debug('Unknown alarm', alarm.name);
   }
