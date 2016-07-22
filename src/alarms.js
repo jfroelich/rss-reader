00849d21495a0b219c047613bad9d712022a3206
Josh <joshua.froelich@gmail.com>
2016-07-21 23:12:44 -0400

    Improved how alarms are created
    Use the new ImageDimensionsService. Only create alarms if not already
    present.

--- src/alarms.js
+++ src/alarms.js
@@ -4,27 +4,38 @@
 
 'use strict';
 
-// Called by Chrome when an alarm wakes up
-// TODO: there is a strange bug, archiveAlarm is never called
-// It looks like chrome is not picking up on alarm name changes, it keeps
-// calling alarm names i no longer use
-// ok it looks like it serializes the alarm when registered, so even if not
-// re-registering, it still eventually gets sent to onAlarm. that's why i am
-// still seeing unknown alarms
-// TODO: think of a way to only bind the listener once. Maybe there is a way
-// to lookup a registered alarm and only register if unregistered.
-function onAlarm(alarm) {
-  console.debug('Received alarm:', alarm.name);
-  const archiveService = new ArchiveService();
-  archiveService.log = new LoggingService();
-  archiveService.log.level = LoggingService.LEVEL_INFO;
+chrome.alarms.get('archive', function(alarm) {
+  if(!alarm) {
+    console.debug('Creating archive alarm');
+    chrome.alarms.create('archive', {'periodInMinutes': 60});
+  }
+});
 
+chrome.alarms.get('poll', function(alarm) {
+  if(!alarm) {
+    console.debug('Creating poll alarm');
+    chrome.alarms.create('poll', {'periodInMinutes': 20});
+  }
+});
+
+function onAlarmListener(alarm) {
+  console.debug('Received alarm wakeup', alarm.name);
+
+  const log = new LoggingService(LoggingService.LEVEL_LOG);
+  const archiveService = new ArchiveService();
+  archiveService.log = log;
+  const faviconCache = new FaviconCache('favicon-cache');
+  faviconCache.log = log;
+  const faviconService = new FaviconService(faviconCache);
+  faviconService.log = log;
+  const imageDimensionsService = new ImageDimensionsService();
+  imageDimensionsService.log = log;
   const pollingService = new PollingService();
-  pollingService.log = new LoggingService();
-  pollingService.log.level = LoggingService.LEVEL_INFO;
+  pollingService.log = log;
+  pollingService.faviconService = faviconService;
+  pollingService.imageDimensionsService = imageDimensionsService;
 
   if(alarm.name === 'archive') {
-    console.debug('Calling archiveService.start() from alarm wakeup');
     archiveService.start();
   } else if(alarm.name === 'poll') {
     pollingService.start();
@@ -33,6 +44,7 @@ function onAlarm(alarm) {
   }
 }
 
-chrome.alarms.create('archive', {'periodInMinutes': 60});
-chrome.alarms.create('poll', {'periodInMinutes': 20});
-chrome.alarms.onAlarm.addListener(onAlarm);
+if(!chrome.alarms.onAlarm.hasListener(onAlarmListener)) {
+  console.debug('Adding alarm listener');
+  chrome.alarms.onAlarm.addListener(onAlarmListener);
+}
