de080ec588cf3e7c269f5c89d71b541de2fcc8e2
Josh <joshua.froelich@gmail.com>
2016-07-27 09:54:08 -0400

    Minor improvements in trim-document.js
    Modified comments, simplified variable name.

--- src/trim-document.js
+++ src/trim-document.js
@@ -4,22 +4,16 @@
 
 'use strict';
 
-// Remove trimmable nodes from the start and end of the document.
-// TODO: this needs to work more like filterLeafElements, where it considers
-// not just empty nodes, but nodes containing only empty nodes, because those
-// are essentially also trimmable. However, maybe then that is simply
-// redundant with leaves. Right now this ignores leaves. So maybe I am
-// confusing myself.
 function trimDocument(document) {
   const bodyElement = document.body;
   if(!bodyElement) {
     return;
   }
 
-  const TRIMMABLE_VOID_ELEMENT_NAMES = {
-    'BR': 1,
-    'HR': 1,
-    'NOBR': 1
+  const VOIDS = {
+    'br': 1,
+    'hr': 1,
+    'nobr': 1
   };
 
   const firstChild = bodyElement.firstChild;
@@ -31,11 +25,9 @@ function trimDocument(document) {
     }
   }
 
-  // NOTE: node.nodeName yields uppercase when document is flagged as HTML
-
   function removeStep(startNode, step) {
     let node = startNode;
-    while(node && (node.nodeName in TRIMMABLE_VOID_ELEMENT_NAMES ||
+    while(node && (node.localName in VOIDS ||
       (node.nodeType === Node.TEXT_NODE && !node.nodeValue.trim()))) {
       let sibling = node[step];
       node.remove();
