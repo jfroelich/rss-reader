3985863325593c9b34a06373da827d309546f437
Josh <joshua.froelich@gmail.com>
2016-07-30 09:03:07 -0400

    Created background-service
    Merged alarms, on-badge-click, on-installed into a single module

---
+++ src/background-service.js
@@ -0,0 +1,76 @@
+// Copyright 2016 Josh Froelich. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file
+
+'use strict';
+
+const BackgroundService = {};
+
+BackgroundService.onBackgroundPageLoad = function(event) {
+  chrome.runtime.onInstalled.addListener(BackgroundService.onInstalled);
+  chrome.browserAction.onClicked.addListener(BackgroundService.onBadgeClick);
+
+  chrome.alarms.get('archive', function(alarm) {
+    if(!alarm) {
+      console.debug('Creating archive alarm');
+      chrome.alarms.create('archive', {'periodInMinutes': 60});
+    }
+  });
+
+  chrome.alarms.get('poll', function(alarm) {
+    if(!alarm) {
+      console.debug('Creating poll alarm');
+      chrome.alarms.create('poll', {'periodInMinutes': 20});
+    }
+  });
+
+  if(!chrome.alarms.onAlarm.hasListener(BackgroundService.onAlarm)) {
+    console.debug('Binding alarm listener');
+    chrome.alarms.onAlarm.addListener(BackgroundService.onAlarm);
+  }
+};
+
+BackgroundService.onInstalled = function(event) {
+  console.log('Installing extension ...');
+  const badgeUpdateService = new BadgeUpdateService();
+  badgeUpdateService.updateCount();
+};
+
+BackgroundService.onBadgeClick = function() {
+  const VIEW_URL = chrome.extension.getURL('slides.html');
+  chrome.tabs.query({'url': VIEW_URL}, onQueryForViewTab);
+
+  function onQueryForViewTab(tabsArray) {
+    if(tabsArray && tabsArray.length) {
+      chrome.tabs.update(tabsArray[0].id, {'active': true});
+    } else {
+      // The trailing slash is required
+      chrome.tabs.query({'url': 'chrome://newtab/'}, onQueryForNewTab);
+    }
+  }
+
+  function onQueryForNewTab(tabsArray) {
+    if(tabsArray && tabsArray.length) {
+      chrome.tabs.update(tabsArray[0].id, {'active': true, 'url': VIEW_URL});
+    } else {
+      chrome.tabs.create({'url': VIEW_URL});
+    }
+  }
+};
+
+BackgroundService.onAlarm = function(alarm) {
+  console.debug('Alarm wakeup', alarm.name);
+
+  const archiveService = new ArchiveService();
+  const pollingService = new PollingService();
+
+  if(alarm.name === 'archive') {
+    archiveService.start();
+  } else if(alarm.name === 'poll') {
+    pollingService.start();
+  } else {
+    console.warn('Unknown alarm', alarm.name);
+  }
+};
+
+BackgroundService.onBackgroundPageLoad();
