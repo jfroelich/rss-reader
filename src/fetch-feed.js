0ed92929a24c50ea32a8bcd5cb9c898a9d30f5b6
Josh <joshua.froelich@gmail.com>
2016-08-01 02:00:15 -0400

    Minor improvements to fetchFeed
    Added an assert, changed some comments

--- src/fetch-feed.js
+++ src/fetch-feed.js
@@ -5,6 +5,7 @@
 'use strict';
 
 function fetchFeed(requestURL, timeoutMillis, excludeEntries, callback) {
+  console.assert(requestURL, 'requestURL is required');
   console.assert('href' in requestURL, 'requestURL must be URL like');
   console.assert(typeof timeoutMillis === 'undefined' ||
     (!isNaN(timeoutMillis) && timeoutMillis >= 0), 'Invalid timeout specified',
@@ -35,6 +36,7 @@ function fetchFeedOnResponse(requestURL, excludeEntries, callback, event) {
     'type': event.type
   };
 
+  // Check that we got a load response
   if(event.type !== 'load') {
     console.debug(event.type, event.target.status, requestURL.href);
     callback({'type': event.type});
@@ -44,6 +46,9 @@ function fetchFeedOnResponse(requestURL, excludeEntries, callback, event) {
   outputEvent.responseURL = new URL(event.target.responseURL);
   outputEvent.responseXML = event.target.responseXML;
 
+  // Check that the document is defined. The document may be undefined when
+  // we fetched a file with a mime-type not compatable with the responseType
+  // setting of XMLHttpRequest
   const document = event.target.responseXML;
   if(!document) {
     outputEvent.type = 'UndefinedDocumentError';
@@ -51,6 +56,7 @@ function fetchFeedOnResponse(requestURL, excludeEntries, callback, event) {
     return;
   }
 
+  // Parse the XML file into a feed-like object
   try {
     outputEvent.feed = FeedParser.parse(document, excludeEntries);
   } catch(exception) {
@@ -64,15 +70,22 @@ function fetchFeedOnResponse(requestURL, excludeEntries, callback, event) {
     return;
   }
 
+  // Define the urls property. The parser does not define it for us because the
+  // parser is not aware of the feed's url.
   outputEvent.feed.urls = [requestURL];
+
+  // Check for a redirect, and if found, append it to the urls property.
   if(outputEvent.responseURL.href !== requestURL.href) {
     console.debug('Detected redirect', requestURL.href,
       outputEvent.responseURL.href);
     outputEvent.feed.urls.push(responseURL);
   }
 
+  // Introduce a date fetched property
   outputEvent.feed.dateFetched = new Date();
 
+  // Introduce a dateLastModified property based on the file's date which is
+  // from the response header.
   const lastModifiedString = event.target.getResponseHeader(
     'Last-Modified');
   if(lastModifiedString) {
