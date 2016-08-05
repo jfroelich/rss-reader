00af23d40213deaae361670e60aa5f446342c3f9
Josh <joshua.froelich@gmail.com>
2016-08-05 18:57:59 -0400

    Created resolve-document-urls
    Moved functionality out of fetch-html.js

--- src/fetch-html.js
+++ src/fetch-html.js
@@ -37,6 +37,8 @@ function fetchHTMLOnFetch(requestURL, callback, event) {
     return;
   }
 
+  // When XMLHttpRequest fetches a document that is not an XML/HTML document,
+  // then the load was successful, but document is undefined.
   const document = event.target.responseXML;
   if(!document) {
     console.warn('Undefined document', requestURL.href);
@@ -53,7 +55,7 @@ function fetchHTMLOnFetch(requestURL, callback, event) {
 
   transformLazyImages(document);
   filterSourcelessImages(document);
-  fetchHTMLResolveURLs(document, outputEvent.responseURL);
+  resolveDocumentURLs(document, outputEvent.responseURL);
   filterTrackingImages(document);
   setImageDimensions(document,
     fetchHTMLOnSetImageDimensions.bind(this, outputEvent, callback));
@@ -61,139 +63,4 @@ function fetchHTMLOnFetch(requestURL, callback, event) {
 
 function fetchHTMLOnSetImageDimensions(event, callback, numImagesModified) {
   callback(event);
-}
-
-function fetchHTMLResolveURLs(document, baseURL) {
-  for(let base of document.querySelectorAll('base')) {
-    base.remove();
-  }
-
-  fetchHTMLResolveElements(document, baseURL);
-  fetchHTMLResolveSrcsets(document, baseURL);
-}
-
-function fetchHTMLResolveElements(document, baseURL) {
-
-  const URL_ATTRIBUTE_MAP = {
-    'A': 'href',
-    'APPLET': 'codebase',
-    'AREA': 'href',
-    'AUDIO': 'src',
-    'BASE': 'href',
-    'BLOCKQUOTE': 'cite',
-    'BODY': 'background',
-    'BUTTON': 'formaction',
-    'DEL': 'cite',
-    'EMBED': 'src',
-    'FRAME': 'src',
-    'HEAD': 'profile',
-    'HTML': 'manifest',
-    'IFRAME': 'src',
-    'FORM': 'action',
-    'IMG': 'src',
-    'INPUT': 'src',
-    'INS': 'cite',
-    'LINK': 'href',
-    'OBJECT': 'data',
-    'Q': 'cite',
-    'SCRIPT': 'src',
-    'SOURCE': 'src',
-    'TRACK': 'src',
-    'VIDEO': 'src'
-  };
-
-  const SELECTOR = Object.keys(URL_ATTRIBUTE_MAP).map(function(key) {
-    return key + '[' + URL_ATTRIBUTE_MAP[key] +']';
-  }).join(', ');
-
-  const elements = document.querySelectorAll(SELECTOR);
-
-  for(let element of elements) {
-    const elementName = element.nodeName.toUpperCase();
-
-    const attributeName = URL_ATTRIBUTE_MAP[elementName];
-    if(!attributeName) {
-      continue;
-    }
-
-    const attributeValue = element.getAttribute(attributeName);
-    if(!attributeValue) {
-      continue;
-    }
-
-    if(/^\s*https?:\/\/#/i.test(attributeValue)) {
-      element.remove();
-      continue;
-    }
-
-    const resolvedURL = fetchHTMLResolveURL(attributeValue, baseURL);
-    // TODO: not equals is weak comparison because it ignores spaces and
-    // is case sensitive, maybe make it stronger
-    if(resolvedURL && resolvedURL.href !== attributeValue) {
-      element.setAttribute(attributeName, resolvedURL.href);
-    }
-  }
-}
-
-function fetchHTMLResolveSrcsets(document, baseURL) {
-  const elements = document.querySelectorAll('img[srcset], source[srcset]');
-  for(let element of elements) {
-    const attributeValue = element.getAttribute('srcset');
-    if(attributeValue) {
-      const srcset = parseSrcset(attributeValue);
-      if(srcset && srcset.length) {
-        let dirtied = false;
-        for(let descriptor of srcset) {
-          const resolvedURL = fetchHTMLResolveURL(descriptor.url, baseURL);
-          if(resolvedURL && resolvedURL.href !== descriptor.url) {
-            dirtied = true;
-            descriptor.url = resolvedURL.href;
-          }
-        }
-
-        if(dirtied) {
-          const newSrcsetValue = fetchHTMLSerializeSrcset(srcset);
-          if(newSrcsetValue && newSrcsetValue !== attributeValue) {
-            element.setAttribute('srcset', newSrcsetValue);
-          }
-        }
-      }
-    }
-  }
-}
-
-function fetchHTMLSerializeSrcset(descriptorsArray) {
-  const outputStringBuffer = [];
-  for(let descriptor of descriptorsArray) {
-    let descriptorStringBuffer = [descriptor.url];
-    if(descriptor.d) {
-      descriptorStringBuffer.push(' ');
-      descriptorStringBuffer.push(descriptor.d);
-      descriptorStringBuffer.push('x');
-    } else if(descriptor.w) {
-      descriptorStringBuffer.push(' ');
-      descriptorStringBuffer.push(descriptor.w);
-      descriptorStringBuffer.push('w');
-    } else if(descriptor.h) {
-      descriptorStringBuffer.push(' ');
-      descriptorStringBuffer.push(descriptor.h);
-      descriptorStringBuffer.push('h');
-    }
-
-    outputStringBuffer.push(descriptorStringBuffer.join(''));
-  }
-
-  // The space is important
-  return outputStringBuffer.join(', ');
-}
-
-function fetchHTMLResolveURL(urlString, baseURL) {
-  if(urlString && !/^\s*javascript:/i.test(urlString) &&
-    !/^\s*data:/i.test(urlString)) {
-    try {
-      return new URL(urlString, baseURL);
-    } catch(exception) {
-      console.warn(urlString, baseURL.href, exception);
-    }
-  }
 }
