ceb2cad8bf7818f6ba1a4562a8296a046f4d1515
Josh <joshua.froelich@gmail.com>
2016-08-05 18:42:44 -0400

    Created transform-lazy-images.js
    Moved functionality out of fetch-html.js

--- src/fetch-html.js
+++ src/fetch-html.js
@@ -50,7 +50,7 @@ function fetchHTMLOnFetch(requestURL, callback, event) {
     'responseURL': new URL(event.target.responseURL)
   };
 
-  fetchHTMLTransformLazilyLoadedImages(document);
+  transformLazyImages(document);
   fetchHTMLFilterSourcelessImages(document);
   fetchHTMLResolveURLs(document, outputEvent.responseURL);
   fetchHTMLFilterTrackingImages(document);
@@ -64,63 +64,10 @@ function fetchHTMLOnSetImageDimensions(event, callback, numImagesModified) {
 
 function fetchHTMLFilterSourcelessImages(document) {
   for(let image of document.querySelectorAll('img')) {
-    if(!fetchHTMLHasSource(image)) {
+    if(!image.hasAttribute('src') && !image.hasAttribute('srcset')) {
       image.remove();
     }
   }
-}
-
-function fetchHTMLTransformLazilyLoadedImages(document) {
-
-/*
-// I see cases like this. Instead of checking for absence of src I should be
-// overwriting maybe?
-<img class="img-responsive svg-fallback js-lazy"
-data-original="http://nodeassets.nbcnews.com/cdnassets/projects/nbcnews-assets
-/footer-logo-msnbc-upper.svg" src="http://sslnodeassets.nbcnews.com/images
-/transparent-placeholder.gif" alt="msnbc">
-*/
-
-  const LAZY_ATTRIBUTES = [
-    'load-src',
-    'data-src',
-    'data-original-desktop',
-    'data-baseurl',
-    'data-lazy',
-    'data-img-src',
-    'data-original',
-    'data-adaptive-img',
-    'data-imgsrc',
-    'data-default-src'
-  ];
-
-  const images = document.querySelectorAll('img');
-  for(let image of images) {
-    if(!fetchHTMLHasSource(image)) {
-      for(let alternateName of LAZY_ATTRIBUTES) {
-        if(image.hasAttribute(alternateName)) {
-          const alternateValue = image.getAttribute(alternateName);
-          if(alternateValue && fetchHTMLIsMinimallyValidURL(alternateValue)) {
-            image.removeAttribute(alternateName);
-            image.setAttribute('src', alternateValue);
-            break;
-          }
-        }
-      }
-    }
-  }
-}
-
-function fetchHTMLHasSource(image) {
-  return image.hasAttribute('src') || image.hasAttribute('srcset');
-}
-
-// Only minimal validation. I cannot fully validate its url, because the url
-// could be relative
-function fetchHTMLIsMinimallyValidURL(inputString) {
-  const MINIMAL_VALID_URL_LENGTH = 'http://a'.length;
-  return inputString.length > MINIMAL_VALID_URL_LENGTH &&
-    !inputString.trim().includes(' ');
 }
 
 // TODO: can i just access image.src property to get hostname
