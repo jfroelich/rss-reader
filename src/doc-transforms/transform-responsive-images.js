f07ec6bf07dc3e31dca0fb1b9466946bae88cfac
Josh <joshua.froelich@gmail.com>
2017-09-24 21:46:26 -0400

    bug fix
    Typos after splitting up function, forgot to check parameter names,
    forgot to remove a closing paren when simplifying an if statement into
    a return value expression

--- src/doc-transforms/transform-responsive-images.js
+++ src/doc-transforms/transform-responsive-images.js
@@ -13,17 +13,17 @@ function transform_responsive_images(doc) {
 }
 
 // Return true if image has a valid src attribute value
-function image_has_valid_src(image) {
+function image_has_valid_src(image_element) {
   const min_url_length = 2;
   let src_value = image_element.getAttribute('src');
   if(src_value) {
     src_value = src_value.trim();
-    return src_value.length > min_url_length && !src_value.includes(' '));
+    return (src_value.length > min_url_length) && !src_value.includes(' ');
   }
 }
 
 // Return true if image has a srcset attribute value
-function image_has_srcset(image) {
+function image_has_srcset(image_element) {
   const srcset_value = image_element.getAttribute('srcset');
   return srcset_value && srcset_value.trim();
 }
