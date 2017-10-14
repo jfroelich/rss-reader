c0debc57d09507bf01cd68526cc3c8fa59206f29
Josh <joshua.froelich@gmail.com>
2017-10-13 22:37:13 -0400

    Reorganized calamine files
    Deleted temp files from modularity attempt. Never finished that one.
    Renamed to html-boilerplate-filter.js and stuck in src.

--- src/calamine/calamine-notes.md
+++
@@ -1,4 +0,0 @@
-
-# About
-
-Helps soothe and scrub out the shingles that is unwanted content in web pages.
--- src/calamine/temp-filter-boilerplate-example.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-function foo(doc) {
-  const model_options = {};
-  const model = bp_create_model(model_options);
-  const score_result = bp_score(doc, model);
-  bp_prune_to_best(doc, score_result);
-}
