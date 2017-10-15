1a2660072510894cea71c53dc0c3d72ac71e5188
Josh <joshua.froelich@gmail.com>
2017-10-14 21:57:47 -0400

    Added image_find_caption

--- src/dom/image.js
+++ src/dom/image.js
@@ -9,10 +9,9 @@ function image_has_source(img) {
 }
 
 // Return true if image has a valid src attribute value
-// TODO: use fn and param names consistent with image_has_source
-function image_has_valid_src(image_element) {
+function image_has_valid_source(image) {
   const min_url_length = 2;
-  let src_value = image_element.getAttribute('src');
+  let src_value = image.getAttribute('src');
   if(src_value) {
     src_value = src_value.trim();
     return (src_value.length > min_url_length) && !src_value.includes(' ');
@@ -20,7 +19,17 @@ function image_has_valid_src(image_element) {
 }
 
 // Return true if image has a srcset attribute value
-function image_has_srcset(image_element) {
-  const srcset_value = image_element.getAttribute('srcset');
+function image_has_srcset(image) {
+  const srcset_value = image.getAttribute('srcset');
   return srcset_value && srcset_value.trim();
+}
+
+// Searches for and returns the corresponding figcaption element
+function image_find_caption(image) {
+  let figcaption;
+  const figure = image.closest('figure');
+  if(figure) {
+    figcaption = figure.querySelector('figcaption');
+  }
+  return figcaption;
 }
