3485a4c277ecc4e3d95f3221e56d00faa2c617f7
Josh <joshua.froelich@gmail.com>
2017-11-13 14:07:49 -0500

    minor cleanup of mime.js

--- src/mime.js
+++ src/mime.js
@@ -3,18 +3,125 @@
 import assert from "/src/assert.js";
 import {filterWhitespace} from "/src/string.js";
 
-// TODO: deprecate mime namespace object after module transition
-// TODO: ensure I am not exporting things that do not need exporting
-
-
 // Some commonly used global mime types
 export const HTML = 'text/html';
 export const XML = 'application/xml';
 
-// Rather arbitrary, but just as a general bound
+// These constraints are rather arbitrary, but just as a general bound
 // TODO: increase and test accuracy
 export const MIME_TYPE_MIN_LENGTH = 7;
 export const MIME_TYPE_MAX_LENGTH = 100;
+
+// Return a mime type corresponding a file name extension
+// @param extension {String}
+// @returns {String} a mime type, or undefined on error or failed lookup
+export function getTypeForExtension(extension) {
+  const extensionVarType = typeof extension;
+  if(extensionVarType === 'undefined' || extension === null) {
+    return;
+  }
+
+  assert(extensionVarType === 'string');
+  return EXTENSION_TYPE_MAP[extension];
+}
+
+// Returns a normalized mime type from a content type
+// @param contentType {String} an http response header value, optional
+// @returns {String} a mime type, or undefined if error
+export function fromContentType(contentType) {
+  const contentTypeVarType = typeof contentType;
+  if(contentTypeVarType === 'undefined') {
+    return;
+  }
+
+  assert(contentTypeVarType === 'string');
+  let mimeType = contentType;
+
+  // Strip the character encoding, if present. The substring gets all
+  // characters up to but excluding the semicolon. The coding
+  // is optional, so leave the type as is if no semicolon is present.
+  const fromIndex = 0;
+  const semicolonPosition = contentType.indexOf(';');
+  if(semicolonPosition !== -1) {
+    mimeType = contentType.substring(fromIndex, semicolonPosition);
+  }
+
+  return normalize(mimeType);
+}
+
+// A basic trivial test of whether the parameter represents a mime type.
+// Inaccurate. Few false negatives but many false positives.
+export function isMimeType(mimeType) {
+  const MIN_LENGTH = 2, MAX_LENGTH = 100;
+  return typeof mimeType === 'string' && mimeType.length > MIN_LENGTH &&
+    mimeType.length < MAX_LENGTH && mimeType.includes('/') &&
+    !mimeType.includes(' ');
+}
+
+export function isBinary(mimeType) {
+  assert(isMimeType(mimeType));
+
+  const slashPosition = mimeType.indexOf('/');
+  const superType = mimeType.substring(0, slashPosition);
+
+  switch(superType) {
+  case 'application': {
+    return !APPLICATION_TEXT_TYPES.includes(mimeType);
+  }
+  case 'text':
+    return false;
+  case 'audio':
+    return true;
+  case 'image':
+    return true;
+  case 'video':
+    return true;
+  case 'multipart':
+    return true;
+  default:
+    console.debug('unhandled mime type:', mimeType);
+    return false;
+  }
+}
+
+function normalize(mimeType) {
+  assert(isMimeType(mimeType));
+  return filterWhitespace(mimeType).toLowerCase();
+}
+
+export function isHTML(contentType) {
+  return /^\s*text\/html/i.test(contentType);
+}
+
+export function isImage(contentType) {
+  return /^\s*image\//i.test(contentType);
+}
+
+export function isXML(contentType) {
+  const mimeType = fromContentType(contentType);
+  const types = [
+    'application/atom+xml',
+    'application/rdf+xml',
+    'application/rss+xml',
+    'application/vnd.mozilla.xul+xml',
+    'application/xml',
+    'application/xhtml+xml',
+    'text/xml'
+  ];
+  return types.includes(mimeType);
+}
+
+// Mime types that have the application super type but are not binary
+const APPLICATION_TEXT_TYPES = [
+  'application/atom+xml',
+  'application/javascript',
+  'application/json',
+  'application/rdf+xml',
+  'application/rss+xml',
+  'application/vnd.mozilla.xul+xml',
+  'application/xhtml+xml',
+  'application/xml'
+];
 
 const EXTENSION_TYPE_MAP = {
   ai:   'application/postscript',
@@ -113,115 +220,3 @@ const EXTENSION_TYPE_MAP = {
   xul:  'application/vnd.mozilla.xul+xml',
   zip:  'application/zip'
 };
-
-// Return a mime type corresponding a file name extension
-// @param extension {String}
-// @returns {String} a mime type, or undefined on error or failed lookup
-export function getTypeForExtension(extension) {
-  const extensionVarType = typeof extension;
-  if(extensionVarType === 'undefined' || extension === null) {
-    return;
-  }
-
-  assert(extensionVarType === 'string');
-
-  return EXTENSION_TYPE_MAP[extension];
-}
-
-// Returns a normalized mime type from a content type
-// @param contentType {String} an http response header value, optional
-// @returns {String} a mime type, or undefined if error
-export function fromContentType(contentType) {
-  const contentTypeVarType = typeof contentType;
-  if(contentTypeVarType === 'undefined') {
-    return;
-  }
-
-  assert(contentTypeVarType === 'string');
-  let mimeType = contentType;
-
-  // Strip the character encoding, if present. The substring gets all
-  // characters up to but excluding the semicolon. The coding
-  // is optional, so leave the type as is if no semicolon is present.
-  const fromIndex = 0;
-  const semicolonPosition = contentType.indexOf(';');
-  if(semicolonPosition !== -1) {
-    mimeType = contentType.substring(fromIndex, semicolonPosition);
-  }
-
-  return normalize(mimeType);
-}
-
-// A basic trivial test of whether the parameter represents a mime type.
-// Inaccurate. Few false negatives but many false positives.
-export function isMimeType(mimeType) {
-  const MIN_LENGTH = 2, MAX_LENGTH = 100;
-  return typeof mimeType === 'string' && mimeType.length > MIN_LENGTH &&
-    mimeType.length < MAX_LENGTH && mimeType.includes('/') &&
-    !mimeType.includes(' ');
-}
-
-// Mime types that have the application super type but are not binary
-const APPLICATION_TEXT_TYPES = [
-  'application/atom+xml',
-  'application/javascript',
-  'application/json',
-  'application/rdf+xml',
-  'application/rss+xml',
-  'application/vnd.mozilla.xul+xml',
-  'application/xhtml+xml',
-  'application/xml'
-];
-
-export function isBinary(mimeType) {
-  assert(isMimeType(mimeType));
-
-  const slashPosition = mimeType.indexOf('/');
-  const superType = mimeType.substring(0, slashPosition);
-
-  switch(superType) {
-  case 'application': {
-    return !APPLICATION_TEXT_TYPES.includes(mimeType);
-  }
-  case 'text':
-    return false;
-  case 'audio':
-    return true;
-  case 'image':
-    return true;
-  case 'video':
-    return true;
-  case 'multipart':
-    return true;
-  default:
-    console.debug('unhandled mime type:', mimeType);
-    return false;
-  }
-}
-
-function normalize(mimeType) {
-  assert(isMimeType(mimeType));
-  return filterWhitespace(mimeType).toLowerCase();
-}
-
-export function isHTML(contentType) {
-  return /^\s*text\/html/i.test(contentType);
-}
-
-export function isImage(contentType) {
-  return /^\s*image\//i.test(contentType);
-}
-
-export function isXML(contentType) {
-  const mimeType = fromContentType(contentType);
-  const types = [
-    'application/atom+xml',
-    'application/rdf+xml',
-    'application/rss+xml',
-    'application/vnd.mozilla.xul+xml',
-    'application/xml',
-    'application/xhtml+xml',
-    'text/xml'
-  ];
-  return types.includes(mimeType);
-}
