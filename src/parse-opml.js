1bbb7a525d1020bf0f64289120f38f3f8105bc0a
Josh <joshua.froelich@gmail.com>
2017-11-15 11:30:49 -0500

    Cleanup parseOPML

--- src/parse-opml.js
+++ src/parse-opml.js
@@ -1,18 +1,19 @@
 // OPML parsing
 
-import {check} from "/src/errors.js";
+import {check, ParserError} from "/src/errors.js";
 import parseXML from "/src/parse-xml.js";
 
-// @param xml {String}
-// @returns {Document} document
-export default function parseOPML(xml) {
-  const doc = parseXML(xml);
+// Returns the parsed document or throws an error
+export default function parseOPML(xmlString) {
+  const doc = parseXML(xmlString);
+  // The lowercase call is important because of how case handling differs based on whether the
+  // document is internally flagged as xml or html. Because of the possible differences in behavior
+  // it is safer to incur the overhead of normalization.
+  // TODO: the above comment would not be needed if there was a helper function that was adequately
+  // named or documented such that it made the helper function call speak for itself. That is the
+  // whole idea of abstraction, so that I don't have to carry around the anxiety, so that the
+  // concern is addressed explicitly and settled.
   const name = doc.documentElement.localName.toLowerCase();
-
-  // TODO: use a more specific error
-  // TODO: use better wording
-  // TODO: do not use unnamed parameter
-  check(name === 'opml', undefined, 'document element is not "opml", it is ' + name);
-
+  check(name === 'opml', ParserError, 'document element ' + name + ' is not opml');
   return doc;
 }
