2c85da38935fa36b796aa2462affb80b43ae7fce
Josh <joshua.froelich@gmail.com>
2017-11-18 15:47:00 -0500

    Minor linting

--- src/filters/sourceless-image-filter.js
+++ src/filters/sourceless-image-filter.js
@@ -1,18 +1,16 @@
+// Removes images that are missing source information from document content
 
 import assert from "/src/assert.js";
-
 import {imageHasSource, removeImage} from "/src/dom.js";
 
 export default function filter(doc) {
   assert(doc instanceof Document);
-
   if(!doc.body) {
     return;
   }
 
   const images = doc.body.querySelectorAll('img');
   for(const image of images) {
-
     if(!imageHasSource(image)) {
       removeImage(image);
     }
