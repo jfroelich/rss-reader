7c26395182dfe4c5ca862c390a271a15c2b784aa
Josh <joshua.froelich@gmail.com>
2017-11-17 20:55:56 -0500

    Revised cli.js to export "namespace" object "cli", broke apart main function into individual functions defined on cli.

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,6 @@
-// Command line interface module
-// This module does not export anything. Instead, it defines a function in global scope (window).
-// The definition occurs as an implicit side effect of importing the module.
-//
-// Modules are basically wrapped in a promise. To enable variables to be accessible from
-// the console, which can only see global variables, and not exported variables
-// from modules, the values must be defined "really" globally. Right now using window seems
-// to work. This is not a recommended practice, but I think this is an exception, because
-// it is the entire point of this module, to be callable from the console
+// Command line interface module. This module does not export anything. Instead, it defines a
+// variable in global scope (window). The definition occurs as an implicit side effect of importing
+// the module.
 
 import archiveEntries from "/src/archive-entries.js";
 import FaviconCache from "/src/favicon-cache.js";
@@ -18,138 +12,103 @@ import removeLostEntries from "/src/remove-lost-entries.js";
 import removeOrphanedEntries from "/src/remove-orphaned-entries.js";
 import {parseInt10} from "/src/string.js";
 
+const cli = {};
 
-// TODO: maybe break apart back into functions and define each one in window, perhaps under a
-// "cli" namespace object
-
-async function execCommand(command, ...args) {
-  switch(command) {
-  case 'refreshicons': {
-    const fic = new FaviconCache();
-    let rConn, _;
-    try {
-      [rConn, _] = await Promise.all([rdb.open(), fic.open()]);
-      await refreshFeedIcons(rConn, fic.conn);
-    } finally {
-      fic.close();
-      rdb.close(rConn);
-    }
-    break;
+cli.refreshIcons = async function() {
+  const fic = new FaviconCache();
+  let rConn, _;
+  try {
+    [rConn, _] = await Promise.all([rdb.open(), fic.open()]);
+    await refreshFeedIcons(rConn, fic.conn);
+  } finally {
+    fic.close();
+    rdb.close(rConn);
   }
-  case 'archive': {
-    let maxAgeMs, conn, limit;
-    if(args && args.length) {
-      limit = args[0];
-    }
+};
 
-    try {
-      conn = await rdb.open();
-      await archiveEntries(conn, maxAgeMs, limit);
-    } finally {
-      rdb.close(conn);
-    }
-    break;
+cli.archiveEntries = async function(limit) {
+  let maxAgeMs, conn;
+  try {
+    conn = await rdb.open();
+    await archiveEntries(conn, maxAgeMs, limit);
+  } finally {
+    rdb.close(conn);
   }
-  case 'poll': {
-    const fic = new FaviconCache();
-    const pfc = new PollFeedsContext();
-    pfc.iconCache = fic;
-    pfc.allowMeteredConnections = true;
-    pfc.ignoreIdleState = true;
-    pfc.ignoreRecencyCheck = true;
-    pfc.ignoreModifiedCheck = true;
-    let _;
-    try {
-      [pfc.readerConn, _] = await Promise.all([rdb.open(), fic.open()]);
-      await pollFeeds(pfc);
-    } finally {
-      fic.close();
-      rdb.close(pfc.readerConn);
-    }
-    break;
-  }
-  case 'scanlost': {
-    let conn;
-    try {
-      conn = await rdb.open();
-      await removeLostEntries(conn, args);
-    } finally {
-      rdb.close(conn);
-    }
-    break;
-  }
-  case 'scanorphans': {
-    let conn;
-    try {
-      conn = await rdb.open();
-      await removeOrphanedEntries(conn, args);
-    } finally {
-      rdb.close(conn);
-    }
-    break;
-  }
-  case 'clearicons': {
-    const fic = new FaviconCache();
-    try {
-      await fic.open();
-      await fic.clear();
-    } finally {
-      fic.close();
-    }
-    break;
-  }
-  case 'iconlookup': {
-    let url, timeout, cacheless = true;
-    if(args && args.length) {
-      url = args[0];
-      if(args.length > 1) {
-        timeout = parseInt10(args[1]);
-        if(args.length > 2) {
-          cacheless = args[2];
-        }
-      }
-    }
+};
 
-    const query = new FaviconLookup();
-    query.cache = new FaviconCache();
-    query.fetchHTMLTimeoutMs = timeout;
+cli.pollFeeds = async function() {
+  const fic = new FaviconCache();
+  const pfc = new PollFeedsContext();
+  pfc.iconCache = fic;
+  pfc.allowMeteredConnections = true;
+  pfc.ignoreIdleState = true;
+  pfc.ignoreRecencyCheck = true;
+  pfc.ignoreModifiedCheck = true;
+  let _;
+  try {
+    [pfc.readerConn, _] = await Promise.all([rdb.open(), fic.open()]);
+    await pollFeeds(pfc);
+  } finally {
+    fic.close();
+    rdb.close(pfc.readerConn);
+  }
+};
 
-    try {
-      if(!cacheless) {
-        await query.cache.open();
-      }
+cli.removeListEntries = async function() {
+  let conn;
+  try {
+    conn = await rdb.open();
+    await removeLostEntries(conn, args);
+  } finally {
+    rdb.close(conn);
+  }
+};
 
-      return await query.lookup(new URL(url));
-    } finally {
-      if(!cacheless) {
-        query.cache.close();
-      }
+cli.removeOrphanedEntries = async function() {
+  let conn;
+  try {
+    conn = await rdb.open();
+    await removeOrphanedEntries(conn, args);
+  } finally {
+    rdb.close(conn);
+  }
+};
+
+cli.clearFavicons = async function() {
+  const fic = new FaviconCache();
+  try {
+    await fic.open();
+    await fic.clear();
+  } finally {
+    fic.close();
+  }
+};
+
+cli.lookupFavicon = async function(url, timeout, cacheless = true) {
+  const query = new FaviconLookup();
+  query.cache = new FaviconCache();
+  query.fetchHTMLTimeoutMs = timeout;
+
+  try {
+    if(!cacheless) {
+      await query.cache.open();
     }
 
-    break;
+    return await query.lookup(new URL(url));
+  } finally {
+    if(!cacheless) {
+      query.cache.close();
+    }
   }
-  default:
-    console.error('Unknown command', command);
-    printUsage();
-    break;
-  }
-}
+};
 
-// TODO: reorder alphabetically
-
-function printUsage() {
-  console.debug('Commands:', [
-    'refreshicons',
-    'archive',
-    'poll',
-    'scanlost',
-    'scanorphans',
-    'clearicons',
-    'iconlookup'
-  ]);
-}
-
-
+// Modules are basically wrapped in a promise. To enable variables to be accessible from the
+// console, which can only see global variables, and not "exported" variables from modules, the
+// values must be defined "really" globally. Right now using window seems to work. This is not a
+// recommended practice, but I think this is an exception because calling this from the console is
+// the entire point of this module.
 if(window) {
-  window.execCommand = execCommand;
+  window.cli = cli;
+} else {
+  console.warn('cli unavailable (no window)');
 }
