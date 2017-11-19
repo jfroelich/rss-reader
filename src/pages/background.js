7c3259ae48ccbe7b3336cf90384d94d76626e9db
Josh <joshua.froelich@gmail.com>
2017-11-19 18:46:47 -0500

    Merged install.js into background.js

--- src/pages/background.js
+++ src/pages/background.js
@@ -2,18 +2,12 @@
 
 import "/src/alarms.js";
 import "/src/cli.js";
-import {addBadgeClickListener, showSlideshowTab} from "/src/extension.js";
-import "/src/install.js";
+import {addBadgeClickListener, addInstallListener, showSlideshowTab} from "/src/extension.js";
+import FaviconCache from "/src/favicon/cache.js";
 import * as rdb from "/src/storage/rdb.js";
 import updateBadgeText from "/src/reader/update-badge-text.js";
 
-// Initialize the extension
-async function init() {
-  console.debug('initializing background page');
-
-  addBadgeClickListener(onClicked);
-
-  // Initialize badge text
+async function initBadgeText() {
   let conn;
   try {
     conn = await rdb.open();
@@ -25,9 +19,32 @@ async function init() {
   }
 }
 
+async function onInstalled(event) {
+  console.debug('onInstalled', event);
+
+  // Setup the reader-db database
+  try {
+    await rdb.setup();
+  } catch(error) {
+    console.warn(error);
+  }
+
+  // Setup the favicon database
+  const fic = new FaviconCache();
+  try {
+    await fic.setup();
+  } catch(error) {
+    console.warn(error);
+  }
+}
+
 async function onClicked(event) {
   showSlideshowTab();
 }
 
-// Run everytime the background page is loaded
-init();
+
+console.debug('initializing background page');
+
+addInstallListener(onInstalled);
+addBadgeClickListener(onClicked);
+initBadgeText();
