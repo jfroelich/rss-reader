7145fd2a7fed47d0ec40a6108fc0aeaec93c8cc0
Josh <joshua.froelich@gmail.com>
2017-11-21 12:55:41 -0500

    Changed assert to always call sprintf, even when condition is truthy. This is a temporary change to expose any simple issues given how rare assert failures are.

--- src/assert.js
+++ src/assert.js
@@ -2,17 +2,23 @@
 
 import sprintf from "/src/utils/sprintf.js";
 
-// TODO: change assert to accept varargs instead of message once sprintf.js settles
-
 // If true, any assertion errors are immediately logged. This helps avoid issues with promise-
-// swallowed exceptions
+// swallowed exceptions. This is tested against every time function is called.
 const LOG_ERRORS = true;
-// If true, assertion errors are thrown. If false, assertion errors are simply logged.
+// If true, assertion errors are thrown. If false, assertion errors are simply logged. This is
+// testeds against when this module is loaded.
 const ASSERTIONS_ENABLED = true;
 
 function assert(condition, ...varargs) {
+
+  // TODO: once it seems like sprintf is always working, only call if !condition. But for now,
+  // always call, so that any error situation shows up really clearly. This will degrade the
+  // performance of pretty much every single function in the entire app, but I want to find any
+  // errors.
+  const formattedMessage = sprintf(...varargs);
+
   if(!condition) {
-    const formattedMessage = sprintf(...varargs);
+
     const error = new AssertionError(formattedMessage);
     if(LOG_ERRORS) {
       console.error(error);
