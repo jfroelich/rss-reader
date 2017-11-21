689840e2388f57ddecfabf005e6f768dc673d7c4
Josh <joshua.froelich@gmail.com>
2017-11-20 22:25:54 -0500

    Minor comment change

--- src/assert.js
+++ src/assert.js
@@ -1,12 +1,5 @@
 // Assertions library
 
-// TODO: should I demand or warn when condition is not boolean? Something about using
-// monomorphic functions. http://mrale.ph/blog/2015/01/11/whats-up-with-monomorphism.html
-// The idea is that I want to signal to v8 to inline. It would be interesting to learn more about
-// how to better signal intent to the interpreter. On the otherhand, what does type restriction
-// really buy? Less caller convenience. Mralpheph's article talks about objects but I wonder if
-// the same idea applies to functions.
-
 // If true, any assertion errors are immediately logged. This helps avoid issues with promise-
 // swallowed exceptions
 const LOG_ERRORS = true;
