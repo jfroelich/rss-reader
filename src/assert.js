10193cef07f2fab7dc17aeb0e0f199bf1921f3c3
Josh <joshua.froelich@gmail.com>
2017-11-21 19:35:30 -0500

    Only call sprintf when assert fails

--- src/assert.js
+++ src/assert.js
@@ -10,15 +10,8 @@ const LOG_ERRORS = true;
 const ASSERTIONS_ENABLED = true;
 
 function assert(condition, ...varargs) {
-
-  // TODO: once it seems like sprintf is always working, only call if !condition. But for now,
-  // always call, so that any error situation shows up really clearly. This will degrade the
-  // performance of pretty much every single function in the entire app, but I want to find any
-  // errors.
-  const formattedMessage = sprintf(...varargs);
-
   if(!condition) {
-
+    const formattedMessage = sprintf(...varargs);
     const error = new AssertionError(formattedMessage);
     if(LOG_ERRORS) {
       console.error(error);
