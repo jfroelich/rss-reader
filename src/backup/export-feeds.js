3b95672fd4e72f20f0c6eab6b66728cf39091941
Josh <joshua.froelich@gmail.com>
2017-11-23 21:56:39 -0500

    Added a note about deprecating outline objects

--- src/backup/export-feeds.js
+++ src/backup/export-feeds.js
@@ -3,6 +3,17 @@ import * as Feed from "/src/storage/feed.js";
 import * as OPMLDocument from "/src/opml/document.js";
 import {xmlToBlob} from "/src/xml/utils.js";
 
+// TODO: for both backup and opml modules, drop support for opml outline objects. Instead,
+// work exclusively with elements. However, maintain a separation between generic opml
+// functionality, and functionality specific to this app. It is ok for backup to have
+// knowledge of the feed model, but not for opml. So, in order for this to, for example,
+// append a feed to an opml document, it has to be able to create the outline element here,
+// and call OPMLDocument.appendOutlineElement instead of appendOutlineObject.
+// The current behavior is to create an outline object representing the feed, then call
+// append, which converts the outline object to an outline element, and then appends the
+// outline element.  Instead, directly create the element.
+
+
 // Triggers the download of an OPML-formatted file containing the given feeds
 // @param feeds {Array}
 // @param title {String} optional
