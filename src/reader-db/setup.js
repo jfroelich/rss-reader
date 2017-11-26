dca60d2c4a64df3cb20b67ed16859dcc7806e1ba
Josh <joshua.froelich@gmail.com>
2017-11-26 11:58:52 -0500

    Fixed incorrect import statement in setup.js

--- src/reader-db/setup.js
+++ src/reader-db/setup.js
@@ -1,11 +1,11 @@
-import open as openDb from "/src/reader-db/open.js";
+import open from "/src/reader-db/open.js";
 import {close as closeDb} from "/src/utils/indexeddb-utils.js";
 
 // Create the reader-db database
 export default async function main() {
   let conn;
   try {
-    conn = await openDb();
+    conn = await open();
   } finally {
     closeDb(conn);
   }
