de691c8f047a9db0c90bc04b39bc5acc717efa0e
Josh <joshua.froelich@gmail.com>
2017-11-28 12:32:30 -0500

    Added a note to is-unchecked-error.js

--- src/utils/is-unchecked-error.js
+++ src/utils/is-unchecked-error.js
@@ -1,6 +1,11 @@
 import {AssertionError} from "/src/assert/assert.js";
 
-// Misc errors and error-related utilities
+// TODO: what if I create an error subclass, CheckedError. Change all errors to extend from
+// CheckedError except for AssertionError. Then change isUncheckedError's logic. Instead of checking
+// an enumerated set of known unchecked errors, this instead does a single test against whether the
+// error is an instance of a checked error, and returns false in that case. Then all non-enumerated
+// errors are considered unchecked. This alleviates the need to manage or be aware of which errors
+// are checked or unchecked, and uses fewer conditions, and is probably simpler to maintain.
 
 // A helper for use in catch blocks that want to re-throw unexpected errors that should not be
 // caught.
