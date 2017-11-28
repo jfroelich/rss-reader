e425278ca325cd36a416f7e6c07112355490b68d
Josh <joshua.froelich@gmail.com>
2017-11-27 21:50:26 -0500

    Created poll-feed.js, moved pollFeed from poll-feeds.js to poll-feed.js

--- src/jobs/poll/poll-feeds.js
+++ src/jobs/poll/poll-feeds.js
@@ -1,12 +1,8 @@
 import assert from "/src/assert.js";
 import {showNotification} from "/src/extension.js";
-import * as Feed from "/src/reader-db/feed.js";
-import fetchFeed from "/src/fetch/fetch-feed.js";
-import * as PollEntryModule from "/src/jobs/poll/poll-entry.js";
+import pollFeed from "/src/jobs/poll/poll-feed.js";
 import updateBadgeText from "/src/reader/update-badge-text.js";
 import getFeedsFromDb from "/src/reader-db/get-feeds.js";
-import parseFeed from "/src/reader/parse-feed.js";
-import putFeed from "/src/reader-db/put-feed.js";
 import promiseEvery from "/src/utils/promise-every.js";
 
 // TODO: in order to make pollFeed directly callable, it needs to still be able to send a
@@ -60,76 +56,4 @@ export async function pollFeeds() {
   const title = 'Added articles';
   const message = 'Added articles';
   showNotification(title, message);
-}
-
-async function pollFeed(feed) {
-  assert(Feed.isFeed(feed));
-  assert(this instanceof PollFeedsContext);
-
-  console.log('Polling feed', Feed.peekURL(feed));
-
-  // If the feed was polled too recently, then exit early.
-  if(!this.ignoreRecencyCheck && feed.dateFetched instanceof Date) {
-    const currentDate = new Date();
-    const elapsedSinceLastPollMs = currentDate - feed.dateFetched;
-
-    // If the amount of time since the feed was last fetched is less than the recency period,
-    // then the feed was fetched too recently.
-
-    if(elapsedSinceLastPollMs < this.recencyPeriodMs) {
-
-      // TEMP: testing whether this is causing no feeds to update when ignoreRecencyCheck
-      // is false.
-      console.debug('Canceling feed poll, fetched too recently,', Feed.peekURL(feed),
-        elapsedSinceLastPollMs, this.recencyPeriodMs);
-      return;
-    }
-  }
-
-  const url = Feed.peekURL(feed);
-
-  const response = await fetchFeed(url, this.fetchFeedTimeoutMs, this.acceptHTML);
-
-  if(!this.ignoreModifiedCheck && feed.dateUpdated && feed.dateLastModified &&
-    response.lastModifiedDate && feed.dateLastModified.getTime() ===
-    response.lastModifiedDate.getTime()) {
-    console.debug('Skipping unmodified feed', url, feed.dateLastModified,
-      response.lastModifiedDate);
-    return;
-  }
-
-  const feedXML = await response.text();
-  const PROCESS_ENTRIES = true;
-  const parseResult = parseFeed(feedXML, url, response.responseURL, response.lastModifiedDate,
-    PROCESS_ENTRIES);
-
-  const mergedFeed = Feed.merge(feed, parseResult.feed);
-  const storedFeed = await putFeed(mergedFeed, this.readerConn);
-  const entries = parseResult.entries;
-
-  // Cascade feed properties to entries
-  for(const entry of entries) {
-    entry.feed = storedFeed.id;
-    entry.feedTitle = storedFeed.title;
-    if(!entry.datePublished) {
-      entry.datePublished = storedFeed.datePublished;
-    }
-  }
-
-  const pec = new PollEntryModule.Context();
-  pec.readerConn = this.readerConn;
-  pec.iconCache = this.iconCache;
-  pec.feedFaviconURL = storedFeed.faviconURLString;
-  pec.fetchHTMLTimeoutMs = this.fetchHTMLTimeoutMs;
-  pec.fetchImageTimeoutMs = this.fetchImageTimeoutMs;
-  const pollEntryPromises = entries.map(PollEntryModule.pollEntry, pec);
-  await promiseEvery(pollEntryPromises);
-
-  // TODO: the badge text call should not occur when no entries have been processed. This needs to
-  // get information back from pollEntry, in the form of an array of resolutions, that ascertains
-  // whether the number of entries added is not zero. Then only call updateBadgeText if the number
-  // is not zero.
-  if(!this.batchMode) {
-    await updateBadgeText(this.readerConn);
-  }
 }
