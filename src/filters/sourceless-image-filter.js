a8071ec5733bd6d4349f04cc357c7c7c25cf71d5
Josh <joshua.froelich@gmail.com>
2017-11-28 22:28:52 -0500

    Minor comments and debugging message changes

--- src/filters/sourceless-image-filter.js
+++ src/filters/sourceless-image-filter.js
@@ -1,17 +1,21 @@
-import {hasSource, removeImage} from "/src/dom/image.js";
 import assert from "/src/assert/assert.js";
+import {hasSource, removeImage} from "/src/dom/image.js";
 
 // Removes images that are missing source information from document content
-
 export default function filter(doc) {
   assert(doc instanceof Document);
   if(!doc.body) {
     return;
   }
 
+  // Use querySelectorAll over getElementsByTagName to simplify removal during iteration.
+
   const images = doc.body.querySelectorAll('img');
   for(const image of images) {
     if(!hasSource(image)) {
+      // TEMP: logging unhandled images for improving lazy/responsive filters
+      console.debug('removing sourceless image', image.outerHTML);
+
       removeImage(image);
     }
   }
