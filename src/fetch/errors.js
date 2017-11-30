8bc2cc3eef6c8bbc5d2bc522dda5923189fa61f1
Josh <joshua.froelich@gmail.com>
2017-11-30 09:11:53 -0500

    Added error descriptions to /src/fetch/errors.js

--- src/fetch/errors.js
+++ src/fetch/errors.js
@@ -1,19 +1,23 @@
 import sprintf from "/src/utils/sprintf.js";
 
-// TODO: describe what these errors represent in a brief comment
-
+// Represents a general class of networking errors, such as unavailability or unreachability of a
+// resource located on a different machine
 export class NetworkError extends Error {
   constructor(...args) {
     super(sprintf(...args) || 'Network error');
   }
 }
 
+// Represents a specific type of networking error where the current computer cannot access any
+// resources on other machines
 export class OfflineError extends NetworkError {
   constructor(...args) {
     super(args.length ? ...args : 'Offline error');
   }
 }
 
+// Represents a general class of errors relating to fetching a resource from another machine, such
+// as requesting the wrong type of resource, or unauthorized, etc.
 export class FetchError extends NetworkError {
   constructor(...args) {
     super(args.length ? ...args : 'Fetch error');
