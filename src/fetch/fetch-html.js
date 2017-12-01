2389853bfc6467409df15ca8b696992f945d1392
Josh <joshua.froelich@gmail.com>
2017-11-30 21:15:20 -0500

    Provide a fallback accept type when requesting html to avoid 406 not accepted responses. The response type is still validated. See issue #406.

--- src/fetch/fetch-html.js
+++ src/fetch/fetch-html.js
@@ -5,10 +5,25 @@ import * as mime from "/src/utils/mime-utils.js";
 // @param url {String} the url to fetch
 // @param timeoutMs {Number} optional, timeout in milliseconds
 export default function fetchHTML(url, timeoutMs) {
+
+  // NOTE: some websites appear to require a fallback */* otherwise a 406 not acceptable response
+  // status code is returned as a result of GET. For a reproducible test case, use the url
+  // http://daringfireball.net/linked/2016/10/31/intel-mbp-ram. By appending the fallback of */*
+  // with a lower priority (that is the "q" below), I no longer get a 406. I do not really have an
+  // explanation for this behavior nor do I fully understand it. But it is now working.
+  // There is that principle of accepting garbage as input on the web, I forget the name of it, this
+  // is probably an example of that. This one Apache server has a distinctive configuration that
+  // is causing this behavior.
+
+  const accept = [
+    mime.MIME_TYPE_HTML,
+    '*/*;q=0.9'
+  ].join(',');
+
   const options = {
     credentials: 'omit',
     method: 'get',
-    headers: {'Accept': mime.MIME_TYPE_HTML},
+    headers: {'Accept': accept},
     mode: 'cors',
     cache: 'default',
     redirect: 'follow',
