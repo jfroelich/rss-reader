14112170010d985248b691811e26e5097a581ee7
Josh <joshua.froelich@gmail.com>
2017-12-01 21:53:33 -0500

    Minor comment and tracing log message change

--- src/filters/canonical-url-filter.js
+++ src/filters/canonical-url-filter.js
@@ -2,8 +2,7 @@ import assert from "/src/assert/assert.js";
 import {parseSrcsetWrapper, serializeSrcset} from "/src/dom/srcset.js";
 import {resolveURLString} from "/src/url/url-string.js";
 
-// TODO: rename this
-const CANONICAL_URL_FILTER_MAP = {
+const ELEMENT_ATTRIBUTE_MAP = {
   a: 'href',
   applet: 'codebase',
   area: 'href',
@@ -31,15 +30,21 @@ const CANONICAL_URL_FILTER_MAP = {
   video: 'src'
 };
 
+// Initialize ELEMENTS_WITH_SRC_SELECTOR once on module load in module scope
+const tags = Object.keys(ELEMENT_ATTRIBUTE_MAP);
+const parts = [];
+for(const tag of tags) {
+  parts.push(`${tag}[${ELEMENT_ATTRIBUTE_MAP[tag]}]`);
+}
+const ELEMENTS_WITH_SRC_SELECTOR = parts.join(',');
+
 // @param doc {Document}
 // @param baseURL {URL}
 export default function filterDocument(doc, baseURL) {
   assert(doc instanceof Document);
   assert(baseURL instanceof URL);
 
-  const srcSelector = createSelector();
-
-  const srcElements = doc.querySelectorAll(srcSelector);
+  const srcElements = doc.querySelectorAll(ELEMENTS_WITH_SRC_SELECTOR);
   for(const srcElement of srcElements) {
     resolveElementAttribute(srcElement, baseURL);
   }
@@ -52,18 +57,8 @@ export default function filterDocument(doc, baseURL) {
   }
 }
 
-// TODO: now that this is modular, statically initialize
-function createSelector() {
-  const tags = Object.keys(CANONICAL_URL_FILTER_MAP);
-  const parts = [];
-  for(const tag of tags) {
-    parts.push(`${tag}[${CANONICAL_URL_FILTER_MAP[tag]}]`);
-  }
-  return parts.join(',');
-}
-
 function resolveElementAttribute(element, baseURL) {
-  const attributeName = CANONICAL_URL_FILTER_MAP[element.localName];
+  const attributeName = ELEMENT_ATTRIBUTE_MAP[element.localName];
   if(!attributeName) {
     return;
   }
@@ -78,7 +73,43 @@ function resolveElementAttribute(element, baseURL) {
     return;
   }
 
+  // TODO: if this is simply appending the slash, maybe it shouldn't. Technically the slash is
+  // implicit, and just a waste of space. Space saving is also a concern. Maybe it should be a
+  // concern of a separate filter that removes unimportant characters from urls, or maybe it should
+  // just also be a concern of this module? Maybe I do something extra here like just check if
+  // path is empty or just a '/', and if so, use .href.substring(-1) or something like that? Maybe
+  // that should be encapsulated in some helper function call too, like getCondensedURLString?
+  // Also, I think URL serialization leaves in '?' even when parameter count is 0, so maybe also
+  // consider that. Also, if I do it here, I should do it for srcset too?
+  // Or wait, maybe this is dumb for certain elements that point to actual resources. For example
+  // image urls pretty much all have paths. So this is really only for things like anchors? Maybe
+  // it really is just too much of a separate concern, but I suppose the aggregate concern is
+  // performance, because I suspect using a separate filter would require re-parsing the url. So I
+  // really should do it here. For that matter, I should be doing it almost anywhere I set an
+  // attribute that contains a url, for those attributes not pointing to files necessarily.
+  // Also, if I do the substring thing, I still want to check if length is not equal to avoid the
+  // cost of setAttribute. So I have to do two length checks. So maybe what I should do is rewrite
+  // this condition to check if lengths equal then return early, otherwise do stuff. The return
+  // early style would be consistent with earlier sections of this function.
+  // Also maybe store resolvedURL.href in a variable like canonicalURLString. It mirrors the
+  // variable originalURLString better, and avoids the possible case that Chrome doesn't do
+  // lazy-setter-call-memoization?
+
+  // TODO: separate idea to explore, what if i have a url-condense-filter that is also site aware
+  // and does things like (if stackoverflow keep only the question id part of the url and strip
+  // the rest)? At this point I am moving way beyond the concerns of canonicalization and in this
+  // sense, it really seems more appropriate to have any condense concerns be apart of this other
+  // filter and not here since I am going to just deal with the perf cost of re-parsing. Or,
+  // maybe performance is the definining concern, and really, I should be using a more general
+  // filter concept like "url-filter.js" that does both canonicalization and condense filters at
+  // once, because performance trumps over concern-based organization of functionality?
+
   if(resolvedURL.href.length !== originalURLString.length) {
+
+    // TEMP: monitoring urls for above todo. This is going to spam but I think I will probably
+    // focus on the todo rather soon.
+    console.debug('canonicalization change', originalURLString, resolvedURL.href);
+
     element.setAttribute(attributeName, resolvedURL.href);
   }
 }
