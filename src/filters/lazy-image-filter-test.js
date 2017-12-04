e13f48d58721071dd02b964fed810d51336e5659
Josh <joshua.froelich@gmail.com>
2017-12-04 13:53:12 -0500

    [content-filters] fixed bug in lazy image filter
    Use getElementsByTagName('img'), not 'image' ... This was a typo due to
    using find and replace in text editor when renaming variables in prior
    commit.
    
    I also added a helper for calling the filter from the console for
    testing purposes.

---
+++ src/filters/lazy-image-filter-test.js
@@ -0,0 +1,21 @@
+import fetchHTML from "/src/fetch/fetch-html.js";
+import filterLazyImages from "/src/filters/lazy-image-filter.js";
+import filterSourcelessImages from "/src/filters/sourceless-image-filter.js";
+import parseHTML from "/src/html/parse.js";
+
+async function test(urlString) {
+  const urlObject = new URL(urlString);
+  const response = await fetchHTML(urlObject);
+  const responseText = await response.text();
+  const document = parseHTML(responseText);
+
+  filterLazyImages(document);
+
+  // Call this subsequently because it prints out missing images
+  filterSourcelessImages(document);
+
+  console.debug('test complete');
+}
+
+
+window.test = test;
--- src/filters/lazy-image-filter.js
+++ src/filters/lazy-image-filter.js
@@ -4,45 +4,58 @@ import {isValidURLString} from "/src/url/url-string.js";
 
 // Transforms lazily-loaded images found in document content
 
-// BUG: This type of message should not have shown up in the log:
-// removing sourceless image <img data-src="url" alt="" content="url">
-// This should have fixed that. I think. What about picture? I need to improve that debugging
-// message so it checks for picture. Suspicion is this is all fallout from switching to
-// hasSource. 
+// TODO: try and determine if an image with a src attribute is using a placeholder image in the
+// src attribute and a full image from another attribute
+
 
 const kLazyAttributeNames = [
   'load-src',
   'data-src',
+  'data-src-full16x9',
+  'data-src-large',
   'data-original-desktop',
   'data-baseurl',
+  'data-flickity-lazyload',
   'data-lazy',
+  'data-path',
   'data-image-src',
   'data-original',
   'data-adaptive-image',
   'data-imgsrc',
-  'data-default-src'
+  'data-default-src',
+  'data-hi-res-src'
 ];
 
 export default function filter(doc) {
   assert(doc instanceof Document);
-
   if(!doc.body) {
     return;
   }
 
-  const images = doc.body.getElementsByTagName('image');
+  // Aha, this was the culprit of the bug! This was searching for 'image' not 'img', which I
+  // inadvertantly set when changing use of img to image in variable names using find and replace
+  // in code editor.
+  const images = doc.body.getElementsByTagName('img');
+
   for(const image of images) {
     if(hasSource(image)) {
       continue;
     }
 
+    const attributeNames = image.getAttributeNames();
+
     for(const lazySourceName of kLazyAttributeNames) {
-      if(image.hasAttribute(lazySourceName)) {
-        const imageSource = image.getAttribute(lazySourceName);
-        if(isValidURLString(imageSource)) {
+      if(attributeNames.includes(lazySourceName)) {
+        const lazySourceAttributeValue = image.getAttribute(lazySourceName);
+        if(isValidURLString(lazySourceAttributeValue)) {
           image.removeAttribute(lazySourceName);
-          image.setAttribute('src', imageSource);
+          image.setAttribute('src', lazySourceAttributeValue);
+
+          console.debug('lazy transform', image.outerHTML);
+
           break;
+        } else {
+          console.debug('found lazy attribute but its value was invalid', image.outerHTML);
         }
       }
     }
