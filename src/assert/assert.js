d32f8360d8a18eb2fd70415f179435c89fd0d562
Josh <joshua.froelich@gmail.com>
2017-12-06 10:47:44 -0500

    [assert] Removed weak assertions
    Removed ability to switch to weak asserts. Added a bunch of clarifying
    comments.

--- src/assert/assert.js
+++ src/assert/assert.js
@@ -1,13 +1,18 @@
 import sprintf from "/src/string/sprintf.js";
 
-// If true, any assertion errors are immediately logged. This helps avoid issues with promise-
-// swallowed exceptions. This is tested against every time function is called.
+// If true, any assertion errors are immediately logged. This helps avoid issues with promise
+// swallowed exceptions. This is tested against every time the assert function is called.
+// Currently this is true in development to really emphasize unexpected errors and make an earnest
+// attempt at avoiding any hidden errors.
 const LOG_ERRORS = true;
-// If true, assertion errors are thrown. If false, assertion errors are simply logged. This is
-// testeds against when this module is loaded.
-const ASSERTIONS_ENABLED = true;
 
-function assert(condition, ...varargs) {
+// Throws an assertion error when the condition value is false-like
+// @param condition {Any} any value, usually the result of some expression
+// @rest any number of any type of additional arguments that are forwarded to a call to
+// sprintf that formats the arguments into a string that becomes the message value of the
+// assertion error that is thrown. If no additional arguments are given then a default error
+// message is used.
+export default function assert(condition, ...varargs) {
   if(!condition) {
     const formattedMessage = sprintf(...varargs);
     const error = new AssertionError(formattedMessage);
@@ -18,7 +23,17 @@ function assert(condition, ...varargs) {
   }
 }
 
-export default ASSERTIONS_ENABLED ? assert : console.assert;
+// An assertion error indicates something went really wrong in an unexpected way. For example,
+// a variable was of the wrong type, or a variable was in an unexpected state. Assertion errors
+// indicate that something that should never happen ... happened. It primarily indicates that
+// the reasoning used somewhere in the collective program is incorrect, that a faulty assumption
+// was made somewhere about the state of things.
+
+// Generally no other module should import or explicitly throw an AssertionError. Instead, that
+// module should call assert with a value that then throws an assertion error as a side effect
+// when the value is falsy. Ideally this error would not be exported. However, a few other
+// modules need to directly access the class itself such as when testing whether an error is a type
+// of an assertion error.
 
 export class AssertionError extends Error {
   constructor(message) {
