3c0de8840fc89dcaa5358816d90e83f010903c78
Josh <joshua.froelich@gmail.com>
2017-12-12 09:45:58 -0500

    [content-filters] Created tests for coerce-element.js

---
+++ src/dom/coerce-element-test.js
@@ -0,0 +1,19 @@
+import coerceElement from "/src/dom/coerce-element.js";
+import parseHTML from "/src/html/parse.js";
+
+let input = '<html><head></head><body><a></a></body></html>';
+let doc = parseHTML(input);
+let anchors = doc.querySelectorAll('a');
+for(let a of anchors) {
+  coerceElement(a, 'b', true);
+}
+console.assert(doc.documentElement.outerHTML ===
+  '<html><head></head><body><b></b></body></html>');
+let bs = doc.querySelectorAll('b');
+for(let b of bs) {
+  coerceElement(b, 'c', true);
+}
+console.assert(doc.documentElement.outerHTML ===
+  '<html><head></head><body><c></c></body></html>');
+
+console.log('If no assertion error messages appear before this message then all tests passed');
