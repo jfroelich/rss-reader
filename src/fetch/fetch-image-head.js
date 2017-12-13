ade342b7994c1cdb646400fc3d3f2ded30558b08
Josh <joshua.froelich@gmail.com>
2017-12-13 07:47:58 -0500

    [fetch] Add policy check to fetchImageHead
    fetchImageHead does not currently use fetchInternals, so I added an
    explicit isAllowedURL call as part of issue #420

--- src/fetch/fetch-image-head.js
+++ src/fetch/fetch-image-head.js
@@ -1,16 +1,16 @@
 import assert from "/src/assert/assert.js";
 import {FetchError} from "/src/fetch/errors.js";
 import fetchWithTimeout from "/src/fetch/fetch-with-timeout.js";
+import isAllowedURL from "/src/fetch/fetch-policy.js";
 import * as FetchUtils from "/src/fetch/utils.js";
-import check from "/src/utils/check.js";
 import * as MimeUtils from "/src/mime/utils.js";
+import {PermissionsError} from "/src/operations/restricted-operation.js";
+import check from "/src/utils/check.js";
 
 // TODO: this should be refactored to use fetchInternal. But I need to calculate content length.
 // So fetchInternal first needs to be refactored to also calculate content length because response
 // is not exposed, just wrapped response.
 // TODO: if not using fetchInternal, sanity check timeoutMs
-// TODO: set fetch properties using same style as in other fetch helper functions
-// TODO: if not using fetchInternal, create and use ResponseWrapper?
 
 // Sends a HEAD request for the given image.
 // This currently does not do any byte inspection, only mime type acceptance check, which causes
@@ -20,18 +20,26 @@ import * as MimeUtils from "/src/mime/utils.js";
 export default async function fetchImageHead(url, timeoutMs) {
   assert(url instanceof URL);
 
-  const headers = {Accept: 'image/*'};
-  const options = {};
-  options.credentials = 'omit';
-  options.method = 'HEAD';
-  options.headers = headers;
-  options.mode = 'cors';
-  options.cache = 'default';
-  options.redirect = 'follow';
-  options.referrer = 'no-referrer';
+  // Because this function does not go through fetchInternal currently, it skips the policy check
+  // that applies to most other fetch functionality. So explicitly perform the check here to comply
+  // with the general warranty that all fetch functionality meets policy constraints. If and when
+  // this correctly uses fetchInternal then this check becomes implicit and is no longer needs here
+  check(isAllowedURL(url), PermissionsError, 'Refused to fetch url', url);
+
+
+  const headers = {accept: 'image/*'};
+  const options = {
+    credentials: 'omit',
+    method: 'head',
+    headers: headers,
+    mode: 'cors',
+    cache: 'default',
+    redirect: 'follow',
+    referrer: 'no-referrer'
+  };
 
   const response = await fetchWithTimeout(url, options, timeoutMs);
-  assert(response);
+  assert(typeof response !== 'undefined');
 
   // TODO: this could probably be expressed in a clearer way, for now I am hackishly adding
   // support for application/octet-stream to stop favicon lookup from failing on certain websites
@@ -48,6 +56,6 @@ export default async function fetchImageHead(url, timeoutMs) {
 function isOtherAcceptableMimeType(contentType) {
   // To support twitch.tv, support 'application/octet-stream'
   const mimeType = MimeUtils.fromContentType(contentType);
-  const otherTypes = ['application/octet-stream'];
-  return otherTypes.includes(mimeType);
+  const types = ['application/octet-stream'];
+  return types.includes(mimeType);
 }
