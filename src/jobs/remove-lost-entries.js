3436ae8dff09102ff7b17effa7e38e22ce3d72b3
Josh <joshua.froelich@gmail.com>
2017-12-21 12:41:09 -0500

    Remove deprecated file remove-lost-entries.js
    Forgot to remove it in prior commit.

--- src/jobs/remove-lost-entries.js
+++
@@ -1,5 +0,0 @@
-import assert from "/src/assert/assert.js";
-import FeedStore from "/src/feed-store/feed-store.js";
-import * as Entry from "/src/feed-store/entry.js";
-
-// TODO: move into feed store
