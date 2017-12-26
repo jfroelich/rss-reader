4219762bf8ca1dc5eabd51fd62a9b3999b36f66a
Josh <joshua.froelich@gmail.com>
2017-12-26 13:53:12 -0500

    [fetch] fetchHTML simplifications
    Removed assertion as it is delegated. No longer set Accept header since
    I had to earlier accept */* anyway to get around a 406 error. Hard code
    the mime type as a string, mime types are not really magic and do not
    require extra constants as they are self-documenting.

--- src/fetch/fetch-html.js
+++ src/fetch/fetch-html.js
@@ -1,27 +1,16 @@
-import assert from "/src/utils/assert.js";
 import {fetchInternal} from "/src/fetch/utils.js";
-import * as MimeUtils from "/src/utils/mime-utils.js";
+
+// TODO: this is now so simple it should maybe just be inlined
 
 // Fetches the html content of the given url
 // @param url {URL} request url
 // @param timeoutMs {Number} optional, in milliseconds, how long to wait before considering the
 // fetch to be a failure.
 export default function fetchHTML(url, timeoutMs) {
-  assert(url instanceof URL);
-
-  // TODO: because I have to accept */*, specifying an Accept header is dumb
-
-  // NOTE: accept */* to fix issue #271 and avoid 406 response code
-  const acceptHeaderValue = [
-    MimeUtils.MIME_TYPE_HTML,
-    '*/*;q=0.9'
-  ].join(',');
-
   const options = {
-    headers: {accept: acceptHeaderValue},
     timeout: timeoutMs
   };
 
-  const acceptedMimeTypes = [MimeUtils.MIME_TYPE_HTML];
+  const acceptedMimeTypes = ['text/html'];
   return fetchInternal(url, options, acceptedMimeTypes);
 }
