f804c10a959c3e5cc4cb0cd87dfd6279c9b76197
Josh <joshua.froelich@gmail.com>
2017-12-26 12:04:37 -0500

    [fetch] Merged fetchWithTimeout into utils.js

--- src/fetch/utils.js
+++ src/fetch/utils.js
@@ -1,14 +1,14 @@
 import assert from "/src/utils/assert.js";
-import {FetchError} from "/src/fetch/errors.js";
+import {FetchError, NetworkError, OfflineError} from "/src/fetch/errors.js";
 import isAllowedURL, {PermissionsError} from "/src/fetch/fetch-policy.js";
-import fetchWithTimeout from "/src/fetch/fetch-with-timeout.js";
-import {compareURLsWithoutHash} from "/src/utils/url-utils.js";
-import {isValidURLString} from "/src/utils/url-string-utils.js";
+import formatString from "/src/utils/format-string.js";
 import isPosInt from "/src/utils/is-pos-int.js";
 import * as MimeUtils from "/src/utils/mime-utils.js";
-import {setTimeoutPromise} from "/src/utils/promise-utils.js";
 import parseInt10 from "/src/utils/parse-int-10.js";
-import formatString from "/src/utils/format-string.js";
+import * as PromiseUtils from "/src/utils/promise-utils.js";
+import TimeoutError from "/src/utils/timeout-error.js";
+import {compareURLsWithoutHash} from "/src/utils/url-utils.js";
+import {isValidURLString} from "/src/utils/url-string-utils.js";
 
 // TODO: given that most fetches use the same options, I should use default options, and then
 // override only explicit options. Then all the callers don't need to specify the other defaults
@@ -36,7 +36,6 @@ export async function fetchInternal(url, options, timeoutMs, acceptedMimeTypes) 
 
   // TODO: rather than pass along options, create a default options object here, and then
   // copy over only options specified by the caller
-
 
   const response = await fetchWithTimeout(url, options, timeoutMs);
   assert(response instanceof Response);
@@ -98,10 +97,81 @@ export async function fetchInternal(url, options, timeoutMs, acceptedMimeTypes) 
   const responseURLObject = new URL(response.url);
   responseWrapper.redirected = detectURLChanged(url, responseURLObject);
 
-
-
   return responseWrapper;
 }
+
+
+// @param url {URL} request url
+// @param options {Object} optional, fetch options parameter
+// @param timeoutMs {Number} optional, timeout in milliseconds
+// @returns {Promise} the fetch promise
+async function fetchWithTimeout(url, options, timeoutMs) {
+  // Accepting a url ensures the url is canonical and thereby avoids allowing fetch to implicitly
+  // resolve a relative url string
+  assert(url instanceof URL);
+  // Avoid the TypeError fetch throws for invalid options, treat it as an assertion error
+  assert(typeof options === 'undefined' || typeof options === 'object');
+  const untimed = typeof timeoutMs === 'undefined';
+  assert(untimed || isPosInt(timeoutMs));
+
+  // Avoid the TypeError fetch throws when offline, and distinguish this type of network error from
+  // other network errors. TypeErrors are unexpected and represent permanent programming errors.
+  // Being offline is an expected and temporary error.
+  if(!navigator.onLine) {
+    const message = formatString('Offline when fetching', url);
+    throw new OfflineError(message);
+  }
+
+  const fetchPromise = fetch(url.href, options);
+  let timeoutId;
+
+  // If a timeout was specified, initialize a derived promise to the result of racing fetch
+  // against timeout. Otherwise, initialize a derived promise to the result of fetch.
+  let aggregatePromise;
+  if(untimed) {
+    aggregatePromise = fetchPromise;
+  } else {
+    let timeoutPromise;
+    [timeoutId, timeoutPromise] = PromiseUtils.setTimeoutPromise(timeoutMs);
+    const contestants = [fetchPromise, timeoutPromise];
+    aggregatePromise = Promise.race(contestants);
+  }
+
+  let response;
+  try {
+    response = await aggregatePromise;
+  } catch(error) {
+    throwTranslated(url, error);
+  }
+
+  // If fetch wins then response is defined.
+  // If timeout wins then response is undefined.
+  // If fetch errors, that was trapped above
+  // Timeout never errors.
+
+  if(response) {
+    clearTimeout(timeoutId);
+  } else {
+    // TODO: cancel/abort the fetch, if that is possible
+    const errorMessage = formatString('Fetch timed out for url', url);
+    throw new TimeoutError(errorMessage);
+  }
+  return response;
+}
+
+function throwTranslated(url, error) {
+  // fetch rejects with a TypeError when a network error is encountered, or when the url contains
+  // credentials. It could also be a timeout, but that is a native timeout.
+  if(error instanceof TypeError) {
+    const message = formatString('Failed to fetch %s because of a network error', url, error);
+    throw new NetworkError(message);
+  } else {
+    // TODO: when does this ever happen?
+    console.warn('Unknown error type thrown by fetch', error);
+    throw error;
+  }
+}
+
 
 // Return true if the response url is 'different' than the request url
 // @param requestURL {URL}
