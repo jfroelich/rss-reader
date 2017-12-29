d7487725138532d949e4aefc24a85947cd87d9b0
Josh <joshua.froelich@gmail.com>
2017-12-29 09:35:58 -0500

    Moved fadeElement to options-page.js
    It was only place it was called. Part of series of changes focusing on
    increasing functional coherency.

--- src/pages/options-page.js
+++ src/pages/options-page.js
@@ -1,6 +1,5 @@
 import assert from "/src/common/assert.js";
 import * as Config from "/src/common/config.js";
-import fadeElement from "/src/utils/dom/fade-element.js";
 import FeedStore from "/src/feed-store/feed-store.js";
 import htmlTruncate from "/src/utils/html/truncate.js";
 import * as PageStyle from "/src/page-style/page-style-settings.js";
@@ -809,3 +808,36 @@ const extURLElement = document.getElementById('extension-homepage');
 extURLElement.textContent = manifest.homepage_url;
 
 showSectionById('subs-list-section');
+
+
+
+
+// Duration and delay can be integer or floats and are required.
+function fadeElement(element, durationSecs, delaySecs) {
+  return new Promise(function executor(resolve, reject) {
+    assert(element instanceof Element);
+    assert(element.style instanceof CSSStyleDeclaration);
+    assert(typeof durationSecs === 'number');
+    assert(typeof delaySecs === 'number');
+
+    const style = element.style;
+    if(style.display === 'none') {
+      // If the element is hidden, it may not have an opacity set. When fading in the element
+      // by setting opacity to 1, it has to change from 0 to work.
+      style.opacity = '0';
+
+      // If the element is hidden, and its opacity is 0, make it eventually visible
+      style.display = 'block';
+    } else {
+      // If the element is visible, and we plan to hide it by setting its opacity to 0, it has
+      // to change from opacity 1 for fade to work
+      style.opacity = '1';
+    }
+
+    element.addEventListener('webkitTransitionEnd', resolve, {'once': true});
+
+    // property duration function delay
+    style.transition = `opacity ${durationSecs}s ease ${delaySecs}s`;
+    style.opacity = style.opacity === '1' ? '0' : '1';
+  });
+}
