01abfa5983602390508cd8a598c4df4ed8836a9f
Josh <joshua.froelich@gmail.com>
2017-12-29 14:57:23 -0500

    Deprecated url-string-utils.js

--- src/favicon/lookup.js
+++ src/favicon/lookup.js
@@ -3,7 +3,7 @@ import * as FetchUtils from "/src/common/fetch-utils.js";
 import FaviconCache from "/src/favicon/cache.js";
 import parseHTML from "/src/utils/html/parse.js";
 import {isUncheckedError} from "/src/common/error-utils.js";
-import {resolveURLString} from "/src/utils/url-string-utils.js";
+
 
 const dprintf = function(){}; // console.debug;
 
@@ -423,4 +423,38 @@ FaviconLookup.prototype.onLookupFailure = function(originURL, entry) {
 function setURLHrefProperty(url, newHrefString) {
   const guardURL = new URL(newHrefString);
   url.href = guardURL.href;
+}
+
+// TODO: maybe inline
+function resolveURLString(urlString, baseURL) {
+  assert(baseURL instanceof URL);
+
+  // Allow for bad input for caller convenience
+  // If the url is not a string (e.g. undefined), return undefined
+  if(typeof urlString !== 'string') {
+    return;
+  }
+
+  // Check if urlString is just whitespace. If just whitespace, then return undefined. This departs
+  // from the behavior of the URL constructor, which tolerates an empty or whitespace string as
+  // input. The url constructor in that case will create a new URL from the base url exclusively.
+  // That is misleading for this purpose.
+
+  // If the length of the string is 0 then return undefined
+  if(!urlString) {
+    return;
+  }
+
+  // If the trimmed length of the string is 0 then return undefined
+  if(!urlString.trim()) {
+    return;
+  }
+
+  let canonicalURL;
+  try {
+    canonicalURL = new URL(urlString, baseURL);
+  } catch(error) {
+    // Ignore
+  }
+  return canonicalURL;
 }
--- src/feed-poll/filters/canonical-url-filter.js
+++ src/feed-poll/filters/canonical-url-filter.js
@@ -1,6 +1,5 @@
 import assert from "/src/common/assert.js";
 import {parseSrcsetWrapper} from "/src/utils/dom/parse-srcset.js";
-import {resolveURLString} from "/src/utils/url-string-utils.js";
 
 const ELEMENT_ATTRIBUTE_MAP = {
   a: 'href',
@@ -152,6 +151,39 @@ function resolveSrcset(element, baseURL) {
   }
 }
 
+// TODO: maybe inline
+function resolveURLString(urlString, baseURL) {
+  assert(baseURL instanceof URL);
+
+  // Allow for bad input for caller convenience
+  // If the url is not a string (e.g. undefined), return undefined
+  if(typeof urlString !== 'string') {
+    return;
+  }
+
+  // Check if urlString is just whitespace. If just whitespace, then return undefined. This departs
+  // from the behavior of the URL constructor, which tolerates an empty or whitespace string as
+  // input. The url constructor in that case will create a new URL from the base url exclusively.
+  // That is misleading for this purpose.
+
+  // If the length of the string is 0 then return undefined
+  if(!urlString) {
+    return;
+  }
+
+  // If the trimmed length of the string is 0 then return undefined
+  if(!urlString.trim()) {
+    return;
+  }
+
+  let canonicalURL;
+  try {
+    canonicalURL = new URL(urlString, baseURL);
+  } catch(error) {
+    // Ignore
+  }
+  return canonicalURL;
+}
 
 
 // @param descriptors {Array} an array of descriptors such as those produced by parseSrcset
