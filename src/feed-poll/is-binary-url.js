16c2ee89794e42745a8a3ddb29d297c27ae5597d
Josh <joshua.froelich@gmail.com>
2017-12-29 10:08:53 -0500

    Moved getTypeForExtension to is-binary-url.js
    It was the only place it was used. Part of series of changes focusing on
    moving from logical coherency to functional coherency.

--- src/feed-poll/is-binary-url.js
+++ src/feed-poll/is-binary-url.js
@@ -25,13 +25,28 @@ export default function isBinaryURL(url) {
     return false;
   }
 
-  const mimeType = MimeUtils.getTypeForExtension(extension);
+  const mimeType = findMimeTypeForExtension(extension);
   if(!mimeType) {
     return false;
   }
 
   return isBinaryMimeType(mimeType);
 }
+
+
+// Return a mime type corresponding a file name extension
+// @param extension {String}
+// @returns {String} a mime type, or undefined on error or failed lookup
+function findMimeTypeForExtension(extension) {
+  const extensionVarType = typeof extension;
+  if(extensionVarType === 'undefined' || extension === null) {
+    return;
+  }
+
+  assert(extensionVarType === 'string');
+  return EXTENSION_TYPE_MAP[extension];
+}
+
 
 // Extracts the mime type of a data uri as string. Returns undefined if not found or invalid.
 function findMimeTypeInDataURL(url) {
@@ -131,3 +146,102 @@ function isBinaryMimeType(mimeType) {
     return false;
   }
 }
+
+
+const EXTENSION_TYPE_MAP = {
+  ai:   'application/postscript',
+  aif:  'audio/aiff',
+  atom: 'application/atom+xml',
+  avi:  'video/avi',
+  bin:  'application/octet-stream',
+  bmp:  'image/bmp',
+  c:    'text/plain',
+  cc:   'text/plain',
+  cgi:  'text/html',
+  'class':'application/java',
+  com:  'application/octet-stream',
+  cpp:  'text/plain',
+  css:  'text/css',
+  doc:  'application/msword',
+  docx: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  eml:  'message/rfc822',
+  eps:  'application/postscript',
+  exe:  'application/octet-stream',
+  flac: 'audio/flac',
+  fli:  'video/fli',
+  gif:  'image/gif',
+  gz:   'application/gzip',
+  h:    'text/plain',
+  htm:  'text/html',
+  html: 'text/html',
+  ico:  'image/x-icon',
+  ics:  'text/calendar',
+  java: 'text/plain',
+  jfif: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  jpg:  'image/jpeg',
+  js:   'application/javascript',
+  json: 'application/json',
+  jsp:  'text/html',
+  log:  'text/plain',
+  m3u8: 'application/x-mpegurl',
+  m4a:  'audio/x-m4a',
+  m4v:  'video/mp4',
+  md:   'text/plain',
+  mth:  'multipart/related',
+  mhtml:'multipart/related',
+  midi: 'audio/midi',
+  mov:  'video/quicktime',
+  mp2:  'audio/mpeg',
+  mp3:  'audio/mpeg',
+  mp4:  'video/mp4',
+  mpeg: 'video/mpeg',
+  mpg:  'video/mpeg',
+  oga:  'audio/ogg',
+  ogg:  'audio/ogg',
+  ogm:  'video/ogg',
+  ogv:  'video/ovg',
+  opus: 'audio/ogg',
+  pdf:  'application/pdf',
+  php:  'text/html',
+  pjp:  'image/jpeg',
+  pjpeg:'image/jpeg',
+  pl:   'text/html',
+  png:  'image/x-png',
+  pps:  'application/vnd.ms-powerpoint',
+  ppt:  'application/vnd.ms-powerpoint',
+  pptx: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  ps:   'application/postscript',
+  rar:  'application/octet-stream',
+  rdf:  'application/rdf+xml',
+  rss:  'application/rss+xml',
+  sh:   'text/x-sh',
+  shtm: 'text/html',
+  shtml:'text/html',
+  svg:  'image/svg+xml',
+  svgz: 'image/svg+xml',
+  swf:  'application/x-shockwave-flash',
+  swl:  'application/x-shockwave-flash',
+  tar:  'application/x-tar',
+  text: 'text/plain',
+  tif:  'image/tiff',
+  tiff: 'image/tiff',
+  tgz:  'application/gzip',
+  txt:  'text/plain',
+  wav:  'audio/wav',
+  webm: 'audio/webm',
+  webp: 'image/webp',
+  woff: 'application/font-woff',
+  xbl:  'text/xml',
+  xht:  'application/xhtml+xml',
+  xhtm: 'application/xhtml+xml',
+  xhtml:'application/xhtml+xml',
+  xmb:  'image/x-xbitmap',
+  xls:  'application/vnd.ms-excel',
+  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  xml:  'text/xml',
+  xsl:  'text/xml',
+  xslt: 'text/xml',
+  xul:  'application/vnd.mozilla.xul+xml',
+  zip:  'application/zip'
+};
