402c39d193bddecfda7c2de9e00a1224762a3f20
Josh <joshua.froelich@gmail.com>
2017-12-29 15:52:14 -0500

    Merged cli into background-page and moved to src

--- src/background-page.js
+++ src/background-page.js
@@ -1,6 +1,4 @@
 import showSlideshowTab from "/src/show-slideshow-tab.js";
-
-import "/src/background-page/cli.js";
 import FaviconCache from "/src/favicon/cache.js";
 import FaviconLookup from "/src/favicon/lookup.js";
 import FeedPoll from "/src/feed-poll/poll-feeds.js";
@@ -131,6 +129,109 @@ function addBadgeClickListener(listener) {
   chrome.browserAction.onClicked.addListener(listener);
 }
 
+
+const cli = {};
+
+cli.refreshIcons = async function() {
+  const fs = new FeedStore();
+  const fc = new FaviconCache();
+  const promises = [fs.open(), fc.open()];
+
+  try {
+    await Promise.all(promises);
+    await fs.refreshFeedIcons(fc);
+  } finally {
+    fs.close();
+    fc.close();
+  }
+};
+
+cli.archiveEntries = async function(limit) {
+  const store = new FeedStore();
+  let maxAgeMs;
+  try {
+    await store.open();
+    await store.archiveEntries(maxAgeMs, limit);
+  } finally {
+    store.close();
+  }
+};
+
+cli.pollFeeds = async function() {
+  const poll = new FeedPoll();
+  poll.init();
+  poll.ignoreRecencyCheck = true;
+  poll.ignoreModifiedCheck = true;
+
+  try {
+    await poll.open();
+    await poll.pollFeeds();
+  } finally {
+    poll.close();
+  }
+};
+
+cli.removeLostEntries = async function(limit) {
+  const store = new FeedStore();
+  try {
+    await store.open();
+    await store.removeLostEntries(limit);
+  } finally {
+    store.close();
+  }
+};
+
+cli.removeOrphanedEntries = async function(limit) {
+  const store = new FeedStore();
+  try {
+    await store.open();
+    await store.removeOrphanedEntries(limit);
+  } finally {
+    store.close();
+  }
+};
+
+cli.clearFavicons = async function() {
+  const cache = new FaviconCache();
+  try {
+    await cache.open();
+    await cache.clear();
+  } finally {
+    cache.close();
+  }
+};
+
+cli.compactFavicons = async function(limit) {
+  const cache = new FaviconCache();
+  let maxAgeMs;
+  try {
+    await cache.open();
+    await cache.compact(maxAgeMs, limit);
+  } finally {
+    cache.close();
+  }
+};
+
+cli.lookupFavicon = async function(url, timeout, cacheless = true) {
+  const query = new FaviconLookup();
+  query.cache = new FaviconCache();
+  query.fetchHTMLTimeoutMs = timeout;
+
+  try {
+    if(!cacheless) {
+      await query.cache.open();
+    }
+
+    return await query.lookup(new URL(url));
+  } finally {
+    if(!cacheless) {
+      query.cache.close();
+    }
+  }
+};
+
+
+
 console.debug('Initializing background page');
 
 addInstallListener(async function(event) {
@@ -159,6 +260,8 @@ addBadgeClickListener(function(event) {
 
 updateBadgeText();
 
+// Expose to console
+window.cli = cli;
 
 chrome.alarms.onAlarm.addListener(onWakeup);
 chrome.alarms.create('archive', {periodInMinutes: 60 * 12});
