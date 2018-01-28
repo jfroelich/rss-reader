839241d45c65093c77e295cbba5e422c4871284d
Josh <joshua.froelich@gmail.com>
2018-01-28 09:01:22 -0500

    export-opml improvements
    added missing async keyword, inlined all functions, removed unused
    imports, linting, cleanup, comments

--- src/export-import/export-opml.js
+++ src/export-import/export-opml.js
@@ -1,28 +1,13 @@
-import {feedPeekURL, getFeeds, isFeed} from "/src/rdb.js";
-
-// TODO: work with elements directly, skip the outline-object intermediate stuff, it is legacy
-// code from when i thought a different style of coherency was a good thing, but all it did was
-// split up code that changes together.
+import {feedPeekURL, getFeeds} from "/src/rdb.js";
 
 // Returns an opml document as a blob that contains outlines representing the feeds
 // in the app's db
-// @param feeds {Array}
+// @param conn {IDBDatabase} optional
 // @param title {String} optional
-export default function exportOPML(conn, title) {
+export default async function exportOPML(conn, title) {
   const feeds = await getFeeds(conn);
-  const document = createOPMLDocument(title);
 
-  for(const feed of feeds) {
-    appendOutlineObject(document, outlineFromFeed(feed));
-  }
-
-  return xmlToBlob(document);
-}
-
-// Create a new OPML document
-// @param title {String} optional document title
-// @return {Document} an xml-flagged document
-function createOPMLDocument(title) {
+  // Create a generic opml document
   const doc = document.implementation.createDocument(null, 'opml', null);
   doc.documentElement.setAttribute('version', '2.0');
 
@@ -52,99 +37,28 @@ function createOPMLDocument(title) {
   const bodyElement = doc.createElement('body');
   doc.documentElement.appendChild(bodyElement);
 
-  return doc;
-}
+  // Append the feeds to the document as outline elements
+  for(const feed of feeds) {
+    const outline = doc.createElement('outline');
+    if(feed.type) {
+      outline.setAttribute('type', feed.type);
+    }
+    outline.setAttribute('xmlUrl', feedPeekURL(feed));
+    if(feed.title) {
+      outline.setAttribute('title', feed.title);
+    }
+    if(feed.description) {
+      outline.setAttribute('description', feed.description);
+    }
+    if(feed.link) {
+      outline.setAttribute('htmlUrl', feed.link);
+    }
 
-
-// Create an outline from a feed
-function outlineFromFeed(feed) {
-  assert(isFeed(feed));
-  const outline = {};
-  outline.type = feed.type;
-  outline.xmlUrl = feedPeekURL(feed);
-  outline.title = feed.title;
-  outline.description = feed.description;
-  outline.htmlUrl = feed.link;
-  return outline;
-}
-
-function getOutlineObjects(doc) {
-  const elements = getOutlineElements(doc);
-  const objects = [];
-  for(const element of elements) {
-    objects.push(outlineElementToObject(element));
-  }
-  return objects;
-}
-
-function getOutlineElements(doc) {
-  assert(doc instanceof Document);
-  return doc.querySelectorAll('opml > body > outline');
-}
-
-function appendOutlineObject(doc, outline) {
-  appendOutlineElement(doc, outlineObjectToElement(doc, outline));
-}
-
-function appendOutlineElement(doc, element) {
-  assert(doc instanceof Document);
-  let bodyElement = doc.querySelector('body');
-  if(!bodyElement) {
-    bodyElement = doc.createElement('body');
-    doc.documentElement.appendChild(bodyElement);
-  }
-  bodyElement.appendChild(element);
-}
-
-function outlineObjectToElement(doc, outline) {
-  assert(doc instanceof Document);
-
-  const element = doc.createElement('outline');
-  if(outline.type) {
-    element.setAttribute('type', outline.type);
+    bodyElement.appendChild(outlineElement);
   }
 
-  if(outline.xmlUrl) {
-    element.setAttribute('xmlUrl', outline.xmlUrl);
-  }
-
-  if(outline.text) {
-    element.setAttribute('text', outline.text);
-  }
-
-  if(outline.title) {
-    element.setAttribute('title', outline.title);
-  }
-
-  if(outline.description) {
-    element.setAttribute('description', outline.description);
-  }
-
-  if(outline.htmlUrl) {
-    element.setAttribute('htmlUrl', outline.htmlUrl);
-  }
-
-  return element;
-}
-
-function outlineElementToObject(element) {
-  const object = {};
-  object.description = element.getAttribute('description');
-  object.htmlUrl = element.getAttribute('htmlUrl');
-  object.text = element.getAttribute('text');
-  object.title = element.getAttribute('title');
-  object.type = element.getAttribute('type');
-  object.xmlUrl = element.getAttribute('xmlUrl');
-  return object;
-}
-
-function xmlToBlob(document) {
-  assert(document instanceof Document);
+  // Serialize the document as a string and create and return a blob
   const serializer = new XMLSerializer();
-  const string = serializer.serializeToString(document);
+  const string = serializer.serializeToString(doc);
   return new Blob([string], {type: 'application/xml'});
-}
-
-function assert(value, message) {
-  if(!value) throw new Error(message || 'Assertion error');
 }
