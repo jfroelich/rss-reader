e099a5dc1d82fea4b5b32aadf9c1fb12f87c2a67
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-02-18 20:09:37 -0500

    added color test files

---
+++ src/color/test.js
@@ -0,0 +1,7 @@
+import * as color from '/src/color/color.js';
+
+// TODO: write pass/fail style tests that test various inputs
+// Consider comparing this library's output to other well known libs
+
+// Export to console for direct testing for now
+window.color = color;
