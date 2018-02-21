87e210e8d37093cc2702a633e40070f7f642341a
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-02-20 19:17:42 -0500

    remove unused import

--- src/content-filters/boilerplate-filter/test.js
+++ src/content-filters/boilerplate-filter/test.js
@@ -1,10 +1,12 @@
 import {boilerplate_filter} from '/src/content-filters/boilerplate-filter/boilerplate-filter.js';
-import {cf_resolve_document_urls, document_set_image_sizes, filter_blacklisted_elements, filter_frame_elements, filter_iframe_elements, filter_script_elements} from '/src/content-filters/content-filters.js';
+import {document_set_image_sizes, filter_blacklisted_elements, filter_frame_elements, filter_iframe_elements, filter_script_elements} from '/src/content-filters/content-filters.js';
 import {fetch_html} from '/src/fetch-utils.js';
 import {html_parse} from '/src/html-utils.js';
 
 window.test = async function(url_string) {
-  const response = await fetch_html(new URL(url_string));
+  const request_url = new URL(url_string);
+  const response = await fetch_html(request_url);
+  const response_url = new URL(response.url);
   const response_text = await response.text();
   const document = html_parse(response_text);
 
@@ -12,8 +14,7 @@ window.test = async function(url_string) {
   filter_iframe_elements(document);
   filter_script_elements(document);
   filter_blacklisted_elements(document);
-  await document_set_image_sizes(document, new URL(response.url));
-
+  await document_set_image_sizes(document, response_url);
   boilerplate_filter(document, {annotate: true});
 
   const best_element = document.querySelector('[data-bp-max]');
