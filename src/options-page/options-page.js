04762086c6b51d2c8ccd7be66bacc3707e921bd7
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-02-26 11:18:21 -0500

    Moved permission.js to permissions/permissions.js

--- src/options-page/options-page.js
+++ src/options-page/options-page.js
@@ -1,7 +1,7 @@
 import {element_fade} from '/src/element-fade.js';
 import {open as favicon_service_open} from '/src/favicon-service/favicon-service.js';
 import {html_truncate} from '/src/html-truncate/html-truncate.js';
-import {permission_has, permission_remove, permission_request} from '/src/options-page/permission.js';
+import {permission_has, permission_remove, permission_request} from '/src/permissions/permissions.js';
 import {ral_activate_feed, ral_deactivate_feed, ral_find_feed_by_id, ral_get_feeds, ral_unsubscribe} from '/src/ral/ral.js';
 import {rdb_feed_peek_url} from '/src/rdb/rdb.js';
 // TEMP: I plan to remove
