c9b68c72cceef9c7acba293e7b4b493e01b9dd03
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-02-26 14:21:17 -0500

    added mode parameter to url_is_external

--- src/cross-origin/cross-origin.js
+++ src/cross-origin/cross-origin.js
@@ -1,6 +1,10 @@
+const local_protocols = ['data:', 'mailto:', 'tel:', 'javascript:'];
 
+export const MODE_UNKNOWN = 0;
+export const MODE_STRICT = 2;
+export const MODE_SLOPPY = 1;
 
-export function url_is_external(document_url, other_url) {
+export function url_is_external(document_url, other_url, mode) {
   if (!(document_url instanceof URL)) {
     throw new TypeError('document_url is not a URL');
   }
@@ -9,12 +13,12 @@ export function url_is_external(document_url, other_url) {
     throw new TypeError('other_url is not a URL');
   }
 
-
-  // Certain protocols are never external in the sense that a network request
-  // is not performed
-  const local_protocols = ['data:', 'mailto:', 'tel:', 'javascript:'];
   if (local_protocols.includes(other_url.protocol)) {
     return false;
+  }
+
+  if (mode === MODE_STRICT) {
+    return document_url.origin !== other_url.origin;
   }
 
   const doc_domain = url_get_upper_domain(document_url);
@@ -22,28 +26,23 @@ export function url_is_external(document_url, other_url) {
   return doc_domain !== other_domain;
 }
 
-// Returns the 1st and 2nd level domains as a string. Basically hostname
-// without subdomains. This only does minimal symbolic validation, and is
-// inaccurate.
 function url_get_upper_domain(url) {
+  // NOTE: ignores port
   if (hostname_is_ipv4(url.hostname) || hostname_is_ipv6(url.hostname)) {
     return url.hostname;
   }
 
   const levels = url.hostname.split('.');
-  // We know hostname is a non-0 length string, so we know levels.length is
-  // greater than 0. Handle the simple cases of 'localhost' or 'example.com'
+
+  // Handle the simple cases of 'localhost' or 'example.com'
+  // NOTE: ignores port
   if (levels.length < 3) {
     return url.hostname;
   }
 
-  // Using the full list from
-  // https://publicsuffix.org/list/public_suffix_list.dat is overkill. Decide
-  // based on tld character length. We know levels.length > 2.
+  // Using the full suffix list is overkill so use tld character length
   const top_level = levels[levels.length - 1];
   const reverse_offset = top_level.length === 2 ? -3 : -2;
-  // [1,2,3,4].slice(-2) => [3,4]
-  // [1,2,3,4].slice(-3) => [2,3,4]
   return levels.slice(reverse_offset).join('.');
 }
 
@@ -67,7 +66,6 @@ function hostname_is_ipv4(string) {
   return true;
 }
 
-// Expects a hostname string property value from a URL object.
 function hostname_is_ipv6(value) {
   return typeof value === 'string' && value.includes(':');
 }
---
+++ src/cross-origin/readme.md
@@ -0,0 +1,17 @@
+The `url_is_external` function returns `true` if the `other_url` is *external* to the `document_url`.
+
+This function is inaccurate as well as insecure. It should not be used for security purposes.
+
+This function is useful to determine if fetching an embedded resource (e.g. the url of an image in a web page) would probably involve a cross origin url.
+
+Here, cross-origin is used loosely. This does not operate on the actual origin. This operates on an approximation of a url's domain by looking only at the upper levels of a domain. This is useful, for example, when determining whether a telemetry image is embedded in an html document, because concerning telemetry images almost always come from a different domain.
+
+# geo suffix
+
+https://publicsuffix.org/list/public_suffix_list.dat
+
+
+# TODO
+
+* review punycode support
+* does origin include port? Maybe I should be doing port comparison
