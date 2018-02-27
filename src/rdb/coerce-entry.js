00b479e5e4e025c99d610b38b6892f35f4b60dcb
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-02-27 08:03:26 -0500

    simplify and cleanup coerce_entry
    remove unused import, shorten import, shorten comments

--- src/rdb/coerce-entry.js
+++ src/rdb/coerce-entry.js
@@ -1,18 +1,15 @@
-import {rdb_entry_append_url, rdb_entry_create, rdb_entry_has_url} from '/src/rdb/rdb.js';
+import * as rdb from '/src/rdb/rdb.js';
 
-// Reformats a parsed feed entry as a storable entry
-// @param parsed_entry {Object} an entry object such as that produced by
-// feed_parse.
-// @throws {Error} if parsed_entry is not an object
-// @return {Object} an entry object in rdb format
+// Reformat a parsed entry as a storable entry
 export function coerce_entry(parsed_entry) {
-  const blank_storable_entry = rdb_entry_create();
+  const blank_storable_entry = rdb.rdb_entry_create();
   const storable_entry = Object.assign(blank_storable_entry, parsed_entry);
-  delete storable_entry.link;
 
+  // Translate link into head url
+  delete storable_entry.link;
   if (parsed_entry.link) {
     try {
-      rdb_entry_append_url(storable_entry, new URL(parsed_entry.link));
+      rdb.rdb_entry_append_url(storable_entry, new URL(parsed_entry.link));
     } catch (error) {
     }
   }
