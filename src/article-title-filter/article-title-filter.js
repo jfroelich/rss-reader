6947dd1c37712150161a030bc9375a2a168b7d72
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-02-27 13:36:52 -0500

    minor cleanup

--- src/article-title-filter/article-title-filter.js
+++ src/article-title-filter/article-title-filter.js
@@ -1,10 +1,14 @@
+const MAX_TAIL_WORDS = 4;
+const MIN_TITLE_LENGTH = 20;
+const MIN_PUBLISHER_NAME_LENGTH = 5;
+
 export function filter_title_publisher(title) {
+  const delimiter_length = 3;
+
   if (typeof title !== 'string') {
-    console.error('Invalid title parameter', title);
     return title;
   }
 
-  // Look for a delimiter
   let delimiter_position = title.lastIndexOf(' - ');
   if (delimiter_position < 0) {
     delimiter_position = title.lastIndexOf(' | ');
@@ -13,43 +17,34 @@ export function filter_title_publisher(title) {
     delimiter_position = title.lastIndexOf(' : ');
   }
 
-  // Exit early if no delimiter found
   if (delimiter_position < 0) {
     return title;
   }
 
-  // Exit early if the delimiter did not occur late enough in the title
-  const MIN_TITLE_LENGTH = 20;
   if (delimiter_position < MIN_TITLE_LENGTH) {
     return title;
   }
 
-  // Exit early if the delimiter was found too close to the end
-  const MIN_PUBLISHER_NAME_LENGTH = 5;
   const remaining_char_count = title.length - delimiter_position;
   if (remaining_char_count < MIN_PUBLISHER_NAME_LENGTH) {
     return title;
   }
 
-  // Break apart the tail into words
-  const delimiter_length = 3;
   const tail = title.substring(delimiter_position + delimiter_length);
-  const words = tokenize(tail);
-
-  // If there are too many words, return the full title, because tail is
-  // probably not a publisher
-  const MAX_TAIL_WORDS = 4;
-  if (words.length > MAX_TAIL_WORDS) {
+  if (count_words(tail) > MAX_TAIL_WORDS) {
     return title;
   }
 
   return title.substring(0, delimiter_position).trim();
 }
 
-// Break apart string into array of words
+function count_words(value) {
+  return tokenize(value).length;
+}
+
 function tokenize(value) {
   if (typeof value === 'string') {
-    // Avoid empty tokens by trimming and checking length
+    // Avoid empty tokens
     const trimmed_input = value.trim();
     if (trimmed_input.length) {
       return trimmed_input.split(/\s+/g);
