ec329af6abdb28768d6801e97415c92e6f77e26b
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-04 08:02:25 -0500

    Created attribute.js

--- src/content-filters/utils.js
+++ src/content-filters/utils.js
@@ -168,30 +168,6 @@ function element_is_offscreen(style) {
   return false;
 }
 
-// Adapted from https://github.com/kangax/html-minifier/issues/63
-const boolean_attribute_names = [
-  'allowfullscreen', 'async',          'autofocus',     'autoplay',
-  'checked',         'compact',        'controls',      'declare',
-  'default',         'defaultchecked', 'defaultmuted',  'defaultselected',
-  'defer',           'disabled',       'draggable',     'enabled',
-  'formnovalidate',  'hidden',         'indeterminate', 'inert',
-  'ismap',           'itemscope',      'loop',          'multiple',
-  'muted',           'nohref',         'noresize',      'noshade',
-  'novalidate',      'nowrap',         'open',          'pauseonexit',
-  'readonly',        'required',       'reversed',      'scoped',
-  'seamless',        'selected',       'sortable',      'spellcheck',
-  'translate',       'truespeed',      'typemustmatch', 'visible'
-];
-
-// Returns whether the attribute name is boolean. The element parameter is
-// present due to legacy code, and currently unused, but I decided to leave it
-// in, in the case that in the future I decide to vary the outcome of the
-// boolean determination based on the type of element in addition to attribute
-// name.
-export function attribute_is_boolean(element, attribute_name) {
-  return boolean_attribute_names.includes(attribute_name);
-}
-
 // Fetches an image element. Returns a promise that resolves to a fetched image
 // element. Data URIs are accepted.
 // @param url {URL}
