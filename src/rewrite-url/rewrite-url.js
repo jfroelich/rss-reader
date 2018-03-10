8272c09e761ac39b7819997d3b16c4ff3a18740c
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-10 09:06:58 -0500

    Make rewrite-url evaluate rules consecutively

--- src/rewrite-url/rewrite-url.js
+++ src/rewrite-url/rewrite-url.js
@@ -1,21 +1,39 @@
+
+const rules = [];
+
 export default function url_rewrite(url) {
   if (!(url instanceof URL)) {
     throw new TypeError('Invalid url parameter', url);
   }
 
+  let prev = url;
+  let next = url;
+  for (const rule of rules) {
+    prev = next;
+    next = rule(prev) || prev;
+  }
+
+  return next;
+}
+
+function google_news_rule(url) {
   if (url.hostname === 'news.google.com' && url.pathname === '/news/url') {
     const param = url.searchParams.get('url');
     try {
       return new URL(param);
     } catch (error) {
-      console.debug('Invalid url param', param);
     }
-    return;
-  } else if (
-      url.hostname === 'techcrunch.com' && url.searchParams.has('ncid')) {
-    // Only modify the clone for purity
+  }
+}
+
+rules.push(google_news_rule);
+
+function techcrunch_rule(url) {
+  if (url.hostname === 'techcrunch.com' && url.searchParams.has('ncid')) {
     const output = new URL(url.href);
     output.searchParams.delete('ncid');
     return output;
   }
 }
+
+rules.push(techcrunch_rule);
