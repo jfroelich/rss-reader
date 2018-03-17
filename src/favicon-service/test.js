c84557c2cfff3c79e21344654a267b020ebd7652
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-16 23:07:53 -0400

    Minor import syntax change in favicon test

--- src/favicon-service/test.js
+++ src/favicon-service/test.js
@@ -1,4 +1,4 @@
-import {clear, compact, lookup, open} from '/src/favicon-service/favicon-service.js';
+import * as favicon_service from '/src/favicon-service/favicon-service.js';
 import {idb_remove} from '/src/idb/idb.js';
 
 window.test_lookup = async function(url, cached) {
@@ -7,10 +7,10 @@ window.test_lookup = async function(url, cached) {
   const query = {};
   query.url = new URL(url);
   if (cached) {
-    query.conn = await open(test_db_name);
+    query.conn = await favicon_service.open(test_db_name);
   }
 
-  const icon_url_string = await lookup(query);
+  const icon_url_string = await favicon_service.lookup(query);
   if (cached) {
     query.conn.close();
 
@@ -20,5 +20,5 @@ window.test_lookup = async function(url, cached) {
   return icon_url_string;
 };
 
-window.test_compact = compact;
-window.test_clear = clear;
+window.test_compact = favicon_service.compact;
+window.test_clear = favicon_service.clear;
