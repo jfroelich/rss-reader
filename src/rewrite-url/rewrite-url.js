745d7a978dc8df5c90d3061b58f752d572486c01
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-17 08:48:45 -0400

    [rewrite-url] simplify function
    Removed sanity checks and superfluous condition

--- src/rewrite-url/rewrite-url.js
+++ src/rewrite-url/rewrite-url.js
@@ -1,16 +1,4 @@
 export function rewrite_url(url, rules) {
-  if (!(url instanceof URL)) {
-    throw new TypeError('url is not a URL ' + url);
-  }
-
-  if (!Array.isArray(rules)) {
-    throw new TypeError('rules is not an array' + rules);
-  }
-
-  if (!rules.length) {
-    return url;
-  }
-
   let prev = url;
   let next = url;
   for (const rule of rules) {
