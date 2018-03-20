61791758bab68bf7d1b6d14e8276bd5f5f8d468e
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-20 09:38:54 -0400

    [favicon-service] moved some comments

--- src/favicon-service/favicon-service.js
+++ src/favicon-service/favicon-service.js
@@ -16,44 +16,12 @@ const MAX_IMAGE_SIZE = 10240;
 
 function noop() {}
 
-// A partial drop in replacement for console that helps avoid the need to check
-// if console is defined every call to one of its methods. Only some methods
-// supported.
 const NULL_CONSOLE = {
   log: noop,
   warn: noop,
   debug: noop
 };
 
-// Options:
-//
-// * console {Object} optional, a console object where logging information is
-// sent. If not specified then a goes-nowhere-stub is used which effectively
-// means no logging.
-// * conn {IDBDatabase} optional, an open database connection to the favicon
-// cache. If specified then the lookup will interact with the cache. If not
-// specified then a cacheless lookup is done.
-// * maxFailureCount {Number} optional, if the lookup detects that too many
-// failures have been recorded in the cache then the lookup will exit early. If
-// a cache is provided and the lookup fails then a corresponding failure will be
-// recorded. Failures are aggregated by origin to limit the  amount of failure
-// entries in the cache.
-// * skipURLFetch {Boolean} optional, defaults to false, whether to skip
-// attempting to fetch the html of the input url
-// * maxAge {Number} optional, integer, the
-// number of millis after which an entry is considered to have expired
-// * fetchHTMLTimeout {Number} optional, integer, number of millis to wait
-// before considering an attempt to fetch the html of a url a failure
-// * fetchImageTimeout {Number} optional, integer, number of millis to wait
-// before considering an attempt to fetch an image (response to HEAD request) is
-// a failure
-// * minImageSize {Number} optional, minimum size in bytes of an image for it to
-// be considered a valid favicon
-// * maxImageSize {Number} optional, maximum size in bytes of an image for it to
-// be considered a valid favicon
-// * url {URL} required, the url to lookup, typically some webpage
-// * document {Document} optional, pre-fetched document that should be specified
-// if the page was previously fetched
 
 const default_options = {
   maxFailureCount: MAX_FAILURE_COUNT,
--- src/favicon-service/readme.md
+++ src/favicon-service/readme.md
@@ -1,6 +1,39 @@
 
 Provides a way to get the favicon image url for a page url
 
+### Options to lookup
+
+
+// Options:
+//
+// * console {Object} optional, a console object where logging information is
+// sent. If not specified then a goes-nowhere-stub is used which effectively
+// means no logging.
+// * conn {IDBDatabase} optional, an open database connection to the favicon
+// cache. If specified then the lookup will interact with the cache. If not
+// specified then a cacheless lookup is done.
+// * maxFailureCount {Number} optional, if the lookup detects that too many
+// failures have been recorded in the cache then the lookup will exit early. If
+// a cache is provided and the lookup fails then a corresponding failure will be
+// recorded. Failures are aggregated by origin to limit the  amount of failure
+// entries in the cache.
+// * skipURLFetch {Boolean} optional, defaults to false, whether to skip
+// attempting to fetch the html of the input url
+// * maxAge {Number} optional, integer, the
+// number of millis after which an entry is considered to have expired
+// * fetchHTMLTimeout {Number} optional, integer, number of millis to wait
+// before considering an attempt to fetch the html of a url a failure
+// * fetchImageTimeout {Number} optional, integer, number of millis to wait
+// before considering an attempt to fetch an image (response to HEAD request) is
+// a failure
+// * minImageSize {Number} optional, minimum size in bytes of an image for it to
+// be considered a valid favicon
+// * maxImageSize {Number} optional, maximum size in bytes of an image for it to
+// be considered a valid favicon
+// * url {URL} required, the url to lookup, typically some webpage
+// * document {Document} optional, pre-fetched document that should be specified
+// if the page was previously fetched
+
 # Test todos:
 
 * actually run tests instead of command line
