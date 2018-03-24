ed57ca0e5be3b80ea4c93cf30081f4c207267206
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-24 19:26:51 -0400

    [operations] created for-each-active-feed

---
+++ src/app/operations/for-each-active-feed.js
@@ -0,0 +1,19 @@
+import {get_feeds} from '/src/app/operations/get-feeds.js';
+
+// Calls the callback function on each feed in the store
+// TODO: currently each call to the callback is blocked by waiting for the
+// prior callback to complete, essentially a serial progression. This should
+// directly interact with the database instead of using get_feeds and
+// pre-loading into an array, and this should walk the feed store and call the
+// callback per cursor walk, advancing the cursor PRIOR to calling the callback,
+// taking advantage of the asynchronous nature of indexedDB cursor request
+// callbacks. This will yield a minor speedup at the cost of being a mild DRY
+// violation. However, the speed is admittedly not that important. This will
+// also make the approach scalable to N feeds (until stack overflow).
+
+export async function for_each_active_feed(conn, callback) {
+  const feeds = await get_feeds(conn);
+  for (const feed of feeds) {
+    callback(feed);
+  }
+}
--- src/rdb/rdb.js
+++ src/rdb/rdb.js
@@ -115,24 +115,6 @@ export async function find_active_feeds(conn) {
   return feeds.filter(feed => feed.active);
 }
 
-// Calls the callback function on each feed in the store
-// TODO: currently each call to the callback is blocked by waiting for the
-// prior callback to complete, essentially a serial progression. This should
-// directly interact with the database instead of using get_feeds and
-// pre-loading into an array, and this should walk the feed store and call the
-// callback per cursor walk, advancing the cursor PRIOR to calling the callback,
-// taking advantage of the asynchronous nature of indexedDB cursor request
-// callbacks. This will yield a minor speedup at the cost of being a mild DRY
-// violation. However, the speed is admittedly not that important. This will
-// also make the approach scalable to N feeds (until stack overflow).
-
-export async function for_each_active_feed(conn, per_feed_callback) {
-  const feeds = await get_feeds(conn);
-  for (const feed of feeds) {
-    per_feed_callback(feed);
-  }
-}
-
 function assert(value, message) {
   if (!value) throw new Error(message || 'Assertion error');
 }
