da2169f63d0281dd7c0a82b63d29872f12406f31
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-24 11:53:26 -0400

    Fix entry_is_valid with temporary cyclical import
    entry_is_valid relies on is_entry, which is presently defined in rdb,
    even though rdb relies on entry.js. This will be fixed in a later
    commit.

--- src/app/objects/entry.js
+++ src/app/objects/entry.js
@@ -1,3 +1,5 @@
+import * as rdb from '/src/rdb/rdb.js';
+
 export const ENTRY_STATE_UNREAD = 0;
 export const ENTRY_STATE_READ = 1;
 export const ENTRY_STATE_UNARCHIVED = 0;
@@ -6,7 +8,7 @@ export const ENTRY_STATE_ARCHIVED = 1;
 
 // TODO: implement
 export function entry_is_valid(entry) {
-  if (!is_entry(entry)) {
+  if (!rdb.is_entry(entry)) {
     return false;
   }
 
