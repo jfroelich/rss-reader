2ab6d7a0c582e3cd2e5fc02101657646f465f605
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-24 21:40:55 -0400

    [channel] flesh out app/handles/channel a bit

--- src/app/handles/channel.js
+++ src/app/handles/channel.js
@@ -1,3 +1,13 @@
 
-export function open_channel() {}
-export function close_channel() {}
+const CHANNEL_NAME = 'reader';
+
+export function channel_open() {
+  return new BroadcastChannel(CHANNEL_NAME);
+}
+
+export function channel_close(handle) {
+  if (handle) {
+    console.debug('Closing channel', handle.name);
+    handle.close();
+  }
+}
