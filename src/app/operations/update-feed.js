18c5bcd4286dc7bea40ead32d171dfff25cc908e
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-25 09:18:29 -0400

    [update-feed] linting
    Use executor helper, cleanup docs

--- src/app/operations/update-feed.js
+++ src/app/operations/update-feed.js
@@ -1,53 +1,41 @@
 import {feed_is_valid, is_feed} from '/src/app/objects/feed.js';
 
-// TODO: when updating, is put result still the feed id? I know
-// that result is feed id when adding, but what about updating? Review
-// the documentation on IDBObjectStore.prototype.put
-// So ... double check and warrant this resolves to an id
-
-// TODO: if awaited, I'd prefer to throw in case of channel postMessage error.
-// How? Or, should it really just be an error, and the caller is responsible for
-// keeping this open?
-
-// Create or update a feed in the database
-// @param conn {IDBDatabase}
-// @param channel {BroadcastChannel} optional
-// @param feed {object}
-// @param validate {Boolean} optional, if true then feed is validated
-// @param set_date_updated {Boolean} optional, if true then the feed's
-// dateUpdated property is implicitly set to the time this function is called
 export function update_feed(
     conn, channel, feed, validate = true, set_date_updated = false) {
-  return new Promise((resolve, reject) => {
-    if (validate) {
-      assert(feed_is_valid(feed));
-    } else {
-      assert(is_feed(feed));
-    }
+  if (validate) {
+    assert(feed_is_valid(feed));
+  } else {
+    assert(is_feed(feed));
+  }
 
-    if (set_date_updated) {
-      feed.dateUpdated = new Date();
-    }
-
-    const txn = conn.transaction('feed', 'readwrite');
-    const store = txn.objectStore('feed');
-    const request = store.put(feed);
-
-    request.onsuccess = _ => {
-      const feed_id = request.result;
-      // Suppress invalid state error when channel is closed in non-awaited call
-      if (channel) {
-        try {
-          channel.postMessage({type: 'feed-updated', id: feed_id});
-        } catch (error) {
-          console.debug(error);
-        }
-      }
-      resolve(feed_id);
-    };
-    request.onerror = _ => reject(request.error);
-  });
+  return new Promise(
+      executor.bind(null, conn, channel, feed, set_date_updated));
 }
+
+function executor(conn, channel, feed, set_date_updated, resolve, reject) {
+  if (set_date_updated) {
+    feed.dateUpdated = new Date();
+  }
+
+  const txn = conn.transaction('feed', 'readwrite');
+  const store = txn.objectStore('feed');
+  const request = store.put(feed);
+
+  request.onsuccess = _ => {
+    const feed_id = request.result;
+    // Suppress invalid state error when channel is closed in non-awaited call
+    if (channel) {
+      try {
+        channel.postMessage({type: 'feed-updated', id: feed_id});
+      } catch (error) {
+        console.debug(error);
+      }
+    }
+    resolve(feed_id);
+  };
+  request.onerror = _ => reject(request.error);
+}
+
 
 function assert(value) {
   if (!value) throw new Error('Assertion error');
---
+++ src/app/operations/update-feed.md
@@ -0,0 +1,25 @@
+The `update_feed` operation creates or updates a feed in the database.
+
+Other than the situation where the set_date_updated flag is true, this inserts the feed object *as-is*. It is the caller's responsibility to take care of any sanitization needs.
+
+### Params
+
+* conn {IDBDatabase} an open database connection
+* channel {BroadcastChannel} optional, a channel to send messages about a feed being updated
+* feed {object} the feed object to store
+* validate {Boolean} optional, if true then feed's properties are validated, and an error is thrown if the feed is invalid
+* set_date_updated {Boolean} optional, if true then the feed's `dateUpdated` property is implicitly set to the time this function is called
+
+### Exceptions
+
+* If conn is not defined, open
+* If channel is not a channel
+* If feed is not a feed object
+* If validate is true and the feed has invalid properties
+* If some error occurs when interacting with the database
+
+### TODOs
+
+* this should only resolve when tx completes, not prematurely on request complete
+* when updating, is put result still the feed id? I know that result is feed id when adding, but what about updating? Review the documentation on IDBObjectStore.prototype.put, double check and warrant this resolves to an id
+* if awaited, I'd prefer to throw in case of channel postMessage error. How? Or, should it really just be an error, and the caller is responsible for keeping this open?
