c14b71bff11916981c8a0b10754fb7efdc8baab1
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-25 20:21:42 -0400

    [badge] add console arg

--- src/badge.js
+++ src/badge.js
@@ -2,7 +2,7 @@ import {count_unread_entries} from '/src/app/operations/count-unread-entries.js'
 
 let update_pending = false;
 
-export async function update(conn) {
+export async function update(conn, console = null_console) {
   if (update_pending) {
     console.debug('prior update pending, update request canceled');
     return;
@@ -25,3 +25,12 @@ export async function update(conn) {
   console.debug('Setting badge text to', text);
   chrome.browserAction.setBadgeText({text: text});
 }
+
+function noop() {}
+
+const null_console = {
+  warn: noop,
+  log: noop,
+  debug: noop,
+  error: noop
+};
