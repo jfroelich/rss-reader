f0465bea98929e83c40df3543470d451aa8be87c
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-30 21:21:43 -0400

    Added find-entry-id-by-url

--- src/operations/contains-entry-with-url.js
+++ src/operations/contains-entry-with-url.js
@@ -1,26 +1,9 @@
 import {entry_is_valid_id} from '/src/objects/entry.js';
+import {find_entry_id_by_url} from '/src/operations/find-entry-id-by-url.js';
 
-// TODO: deprecate in favor of find_entry_by_url
-// TODO: the promise should not resolve until the transaction resolves
+// TODO: deprecate entirely, the caller should use find_entry_id_by_url
 
-export function contains_entry_with_url(conn, url) {
-  if (!(url instanceof URL)) {
-    throw new TypeError('url is not a URL: ' + url);
-  }
-
-  return new Promise(executor.bind(null, conn, url));
-}
-
-function executor(conn, url, resolve, reject) {
-  const txn = conn.transaction('entry');
-  const store = txn.objectStore('entry');
-  const index = store.index('urls');
-  const request = index.getKey(url.href);
-  request.onsuccess = request_onsuccess.bind(request, resolve);
-  request.onerror = _ => reject(request.error);
-}
-
-function request_onsuccess(callback, event) {
-  const entry_id = event.target.result;
-  resolve(entry_is_valid_id(entry_id));
+export async function contains_entry_with_url(conn, url) {
+  const entry_id = await find_entry_id_by_url(conn, url);
+  return entry_is_valid_id(entry_id);
 }
