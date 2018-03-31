23c0af5bd9439f6bb15719f4975dc7fc292152b0
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-31 09:14:12 -0400

    [rdr-fetch-image] added a note

--- src/operations/rdr-fetch-image.js
+++ src/operations/rdr-fetch-image.js
@@ -1,5 +1,8 @@
 import {tfetch} from '/src/url-loader/url-loader.js';
 
+// TODO: this is only in use by the favicon-service. Therefore I should probably
+// make this is a sub-module of the favicon-service, not an app-wide operation
+
 const image_mime_types = [
   'application/octet-stream', 'image/x-icon', 'image/jpeg', 'image/gif',
   'image/png', 'image/svg+xml', 'image/tiff', 'image/webp',
