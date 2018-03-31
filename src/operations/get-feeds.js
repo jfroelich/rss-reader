e9b0e05288a6c991bcbed88359cc8fcfcac4300d
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-31 15:21:45 -0400

    [get-feeds] add title sort parameter
    Enable the caller to specify that the resulting feed array should be
    sorted by title (lexicographic, ascending)

--- src/operations/get-feeds.js
+++ src/operations/get-feeds.js
@@ -1,14 +1,29 @@
 
 // TODO: deprecate in favor of an openCursor approach for scalability purposes
 
-export function get_feeds(conn) {
-  return new Promise(executor.bind(null, conn));
+export function get_feeds(conn, title_sort_flag = false) {
+  return new Promise(executor.bind(null, conn, title_sort_flag));
 }
 
-function executor(conn, resolve, reject) {
+function executor(conn, title_sort_flag, resolve, reject) {
   const txn = conn.transaction('feed');
   const store = txn.objectStore('feed');
   const request = store.getAll();
-  request.onsuccess = _ => resolve(request.result);
+  request.onsuccess = request_onsuccess.bind(request, title_sort_flag, resolve);
   request.onerror = _ => reject(request.error);
+}
+
+function request_onsuccess(title_sort_flag, callback, event) {
+  const feeds = event.target.result;
+  if (title_sort_flag) {
+    feeds.sort(feed_compare);
+  }
+
+  callback(feeds);
+}
+
+function feed_compare(a, b) {
+  const atitle = a.title ? a.title.toLowerCase() : '';
+  const btitle = b.title ? b.title.toLowerCase() : '';
+  return indexedDB.cmp(atitle, btitle);
 }
