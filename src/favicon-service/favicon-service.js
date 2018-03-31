ef503ffef5cf70368899797fd523373aae528c05
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-03-31 10:26:28 -0400

    [rdr-fetch-image] move to favicon-service
    Created fetch-image within favicon-service. Removed policy check for
    image fetch (for now).

--- src/favicon-service/favicon-service.js
+++ src/favicon-service/favicon-service.js
@@ -1,10 +1,10 @@
+import {fetch_image} from '/src/favicon-service/fetch-image.js';
 import * as html_parser from '/src/lib/html-parser/html-parser.js';
 import * as idb from '/src/lib/idb/idb.js';
 import * as mime from '/src/lib/mime/mime.js';
 import * as url_loader from '/src/lib/url-loader/url-loader.js';
-import {rdr_fetch_html} from '/src/operations/rdr-fetch-html.js';
 // TODO: lib modules should not depend on app modules
-import {rdr_fetch_image} from '/src/operations/rdr-fetch-image.js';
+import {rdr_fetch_html} from '/src/operations/rdr-fetch-html.js';
 
 export function FaviconService() {
   this.name = 'favicon-cache';
@@ -362,7 +362,7 @@ FaviconService.prototype.in_range = function(response) {
 
 FaviconService.prototype.head_image = async function(url) {
   const options = {method: 'head', timeout: this.fetch_image_timeout};
-  const response = await rdr_fetch_image(url, options);
+  const response = await fetch_image(url, options);
   if (response.ok && !this.in_range(response)) {
     this.console.debug('image size not in range', url.href, size);
     return url_loader.create_error_response(url_loader.STATUS_RANGE_ERROR);
