4deecd309619c6bebcdb1b944068a9d8e5b2e682
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-01 11:08:10 -0400

    [css] Fixed export error
    Forgot to export the helper! Was causing the slideshow to fail to react
    dynamically to options page display settings changes

--- src/lib/css/css.js
+++ src/lib/css/css.js
@@ -2,7 +2,7 @@
 // Returns the first matching css rule or undefined
 // @param selector_text {String}
 // @returns rule {CSSStyleRule}
-function find_rule(selector_text) {
+export function find_rule(selector_text) {
   for (const sheet of document.styleSheets) {
     for (const rule of sheet.rules) {
       if (rule.selectorText === selector_text) {
