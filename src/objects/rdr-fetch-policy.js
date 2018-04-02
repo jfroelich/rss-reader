b916923deb1d3d9753dd97703b00b065d59e8a68
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-02 08:31:42 -0400

    [rdr-fetch-policy] refining impl
    Use a single boolean expression

--- src/objects/rdr-fetch-policy.js
+++ src/objects/rdr-fetch-policy.js
@@ -1,33 +1,14 @@
+const allowed_protocols = ['data:', 'http:', 'https'];
 
-// TODO: what about a whitelist approach, only allow http/https?
-const protocol_blacklist =
-    ['about:', 'chrome:', 'chrome-extension:', 'file:', 'tel:', 'mailto:'];
-
+// Restrict protocol to allowed protocols
+// Deny local urls
+// Deny credentialed urls
 export function url_is_allowed(url) {
   if ((!url instanceof URL)) {
     throw new TypeError('url is not a URL ' + url);
   }
 
-  // Permit all data uris as there is no network concern, and to simplify all
-  // later conditions to not have to deal with data uris
-  if (url.protocol === 'data:') {
-    return true;
-  }
-
-  // Deny certain protocols
-  if (protocol_blacklist.includes(url.protocol)) {
-    return false;
-  }
-
-  // Deny local urls
-  if (url.hostname === 'localhost' || url.hostname === '127.0.0.1') {
-    return false;
-  }
-
-  // Deny credentialed urls
-  if (url.username || url.password) {
-    return false;
-  }
-
-  return true;
+  return allowed_protocols.includes(url.protocol) &&
+      url.hostname !== 'localhost' && url.hostname !== '127.0.0.1' &&
+      !url.username && !url.password;
 }
