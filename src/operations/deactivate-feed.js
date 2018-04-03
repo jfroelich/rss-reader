a21d8941a2390d40f82e1040c13d681c6ac3643a
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-03 11:44:18 -0400

    [deactivate-feed] use executor helper function
    Be consistent in use of promise helper similar to other operation
    implementations

--- src/operations/deactivate-feed.js
+++ src/operations/deactivate-feed.js
@@ -2,15 +2,17 @@ import {feed_is_valid_id} from '/src/objects/feed.js';
 
 export function deactivate_feed(conn, channel, feed_id, reason_text) {
   assert(feed_is_valid_id(feed_id));
+  return new Promise(executor.bind(null, conn, channel, feed_id, reason_text));
+}
 
-  return new Promise((resolve, reject) => {
-    const txn = conn.transaction('feed', 'readwrite');
-    txn.oncomplete = txn_oncomplete.bind(txn, channel, resolve, feed_id);
-    txn.onerror = _ => reject(txn.error);
-    const store = txn.objectStore('feed');
-    const request = store.get(feed_id);
-    request.onsuccess = request_onsuccess.bind(request, reason_text);
-  });
+function executor(conn, channel, feed_id, reason_text, resolve, reject) {
+  const txn = conn.transaction('feed', 'readwrite');
+  txn.oncomplete = txn_oncomplete.bind(txn, channel, resolve, feed_id);
+  txn.onerror = _ => reject(txn.error);
+
+  const store = txn.objectStore('feed');
+  const request = store.get(feed_id);
+  request.onsuccess = request_onsuccess.bind(request, reason_text);
 }
 
 function txn_oncomplete(channel, callback, feed_id, event) {
