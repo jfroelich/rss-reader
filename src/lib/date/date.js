2e214ba71fde0dc52bbc25dd3fcf292c273fe3bd
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-05 16:04:28 -0400

    [date] improve invalid date check

--- src/lib/date/date.js
+++ src/lib/date/date.js
@@ -3,6 +3,12 @@ export function date_format(date) {
     return 'Invalid date';
   }
 
+  // Date objects created by new Date(string) or Date.parse produce a date
+  // object with an invalid internal time property
+  if (isNaN(date.getTime())) {
+    return 'Invalid date';
+  }
+
   const formatter = new Intl.DateTimeFormat();
   try {
     return formatter.format(date);
