ca1fbe3668ef379eb386cc82f40027f529a10563
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-07 08:51:50 -0400

    [refresh-feed-icons] add console-stub

--- src/operations/refresh-feed-icons.js
+++ src/operations/refresh-feed-icons.js
@@ -1,16 +1,20 @@
+import {console_stub} from '/src/lib/console-stub/console-stub.js';
 import {FaviconService} from '/src/lib/favicon-service/favicon-service.js';
 import {feed_create_favicon_lookup_url, feed_has_url} from '/src/objects/feed.js';
 import {for_each_active_feed} from '/src/operations/for-each-active-feed.js';
 import {update_feed} from '/src/operations/update-feed.js';
 
-export async function refresh_feed_icons(rconn, iconn, channel) {
+export async function refresh_feed_icons(
+    rconn, iconn, channel, console = console_stub) {
   const promises = [];
   await for_each_active_feed(
-      rconn, feed => promises.push(refresh_feed(rconn, iconn, channel, feed)));
+      rconn,
+      feed =>
+          promises.push(refresh_feed(rconn, iconn, channel, console, feed)));
   await Promise.all(promises);
 }
 
-async function refresh_feed(rconn, iconn, channel, feed) {
+async function refresh_feed(rconn, iconn, channel, console, feed) {
   if (!feed_has_url(feed)) {
     return;
   }
@@ -22,6 +26,7 @@ async function refresh_feed(rconn, iconn, channel, feed) {
 
   const fs = new FaviconService();
   fs.conn = iconn;
+  fs.console = console;
 
   const icon_url_string = await fs.lookup(lookup_url);
 
