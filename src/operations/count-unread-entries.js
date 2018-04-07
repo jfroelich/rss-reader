39487f4a61aef65ce8d517eb14ea2990edac49fd
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-07 18:43:53 -0400

    [count-unread-entries] minor comment change

--- src/operations/count-unread-entries.js
+++ src/operations/count-unread-entries.js
@@ -1,5 +1,18 @@
 import {ENTRY_STATE_UNREAD} from '/src/objects/entry.js';
 
+// TODO: now that I have this operations folder, I am more confident that it
+// makes sense to deprecate this call. This is only used by one other module,
+// which is also an operation. This should just be a helper to it. There are no
+// other users of this function. This is a classic case of trying to design for
+// something that is not the actual need, a sort of just-in-case design
+// approach, which is wrong. Design for the task at hand, not the possible
+// future task.
+// Furthermore, if I deprecate and make a helper, I could look more into making
+// rdr_badge_update easily unawaitable, because I only need to guarantee the
+// request is set while the connection is not close-pending, so that even if the
+// caller does close while it is pending, there is no issue, because close
+// implicitly waits for pendings to settle.
+
 // Return the number of unread entries in the database
 // @param conn {IDBDatabase} an open database connection, required
 // @return {Promise} a promise that resolves to the number of unread entries, or
