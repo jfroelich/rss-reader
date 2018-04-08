792e8277b770d957065ac94adc6a520d7d563d66
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-08 13:27:11 -0400

    [rdr-archive-entries-test] cleanup
    Stub out channel, use named parameter, use undefined instead of null

--- src/operations/rdr-archive-entries-test.js
+++ src/operations/rdr-archive-entries-test.js
@@ -2,12 +2,20 @@ import {idb_remove} from '/src/lib/idb/idb.js';
 import {rdr_archive} from '/src/operations/rdr-archive-entries.js';
 import {rdr_create_conn} from '/src/operations/rdr-create-conn.js';
 
+const channel_stub = {
+  name: 'channel-stub',
+  postMessage: noop,
+  close: noop
+};
+
 async function test() {
+  let max_age;
   const conn = await rdr_create_conn('archive-entries-test');
-  await rdr_archive(
-      conn, /* channel */ undefined, console, /* max_age */ undefined);
+  await rdr_archive(conn, channel_stub, console, max_age);
   conn.close;
   await idb_remove(conn.name);
 }
+
+function noop() {}
 
 window.test = test;
