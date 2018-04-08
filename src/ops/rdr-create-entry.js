e3a30b2c883aaca4171d3c0db32f4224e99694b5
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-08 19:45:36 -0400

    [rdr-update-entry] use context

--- src/ops/rdr-create-entry.js
+++ src/ops/rdr-create-entry.js
@@ -2,6 +2,12 @@ import {filter_empty_properties} from '/src/lib/object/object.js';
 import {entry_is_valid, entry_sanitize, ENTRY_STATE_UNARCHIVED, ENTRY_STATE_UNREAD, is_entry} from '/src/objects/entry.js';
 import {rdr_update_entry} from '/src/ops/rdr-update-entry.js';
 
+const channel_stub = {
+  name: 'channel-stub',
+  postMessage: noop,
+  close: noop
+};
+
 export async function rdr_create_entry(entry, validate) {
   if (!is_entry(entry)) {
     throw new TypeError('entry argument is not an entry object: ' + entry);
@@ -19,14 +25,18 @@ export async function rdr_create_entry(entry, validate) {
   const sanitized_entry = entry_sanitize(entry);
   const storable_entry = filter_empty_properties(sanitized_entry);
 
-  // Setup or remove initial values
+  // Setup initial values
   storable_entry.readState = ENTRY_STATE_UNREAD;
   storable_entry.archiveState = ENTRY_STATE_UNARCHIVED;
   storable_entry.dateCreated = new Date();
   delete storable_entry.dateUpdated;
 
-  const entry_id =
-      await rdr_update_entry(this.conn, void channel, storable_entry);
+  const uec = {};
+  uec.conn = this.conn;
+  // Redirect the update message to /dev/null basically
+  uec.channel = channel_stub;
+  uec.console = this.console;
+  const entry_id = await rdr_update_entry.call(uec, storable_entry);
 
   this.channel.postMessage({type: 'entry-added', id: entry_id});
 
@@ -34,3 +44,5 @@ export async function rdr_create_entry(entry, validate) {
   storable_entry.id = entry_id;
   return storable_entry;
 }
+
+function noop() {}
--- src/ops/rdr-update-entry.js
+++ src/ops/rdr-update-entry.js
@@ -1,25 +1,24 @@
 import {is_entry} from '/src/objects/entry.js';
 
-export function rdr_update_entry(conn, channel, entry) {
+export function rdr_update_entry(entry) {
   if (!is_entry(entry)) {
     throw new TypeError('entry is not an entry ' + entry);
   }
 
-  return new Promise(executor.bind(null, conn, channel, entry));
+  return new Promise(executor.bind(this, entry));
 }
 
-function executor(conn, channel, entry, resolve, reject) {
-  const txn = conn.transaction('entry', 'readwrite');
+function executor(entry, resolve, reject) {
+  const txn = this.conn.transaction('entry', 'readwrite');
   const store = txn.objectStore('entry');
   const request = store.put(entry);
-  request.onsuccess = request_onsuccess.bind(request, channel, resolve);
+  request.onsuccess = request_onsuccess.bind(this, resolve);
   request.onerror = _ => reject(request.error);
 }
 
-function request_onsuccess(channel, callback, event) {
+function request_onsuccess(callback, event) {
   const entry_id = event.target.result;
-  if (channel) {
-    channel.postMessage({type: 'entry-updated', id: entry_id});
-  }
+  this.channel.postMessage({type: 'entry-updated', id: entry_id});
+  this.console.debug('Updated entry', entry_id);
   callback(entry_id);
 }
