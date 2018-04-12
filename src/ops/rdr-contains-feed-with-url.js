88f1f115994b77c64555d01fa0e791eb23f8ae0d
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-11 22:31:42 -0400

    [rdr-find-feed-by-url] first draft

--- src/ops/rdr-contains-feed-with-url.js
+++ src/ops/rdr-contains-feed-with-url.js
@@ -1,16 +1,6 @@
 import {feed_is_valid_id} from '/src/objects/feed.js';
 
-// TODO: deprecate in favor of find_feed_id_by_url
-
-/*
-TODO: actually instead of above, create rdr_find_feed_by_url, and add a
-parameter to it, key_only, default false. If key_only is true, then this returns
-a feed object with only an id property. If key_only is false, then this returns
-a feed object with all properties. If no match is found, returns undefined. In
-both cases an object is returned (which will change up how find_feed_id stuff
-would be used). If this turns out as ergonomic then consider also applying this
-approach to finding entries.
-*/
+// TODO: deprecate in favor of rdr_find_feed_by_url
 
 export function rdr_contains_feed_with_url(conn, url) {
   if (!(url instanceof URL)) {
