4213122f16606783e8b5f6a7bd6fbd63c6221194
Joshua Froelich <joshuafroelich@mergepoint-ems.mi.local>
2018-04-13 13:38:51 -0400

    [rdr-poll-entry] add fb-exit rule

--- src/ops/rdr-poll-entry.js
+++ src/ops/rdr-poll-entry.js
@@ -172,6 +172,7 @@ function build_rewrite_rules() {
   const rules = [];
   rules.push(google_news_rule);
   rules.push(techcrunch_rule);
+  rules.push(facebook_exit_rule);
   return rules;
 }
 
@@ -190,5 +191,15 @@ function techcrunch_rule(url) {
     const output = new URL(url.href);
     output.searchParams.delete('ncid');
     return output;
+  }
+}
+
+function facebook_exit_rule(url) {
+  if (url.hostname === 'l.facebook.com' && url.pathname === '/l.php') {
+    const param = url.searchParams.get('u');
+    try {
+      return new URL(param);
+    } catch (error) {
+    }
   }
 }
