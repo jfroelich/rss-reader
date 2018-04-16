356d4a33e297ab1973c5f917f58b289f320d50bd
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-04-16 10:45:47 -0400

    [fetch-policy] use an easier to read code style
    Do not define function bodies within object literals

--- src/objects/fetch-policy.js
+++ src/objects/fetch-policy.js
@@ -2,17 +2,21 @@ const allowed_protocols = ['data:', 'http:', 'https:'];
 const allowed_methods = ['GET', 'HEAD'];
 
 export const fetch_policy = {
-  allows_url: function(url) {
-    if ((!url instanceof URL)) {
-      throw new TypeError('url is not a URL ' + url);
-    }
-
-    return allowed_protocols.includes(url.protocol) &&
-        url.hostname !== 'localhost' && url.hostname !== '127.0.0.1' &&
-        !url.username && !url.password;
-  },
-  allows_method: function(raw_method) {
-    const method = raw_method ? raw_method.toUpperCase() : 'GET';
-    return allowed_methods.includes(method);
-  }
+  allows_url: allows_url,
+  allows_method: allows_method
 };
+
+function allows_url(url) {
+  if ((!url instanceof URL)) {
+    throw new TypeError('url is not a URL ' + url);
+  }
+
+  return allowed_protocols.includes(url.protocol) &&
+      url.hostname !== 'localhost' && url.hostname !== '127.0.0.1' &&
+      !url.username && !url.password;
+}
+
+function allows_method(raw_method) {
+  const method = raw_method ? raw_method.toUpperCase() : 'GET';
+  return allowed_methods.includes(method);
+}
