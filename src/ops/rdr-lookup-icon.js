c04acf11a63fffab51d1d1aa210003c895b73b6b
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-04-16 10:27:11 -0400

    [rdr-lookup-icon] linting

--- src/ops/rdr-lookup-icon.js
+++ src/ops/rdr-lookup-icon.js
@@ -1,6 +1,11 @@
 import {FaviconService} from '/src/lib/favicon-service/favicon-service.js';
 
-// conn, console, skip_fetch, url
+// Lookup the favicon url for a url. Returns a promise that resolves to the
+// url (string).
+// @param url {URL} the location to investigate
+// @param skip_fetch {Boolean} whether to attempt to fetch the full text of the
+// resource, and if it is html, search for a url within the html, before
+// continuing to check other places.
 
 export function rdr_lookup_icon(url, skip_fetch) {
   const fs = new FaviconService();
