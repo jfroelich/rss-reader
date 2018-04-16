fb711d99bff5393fc16e3609de40640a75204b92
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-04-16 10:00:22 -0400

    [rdr-create-feed] experiment with function pointers
    Instead of using function.call, assign the function to an object and
    call the object's method.

--- src/ops/rdr-create-feed.js
+++ src/ops/rdr-create-feed.js
@@ -19,20 +19,23 @@ export async function rdr_create_feed(
   clone.dateCreated = new Date();
   delete clone.dateUpdated;
 
-  const update_context = {};
-  update_context.conn = conn;
+  const update_op = {};
+  update_op.conn = conn;
   // Silence the update message, we substitute our own
-  update_context.channel = channel_stub;
+  update_op.channel = channel_stub;
   // TODO: get from parameter instead
-  update_context.console = void console;
+  update_op.console = void console;
+  update_op.run = rdr_update_feed;
 
   const update_options = {};
   update_options.sanitize = sanitize;
   update_options.validate = true;
   update_options.set_date_updated = false;
 
-  const stored_feed =
-      await rdr_update_feed.call(update_context, clone, update_options);
+  // const stored_feed =
+  //    await rdr_update_feed.call(update_op, clone, update_options);
+  const stored_feed = update_op.run(clone, update_options);
+
   channel.postMessage({type: 'feed-added', id: stored_feed.id});
   return stored_feed;
 }
