691e948c73016ca21e7b29d832f1c3e5885decfd
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-04-22 14:14:48 -0400

    [test] more test notes

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,9 +1,18 @@
+// TODO: currently doesn't work, feeds are not loaded, I basically need to do
+// something like include all test modules here, so that each one registers
+// itself as a side effect of import. But tests depend on this file, so
+// circular, so first I need to fix that, maybe by moving test helpers to a
+// separate file
+
+
 // TODO: the basic idea is that I can have any number of separate tests
 // Each test file should import this file
 // Each test file should register its tests using register_test
 // A test file can register more than one test
 // All tests are run async
 // Every test should be an async function, even if it is sync
+
+
 
 // TODO: what does it mean to fail? not return true? throw an error?
 // I think it will be better with errors because that means the test code
@@ -36,7 +45,7 @@ export function register_test(test_function) {
   tests.push(test_function);
 }
 
-export async function run() {
+async function main() {
   console.debug('Running all tests');
 
   const test_promises = [];
@@ -48,3 +57,5 @@ export async function run() {
   const results = await Promise.all(test_promises);
   console.debug('Completed all tests');
 }
+
+window.main = main;
