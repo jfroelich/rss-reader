26f1c8ed329f72548723b65e7077362003f772f7
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-05-03 09:02:31 -0400

    [test] added test timeouts

--- src/tests/test.js
+++ src/tests/test.js
@@ -53,11 +53,27 @@ const test_registry = [
 // clang-format on
 
 // Wrap the call to a test function with some extra log messages
-async function run_test_function(test_function) {
+async function run_test_function(test_function, timeout = 0) {
   console.log('%s: started', test_function.name);
-  await test_function();
+  // await test_function();
+  if (timeout) {
+    const test_promise = test_function();
+    const timeout_promise = deferred_rejection(test_function, timeout);
+    await Promise.race([test_promise, timeout_promise]);
+  } else {
+    await test_function();
+  }
+
   console.log('%s: completed', test_function.name);
 }
+
+function deferred_rejection(test_function, time_ms) {
+  const error = new Error('Test ' + test_function.name + ' timed out');
+  return new Promise((resolve, reject) => {
+    setTimeout(reject, time_ms, error);
+  });
+}
+
 
 // Lookup a test function by the function's name
 function find_test_by_name(test_name) {
@@ -84,10 +100,11 @@ async function run_one(test_function_name) {
 }
 
 async function run_parallel() {
+  let timeout = 10000;
   console.log('Running all tests in parallel');
   const test_promises = [];
   for (const test of test_registry) {
-    test_promises.push(run_test_function(test));
+    test_promises.push(run_test_function(test, timeout));
   }
 
   await Promise.all(test_promises);
@@ -95,9 +112,14 @@ async function run_parallel() {
 }
 
 async function run_serial() {
+  let timeout = 5000;
+  let counter = 0;
+  let total = test_registry.length;
   console.log('Running all tests serially');
   for (const test of test_registry) {
-    await run_test_function(test);
+    await run_test_function(test, timeout);
+    counter++;
+    console.debug('finished %d of %d', counter, total);
   }
 
   console.log('Completed all tests');
