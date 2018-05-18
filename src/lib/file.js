5b767a56c71d79a28edb38caf0f021b4dbc50cdd
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-05-17 21:26:22 -0400

    [import-opml] revised
    subscribe concurrently, remove unused import, deprecate file lib, add
    some comments

--- src/lib/file.js
+++
@@ -1,8 +0,0 @@
-export function read_text(file) {
-  return new Promise(function executor(resolve, reject) {
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = () => reject(reader.error);
-  });
-}
