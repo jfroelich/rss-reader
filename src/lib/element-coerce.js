27f42dad8be82599612217bb5f59d450cfb41ae1
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-05-20 12:10:28 -0400

    [element-coerce] minor comments

--- src/lib/element-coerce.js
+++ src/lib/element-coerce.js
@@ -1,9 +1,16 @@
+// TODO: move the docs back into here as comments, it is much easier to maintain
+// and more standalone
+// TODO: add console parameter
+// TODO: rename to coerce-element (both file and function)
+// TODO: maybe move to subfolder element within lib
+// TODO: maybe merge into its one callsite
+
 export function element_coerce(element, new_name, copy_attributes = true) {
   if (!(element instanceof Element)) {
     throw new TypeError('element is not an Element');
   }
 
-  if (!element_name_is_valid(new_name)) {
+  if (!is_valid_element_name(new_name)) {
     throw new TypeError('Invalid new name ' + new_name);
   }
 
@@ -17,19 +24,22 @@ export function element_coerce(element, new_name, copy_attributes = true) {
   }
 
   const next_sibling = element.nextSibling;
+
   element.remove();
 
   const new_element = element.ownerDocument.createElement(new_name);
+
   if (copy_attributes) {
-    element_copy_attributes(element, new_element);
+    copy_element_attributes(element, new_element);
   }
 
-  element_move_child_nodes(element, new_element);
+  move_child_nodes(element, new_element);
+
   return parent_element.insertBefore(new_element, next_sibling);
 }
 
-export function element_move_child_nodes(from_element, to_element) {
-  if (element_is_void(to_element)) {
+export function move_child_nodes(from_element, to_element) {
+  if (is_void_element(to_element)) {
     return;
   }
 
@@ -40,20 +50,20 @@ export function element_move_child_nodes(from_element, to_element) {
   }
 }
 
-export const void_elements = [
+const void_elements = [
   'area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta',
   'param', 'source', 'track', 'wbr'
 ];
 
-export function element_is_void(element) {
+export function is_void_element(element) {
   return void_elements.includes(element.localName);
 }
 
-function element_name_is_valid(value) {
+function is_valid_element_name(value) {
   return typeof value === 'string' && value.length && !value.includes(' ');
 }
 
-export function element_copy_attributes(from_element, to_element) {
+export function copy_element_attributes(from_element, to_element) {
   const names = from_element.getAttributeNames();
   for (const name of names) {
     const value = from_element.getAttribute(name);
