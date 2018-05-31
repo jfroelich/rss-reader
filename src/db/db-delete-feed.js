dac5298ab63573914e75b20207c853cc032e468e
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-05-31 12:39:14 -0400

    [db-delete-feed] commentary cleanup

--- src/db/db-delete-feed.js
+++ src/db/db-delete-feed.js
@@ -2,68 +2,71 @@ import {refresh_badge} from '/src/badge.js';
 import {is_valid_feed_id} from '/src/feed.js';
 import {log} from '/src/log.js';
 
-// Removes a feed, and any entries tied to the feed, from the database. The
-// operation resolves once the internal database transaction has committed. A
-// message is sent to the channel when the feed is deleted, and when an entry is
-// deleted. This never actually checks if the feed exists.
-
-// Required context properties
-// * conn {IDBDatabase} an open database connection
-// * channel {BroadcastChannel} will send messages when the feed is deleted
-// and for each entry deleted
-
-// @param feed_id {Number} the feed to delete
-// @param reason_text {String} optional, a categorical description of the reason
+// Asynchronously removes a feed, and any entries associated with the feed, from
+// the database. The operation resolves once the sole internal database
+// transaction commits. A message is sent to the channel when the feed is
+// deleted, and for each associated entry that is deleted.
+//
+// This checks that the input feed id is valid in the sense that it is
+// well-formed, but this never actually checks if the feed id corresponds to a
+// feed object in the database, so be forewarned it is possible to call this and
+// produce no effect. I anticipate that most of the time the caller should know
+// that the feed id corresponds, so this correspondence is not validated because
+// it would be pointless overhead to perform an extra lookup by id before
+// deleting. Note this behavior is largely born out of the fact that this is
+// only a shallow abstraction over an indexedDB call that exhibits this same
+// issue of incomplete knowledge.
+//
+// @context-param conn {IDBDatabase} an open database connection
+// @context-param channel {BroadcastChannel} receives messages when the feed is
+// deleted and for each entry deleted
+// @param feed_id {Number} the id of the feed to delete
+// @param reason {String} optional, a categorical description of the reason
 // for deletion, such as 'unsubscribe', or 'inactive', this parameter is not
 // sanity checked
-// @throws {TypeError} if the input feed id is invalid, this is thrown immediately
-// unlike the promise rejections
+// @throws {TypeError} invalid feed id
 // @rejects {DOMException} database errors
 // @rejects {InvalidStateError} if channel is closed when posting message, but
 // note that db still modified
-// @return {Promise} a promise that resolves to an array of the entry ids that
-// were deleted. The array is always defined but may be zero-length.
+// @return {Promise} resolves to undefined
+// TODO: should consider de-optimizing by removing the feed index from the
+// entry store. Deletes are rare events. There is not a great need for this
+// operation to be fast. Maintaining the feed index on the entry store is
+// expensive. What else relies on the feed index? If nothing else uses the feed
+// index I could just remove it. Of course this is complex because it also
+// requires a schema change, so I have to further complicate db-open.
+export function db_delete_feed(feed_id, reason) {
+  return new Promise(executor.bind(this, feed_id, reason));
+}
 
-export function db_delete_feed(feed_id, reason_text) {
+function executor(feed_id, reason, resolve, reject) {
+  // This check is justified because this resolves without error otherwise. It
+  // would be a harmless no-op if deleting a bad id was allowed, but the app
+  // policy is to discourage pointless no-ops particularly because they consume
+  // resources and because it reduces confusion. This constitutes programmer
+  // error.
   if (!is_valid_feed_id(feed_id)) {
     throw new TypeError('Invalid feed id ' + feed_id);
   }
 
-  return new Promise(delete_feed_executor.bind(this, feed_id, reason_text));
-}
-
-function delete_feed_executor(feed_id, reason_text, resolve, reject) {
   let entry_ids = [];
-
-  // This uses a single database transaction to guarantee data integrity. If
-  // deleting an entry for a feed causes an error after the feed was deleted,
-  // the feed is not actually deleted.
-  const txn = this.conn.transaction(['feed', 'entry'], 'readwrite');
+  const txn_store_names = ['feed', 'entry'];
+  const txn = this.conn.transaction(txn_store_names, 'readwrite');
   txn.oncomplete = delete_feed_txn_oncomplete.bind(
-      this, feed_id, reason_text, entry_ids, resolve);
+      this, feed_id, reason, entry_ids, resolve);
   txn.onerror = _ => reject(txn.error);
 
   const feed_store = txn.objectStore('feed');
 
-  // Delete the feed
-  log('Deleting feed with id', feed_id);
+  log('%s: deleting feed %d', db_delete_feed.name, feed_id);
   feed_store.delete(feed_id);
-
-  // Delete all entries belonging to the feed
-  // TODO: should consider de-optimizing by removing the feed index from the
-  // entry store. Deletes are rare events. There is not a great need for this
-  // operation to be fast. Maintaining the feed index on the entry store is
-  // expensive. What else relies on the feed index? Rather than finding entries
-  // for the feed by querying the feed index this could simply walk the entire
-  // entry store, one entry at a time.
 
   const entry_store = txn.objectStore('entry');
   const feed_index = entry_store.index('feed');
 
   // This uses getAllKeys rather than getAll when finding and iterating over
   // entries for a feed in order to avoid unnecessary deserialization of entries
-  // TODO: use openKeyCursor for scalability?
-
+  // TODO: use openKeyCursor for better scalability?
   const request = feed_index.getAllKeys(feed_id);
 
   // TODO: do not nest
@@ -79,40 +82,60 @@ function delete_feed_executor(feed_id, reason_text, resolve, reject) {
 }
 
 function delete_feed_txn_oncomplete(
-    feed_id, reason_text, entry_ids, callback, event) {
-  // Messages are not sent prematurely, messages are only sent when the
-  // transaction has actually committed successfully
+    feed_id, reason, entry_ids, callback, event) {
+  const feed_msg = {};
+  feed_msg.type = 'feed-deleted';
+  feed_msg.id = feed_id;
+  feed_msg.reason = reason;
+  this.channel.postMessage(feed_msg);
 
-  const msg = {type: 'feed-deleted', id: feed_id, reason: reason_text};
-  log('%s: %o', db_delete_feed.name, msg);
-  this.channel.postMessage(msg);
-
-  msg.type = 'entry-deleted';
+  const entry_msg = {};
+  entry_msg.type = 'entry-deleted';
+  entry_msg.id = 0;
+  entry_msg.reason = reason;
+  entry_msg.feed_id = feed_id;  // retain correspondence for listener
   for (const id of entry_ids) {
-    msg.id = id;
-    this.channel.postMessage(msg);
+    entry_msg.id = id;
+    this.channel.postMessage(entry_msg);
   }
 
-  // Deleting a feed may have deleted one or more entries that were in the
-  // unread state and were contributing to the total unread count, so the badge
-  // text is out of date, so this refreshes the badge. Because the call to
-  // `refresh_badge` is un-awaited it will still be pending at time of
-  // resolution of db_delete_feed. The database connection can still be closed
-  // immediately on resolution of the outer promise because the refresh call
-  // occurs in the same tick, before the close-pending flag has been set, and
-  // IDBDatabase.prototype.close can be called while transactions are still
-  // pending (they still settle, the close call just waits).
-  // TODO: should this not be a concern? should it instead be the caller's
-  // concern? As in, the concern of some outer layer? I do not love how this
-  // has a sort of circular dependency between layers
+  // A lot of commentary because this is complexity shoved under the rug
 
-  // TODO: should get conn from event rather than from context? This is
-  // stylistic issue and I do not know which is better
-
+  // Deleting entries potentially modifies unread-count, so to maintain the
+  // property of the badge unread count reflecting the state of the app, the
+  // badge needs to be refreshed. In other words I want view state to be in sync
+  // with database state. At the same time, I do not want to prolong the
+  // life of the delete-feed call while it does this housekeeping work, so I
+  // want to call refresh-badge unawaited. Because the call to refresh-badge is
+  // unawaited it will still be pending when the delete-feed promise resolves.
+  // The database connection can still be closed immediately on resolution of
+  // the delete-feed promise because the refresh call at least starts in the
+  // same tick as delete-feed, before the close-pending flag has been set as a
+  // result of calling indexedDB's close method, and because indexedDB's close
+  // method can be called while transactions are still pending.
+  // TODO: avoid circular layer dependency, unfortunately i made the design
+  // choice of integrating channel calls into db layer, but have no way to
+  // listen to channel messages elsewhere because nothing else is guaranteed at
+  // this point to point to a loaded-page that is listening, the only thing we
+  // know is that some view is loaded, whichever view is calling this, but we do
+  // not know which view. refresh-badge exists in the view layer but does not
+  // have its own view, it is a sub-view of sorts. Maybe add an entry-deleted
+  // event listener to every view that refreshes and remove this? Overhaul the
+  // vision for refresh-badge? I want this to be responsible for maintaining
+  // this view-to-db sync correspondence at the same time as not wanting it to
+  // be a concern of this module.
+  // TODO: having a design where the lifetime of forked functions outlives the
+  // calling function lifetime leads to some complexity, I think this complexity
+  // smells bad, and really want to think of a better way to do this. I want
+  // operations to complete (as in, really settle where resource usage becomes
+  // 0), when they say they complete (when they exit). This is basically
+  // misleading by having delete-feed claim it is finished when it is actually
+  // not. It is like a coincidental side-effect. Also hard to test. At the
+  // same time I do not want to needlessly prolongue the lifetime of this
+  // function. Maybe I should? Maybe this should be awaited?
+  // NOTE: the catch is important, otherwise errors get swallowed, this is yet
+  // another reason why forking is nasty business.
   refresh_badge(this.conn).catch(log);
 
-  // TODO: if entry ids are posted to the channel then resolving with this
-  // array is redundant. It would be better then to resolve with undefined.
-
-  callback(entry_ids);
+  callback();
 }
