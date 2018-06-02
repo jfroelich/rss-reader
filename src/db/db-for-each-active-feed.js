74834567196fadad97cd5f26f48e76ea89f750ec
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-02 11:13:43 -0400

    [db-for-each-active-feed] comment cleanup

--- src/db/db-for-each-active-feed.js
+++ src/db/db-for-each-active-feed.js
@@ -1,10 +1,9 @@
 import {log} from '/src/log.js';
 
-// TODO: rename to find-active-feeds, it is ok to keep this api style of using
-// a handler callback function
-
-// TODO: actually merge it into find-feeds, with an is-active parameter to the
-// query
+// TODO: deprecate. Instead, create a read-feeds.js module that accepts a query
+// with something like {criteria: active} that yields an array.
+// TODO: consider using an index on active to speed this up
+// TODO: consider a limit parameter
 
 // Async. Queries the database for active feeds and then calls the `handle_feed`
 // function argument on each feed. The handler function is called *during*
@@ -12,28 +11,12 @@ import {log} from '/src/log.js';
 // Completes when iteration over the feeds in the database completes. *Iteration
 // may complete prior to all handlers completing*. In other words, iteration
 // over the feeds can complete while one or more handlers are pending.
-
-// ### Params
-// * **conn** {IDBDatabase} an open database connection to the feed database
-// * **handle_feed** {Function} called for each active feed with the
+// @param conn {IDBDatabase} an open database connection to the feed database
+// @param handle_feed {Function} called for each active feed with the
 // deserialized feed object as its sole argument, the function should be
 // synchronous and pure (free of side effects, at least persistent state
 // changing ones)
-
-// ### Return value
-// Returns a promise. The return value of the promise itself is undefined, or at
-// least, the return value of the promise should be ignored.
-
-// ### TODOS
-// * Refactor db-get-feeds as for-each-feed, have for-each-feed accept a
-// predicate parameter, then remove the implementation here and have it simply
-// decorate a call to for-each-feed with a feed-is-active predicate, or have the
-// caller just call for-each-feed directly and deprecate this operation
-// * Eventually, consider using an index on active to speed this up. I am not at
-// the moment because I do not think there is much of a speed increase, and
-// prefer not to use an index unless there is a material performance benefit
-// * consider a limit parameter that applies to number of active feeds evaluated
-
+// @return {Promise} resolves to undefined
 export function db_for_each_active_feed(conn, handle_feed) {
   return new Promise(executor.bind(null, conn, handle_feed));
 }
