d8c1fdcc65470512951766deda5737b6c0c68cc4
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-02 11:02:59 -0400

    [db-find-feed-by-id] minor cleanup
    cleanup comment, move immediately thrown exception into promise as
    rejection

--- src/db/db-find-feed-by-id.js
+++ src/db/db-find-feed-by-id.js
@@ -1,20 +1,18 @@
 import {is_valid_feed_id} from '/src/feed.js';
 
-// TODO: create find-feed.js, merge db-find-feed-by-id and db-find-feed-by-url
-// into find-feed.js
-// TODO: actually, create read-feed.js, and read-feeds.js. All related
-// functionality should be merged into these two functions. This is essentially
-// an attempt at a REST-style API. Should do similar to entries
+// TODO: create read-feed.js, and read-feeds.js. All related functionality
+// should be merged into these two functions. This is essentially an attempt at
+// a REST-style API. Should do similar to entries
 
 export function db_find_feed_by_id(conn, id) {
-  if (!is_valid_feed_id(id)) {
-    throw new TypeError('Invalid feed id ' + id);
-  }
-
   return new Promise(executor.bind(null, conn, id));
 }
 
 function executor(conn, id, resolve, reject) {
+  if (!is_valid_feed_id(id)) {
+    throw new TypeError('Invalid feed id ' + id);
+  }
+
   const txn = conn.transaction('feed');
   const store = txn.objectStore('feed');
   const request = store.get(id);
