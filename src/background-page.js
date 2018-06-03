8b383bbc83d079dcb6ea86ac98d865fdb9ac3fdb
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-03 17:20:36 -0400

    [cron] split off from background-page

--- src/background-page.js
+++ src/background-page.js
@@ -1,15 +1,13 @@
 import '/src/cli.js';
-
+import '/src/cron.js';
 import {refresh_badge} from '/src/badge.js';
-import {db_archive_entries} from '/src/db/db-archive-entries.js';
 import {db_open} from '/src/db/db-open.js';
-import {db_remove_lost_entries} from '/src/db/db-remove-lost-entries.js';
-import {db_remove_orphaned_entries} from '/src/db/db-remove-orphaned-entries.js';
-import {favicon_compact, favicon_create_conn, favicon_refresh_feeds} from '/src/favicon.js';
 import {register_install_listener} from '/src/install.js';
 import {log} from '/src/log.js';
 import {open_view} from '/src/open-view.js';
-import {poll_feeds} from '/src/poll/poll-feeds.js';
+
+// TODO: just use console.log directly and decouple from log library, logging
+// here does not need to be toggleable
 
 // Loaded exclusively by the background page. This page is loaded via the
 // background page instead of directly via the scripts property in the manifest.
@@ -24,95 +22,6 @@ import {poll_feeds} from '/src/poll/poll-feeds.js';
 // * Handling app installation and updates
 // * Exposing cli functionality to the console for the background page
 // * Cron jobs (via chrome.alarms)
-//
-// TODO: Consider sharing functionality between alarms and cli, both libs do
-// roughly the same thing. Currently there is some redundancy and real
-// similarity between the bodies of the functions in each lib. Maybe have a
-// layer called headless-tasks that provides functions that carry out a task.
-// Then have both the cli and alarm wakeup handlers just call out to this helper
-// module.
-// TODO: Spend some time thinking more about testing
-// TODO: Configurable cron settings
-// TODO: move alarms back its own module, maybe call it cron.js
-
-async function handle_compact_favicons_alarm(alarm) {
-  await favicon_compact();
-}
-
-async function handle_archive_alarm_wakeup(alarm) {
-  const conn = await db_open();
-  const channel = new BroadcastChannel(localStorage.channel_name);
-  await db_archive_entries(conn, channel);
-  channel.close();
-  conn.close();
-}
-
-async function handle_lost_entries_alarm(alarm) {
-  const op = {};
-  op.conn = await db_open();
-  op.channel = new BroadcastChannel(localStorage.channel_name);
-  op.db_remove_lost_entries = db_remove_lost_entries;
-  await op.db_remove_lost_entries();
-  op.conn.close();
-  op.channel.close();
-}
-
-async function handle_orphan_entries_alarm(alarm) {
-  const op = {};
-  op.conn = await db_open();
-  op.channel = new BroadcastChannel(localStorage.channel_name);
-  op.db_remove_orphaned_entries = db_remove_orphaned_entries;
-  await op.db_remove_orphaned_entries();
-  op.conn.close();
-  op.channel.close();
-}
-
-async function handle_refresh_icons_alarm(alarm) {
-  const proms = [db_open(), favicon_create_conn()];
-  const [rconn, iconn] = await Promise.all(proms);
-  const channel = new BroadcastChannel(localStorage.channel_name);
-
-  const op = {};
-  op.rconn = rconn;
-  op.iconn = iconn;
-  op.channel = channel;
-  op.favicon_refresh_feeds = favicon_refresh_feeds;
-  await op.favicon_refresh_feeds();
-
-  rconn.close();
-  iconn.close();
-  channel.close();
-}
-
-async function handle_poll_feeds_alarm(alarm) {
-  if ('ONLY_POLL_IF_IDLE' in localStorage) {
-    const idle_period_secs = 30;
-    const state = await query_idle_state(idle_period_secs);
-    if (state !== 'locked' || state !== 'idle') {
-      return;
-    }
-  }
-
-  const options = {};
-  options.ignore_recency_check = false;
-  options.notify = true;
-
-  const rconn = await db_open();
-  const iconn = await favicon_create_conn();
-  const channel = new BroadcastChannel(localStorage.channel_name);
-
-  await poll_feeds(rconn, iconn, channel, options);
-
-  channel.close();
-  iconn.close();
-  rconn.close();
-}
-
-function query_idle_state(idle_period_secs) {
-  return new Promise(function executor(resolve, reject) {
-    chrome.idle.queryState(idle_period_secs, resolve);
-  });
-}
 
 // On module load, register the install listener
 // TODO: somehow do not do this on every page load, no idea how though
@@ -128,41 +37,3 @@ async function badge_init() {
 }
 
 badge_init();
-
-chrome.alarms.onAlarm.addListener(function(alarm) {
-  log('onalarm: alarm name', alarm.name);
-  localStorage.LAST_ALARM = alarm.name;
-
-  switch (alarm.name) {
-    case 'archive':
-      handle_archive_alarm_wakeup(alarm).catch(log);
-      break;
-    case 'poll':
-      handle_poll_feeds_alarm(alarm).catch(log);
-      break;
-    case 'remove-entries-missing-urls':
-      handle_lost_entries_alarm(alarm).catch(log);
-      break;
-    case 'remove-orphaned-entries':
-      handle_orphan_entries_alarm(alarm).catch(log);
-      break;
-    case 'refresh-feed-icons':
-      handle_refresh_icons_alarm(alarm).catch(log);
-      break;
-    case 'compact-favicon-db':
-      handle_compact_favicons_alarm(alarm).catch(log);
-      break;
-    default:
-      log('unhandled alarm', alarm.name);
-      break;
-  }
-});
-
-chrome.alarms.create('archive', {periodInMinutes: 60 * 12});
-chrome.alarms.create('poll', {periodInMinutes: 60});
-chrome.alarms.create(
-    'remove-entries-missing-urls', {periodInMinutes: 60 * 24 * 7});
-chrome.alarms.create(
-    'db-remove-orphaned-entries', {periodInMinutes: 60 * 24 * 7});
-chrome.alarms.create('refresh-feed-icons', {periodInMinutes: 60 * 24 * 7 * 2});
-chrome.alarms.create('compact-favicon-db', {periodInMinutes: 60 * 24 * 7});
---
+++ src/cron.js
@@ -0,0 +1,135 @@
+import {db_archive_entries} from '/src/db/db-archive-entries.js';
+import {db_open} from '/src/db/db-open.js';
+import {db_remove_lost_entries} from '/src/db/db-remove-lost-entries.js';
+import {db_remove_orphaned_entries} from '/src/db/db-remove-orphaned-entries.js';
+import {favicon_compact, favicon_create_conn, favicon_refresh_feeds} from '/src/favicon.js';
+import {log} from '/src/log.js';
+import {poll_feeds} from '/src/poll/poll-feeds.js';
+
+// TODO: configurable cron settings?
+// TODO: decouple from log module, just use console.log directly
+// TODO: shove the on-load stuff into a function so that it is more configurable
+// regarding when the binding occurs as opposed to being tightly coupled to the
+// onload event? maybe even export it and have importing module call it, so that
+// concern is shifted somewhere else? Or maybe the concern is fine to hide here?
+// Part of the issue is also that onload stuff should not happen every module
+// load, it should only happen on app update
+// TODO: invent a method of cleaning up outdated alarms, because simply using
+// a new name for an alarm does not de-register its old listener from Chrome
+
+async function handle_compact_favicons_alarm(alarm) {
+  await favicon_compact();
+}
+
+async function handle_archive_alarm_wakeup(alarm) {
+  const conn = await db_open();
+  const channel = new BroadcastChannel(localStorage.channel_name);
+  await db_archive_entries(conn, channel);
+  channel.close();
+  conn.close();
+}
+
+async function handle_lost_entries_alarm(alarm) {
+  const op = {};
+  op.conn = await db_open();
+  op.channel = new BroadcastChannel(localStorage.channel_name);
+  op.db_remove_lost_entries = db_remove_lost_entries;
+  await op.db_remove_lost_entries();
+  op.conn.close();
+  op.channel.close();
+}
+
+async function handle_orphan_entries_alarm(alarm) {
+  const op = {};
+  op.conn = await db_open();
+  op.channel = new BroadcastChannel(localStorage.channel_name);
+  op.db_remove_orphaned_entries = db_remove_orphaned_entries;
+  await op.db_remove_orphaned_entries();
+  op.conn.close();
+  op.channel.close();
+}
+
+async function handle_refresh_icons_alarm(alarm) {
+  const proms = [db_open(), favicon_create_conn()];
+  const [rconn, iconn] = await Promise.all(proms);
+  const channel = new BroadcastChannel(localStorage.channel_name);
+
+  const op = {};
+  op.rconn = rconn;
+  op.iconn = iconn;
+  op.channel = channel;
+  op.favicon_refresh_feeds = favicon_refresh_feeds;
+  await op.favicon_refresh_feeds();
+
+  rconn.close();
+  iconn.close();
+  channel.close();
+}
+
+async function handle_poll_feeds_alarm(alarm) {
+  if ('ONLY_POLL_IF_IDLE' in localStorage) {
+    const idle_period_secs = 30;
+    const state = await query_idle_state(idle_period_secs);
+    if (state !== 'locked' || state !== 'idle') {
+      return;
+    }
+  }
+
+  const options = {};
+  options.ignore_recency_check = false;
+  options.notify = true;
+
+  const rconn = await db_open();
+  const iconn = await favicon_create_conn();
+  const channel = new BroadcastChannel(localStorage.channel_name);
+
+  await poll_feeds(rconn, iconn, channel, options);
+
+  channel.close();
+  iconn.close();
+  rconn.close();
+}
+
+function query_idle_state(idle_period_secs) {
+  return new Promise(function executor(resolve, reject) {
+    chrome.idle.queryState(idle_period_secs, resolve);
+  });
+}
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  log('onalarm: alarm name', alarm.name);
+  localStorage.LAST_ALARM = alarm.name;
+
+  switch (alarm.name) {
+    case 'archive':
+      handle_archive_alarm_wakeup(alarm).catch(log);
+      break;
+    case 'poll':
+      handle_poll_feeds_alarm(alarm).catch(log);
+      break;
+    case 'remove-entries-missing-urls':
+      handle_lost_entries_alarm(alarm).catch(log);
+      break;
+    case 'remove-orphaned-entries':
+      handle_orphan_entries_alarm(alarm).catch(log);
+      break;
+    case 'refresh-feed-icons':
+      handle_refresh_icons_alarm(alarm).catch(log);
+      break;
+    case 'compact-favicon-db':
+      handle_compact_favicons_alarm(alarm).catch(log);
+      break;
+    default:
+      log('unhandled alarm', alarm.name);
+      break;
+  }
+});
+
+chrome.alarms.create('archive', {periodInMinutes: 60 * 12});
+chrome.alarms.create('poll', {periodInMinutes: 60});
+chrome.alarms.create(
+    'remove-entries-missing-urls', {periodInMinutes: 60 * 24 * 7});
+chrome.alarms.create(
+    'db-remove-orphaned-entries', {periodInMinutes: 60 * 24 * 7});
+chrome.alarms.create('refresh-feed-icons', {periodInMinutes: 60 * 24 * 7 * 2});
+chrome.alarms.create('compact-favicon-db', {periodInMinutes: 60 * 24 * 7});
