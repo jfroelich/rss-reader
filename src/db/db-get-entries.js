dc67d0abbbc35487f0365c157d57401f443522bb
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-03 20:54:45 -0400

    [db-get-entries] some comment cleanup

--- src/db/db-get-entries.js
+++ src/db/db-get-entries.js
@@ -1,16 +1,5 @@
 import {ENTRY_STATE_UNARCHIVED, ENTRY_STATE_UNREAD} from '/src/entry.js';
 
-// TODO: this is a placeholder. My goal is to unify all of the various queries
-// that involve loading multiple entries.
-// * Using CRUD and/or REST-API terminology, use method names like
-// get-create-update-delete, and stop trying to use file metaphor of read/write.
-// * load all into array up, give up on the cursor-callback approach.
-// * as a halfway improvement, have a query-category parameter that represents
-// which type of query should be performed
-// * merge in one module at a time is the plan, going top down in folder
-// alphabetical order
-// * give up on using context
-
 export async function db_get_entries(conn, options = {}) {
   return new Promise(db_get_entries_executor.bind(null, conn, options));
 }
@@ -29,18 +18,14 @@ function db_get_entries_executor(conn, options, resolve, reject) {
   const txn = conn.transaction('entry');
   txn.oncomplete = _ => resolve(shared_state.entries);
   txn.onerror = _ => reject(txn.error);
-
   const store = txn.objectStore('entry');
 
-  // Based on the mode, fire off the proper openCursor request and start
-  // iterating entries
   let request;
   if (options.mode === 'viewable') {
     const index = store.index('archiveState-readState');
     const key_path = [ENTRY_STATE_UNARCHIVED, ENTRY_STATE_UNREAD];
     request = index.openCursor(key_path);
   } else {
-    // Default to load all
     request = store.openCursor();
   }
 
@@ -50,6 +35,7 @@ function db_get_entries_executor(conn, options, resolve, reject) {
 function request_onsuccess(state, event) {
   const cursor = event.target.result;
   if (!cursor) {
+    // Either no entries found, or all entries iterated
     return;
   }
 
@@ -63,15 +49,13 @@ function request_onsuccess(state, event) {
 
   state.entries.push(cursor.value);
 
-  if (state.limit > 0) {
-    // If limit specified, then only advance if under limit.
-    if (state.entries.length < state.limit) {
-      cursor.continue();
-    }
-  } else {
-    // Always advance if unlimited
+  // If limit specified, then only advance if under limit
+  if (state.limit > 0 && state.entries.length < state.limit) {
     cursor.continue();
+    return;
   }
+
+  cursor.continue();
 }
 
 function is_valid_limit(limit) {
