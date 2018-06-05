e1bd398a7c2d8794acb555673c19a2a8e1b91136
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-05 10:21:34 -0400

    [db-get-entries] fix limited loading
    This was always advanced past limit due to a logic error. Now this correctly halts once limit is reached. Should help fix some of the slideshow-view-load jank

--- src/db/db-get-entries.js
+++ src/db/db-get-entries.js
@@ -15,20 +15,18 @@ import {ENTRY_STATE_UNARCHIVED, ENTRY_STATE_UNREAD} from '/src/entry.js';
 // @return {Promise} resolves to an array of entry objects
 export async function db_get_entries(
     conn, mode = 'all', offset = 0, limit = -1) {
-  return new Promise(
-      db_get_entries_executor.bind(null, conn, mode, offset, limit));
+  return new Promise(executor.bind(null, conn, mode, offset, limit));
 }
 
-function db_get_entries_executor(conn, mode, offset, limit, resolve, reject) {
+function executor(conn, mode, offset, limit, resolve, reject) {
   assert(mode && typeof mode === 'string');
   assert(is_valid_offset(offset));
   assert(is_valid_limit(limit));
 
-  const shared_state =
-      {entries: [], advanced: false, limit: limit, offset: offset};
+  const state = {entries: [], advanced: false, limit: limit, offset: offset};
 
   const txn = conn.transaction('entry');
-  txn.oncomplete = _ => resolve(shared_state.entries);
+  txn.oncomplete = _ => resolve(state.entries);
   txn.onerror = _ => reject(txn.error);
   const store = txn.objectStore('entry');
 
@@ -43,7 +41,7 @@ function db_get_entries_executor(conn, mode, offset, limit, resolve, reject) {
     throw new TypeError('Invalid mode ' + mode);
   }
 
-  request.onsuccess = request_onsuccess.bind(request, shared_state);
+  request.onsuccess = request_onsuccess.bind(request, state);
 }
 
 function request_onsuccess(state, event) {
@@ -63,9 +61,14 @@ function request_onsuccess(state, event) {
 
   state.entries.push(cursor.value);
 
-  // If limit specified, then only advance if under limit
-  if (state.limit > 0 && state.entries.length < state.limit) {
-    cursor.continue();
+  // If limit specified
+  if (state.limit > 0) {
+    // Advance if under limit
+    if (state.entries.length < state.limit) {
+      cursor.continue();
+    }
+
+    // Always exit. We may or may not have advanced
     return;
   }
 
