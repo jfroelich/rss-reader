ee9d7b34d218c75e830fc8bf188bd8d318ae4670
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-13 23:37:00 -0400

    [is-external-url] deprecate, merge into lonestar

--- src/lib/filters/lonestar-filter.js
+++ src/lib/filters/lonestar-filter.js
@@ -2,10 +2,13 @@ import {element_is_hidden_inline} from '/src/lib/dom/element-is-hidden-inline.js
 import {remove_image} from '/src/lib/dom/remove-image.js';
 import {filter_anchor_noref} from '/src/lib/filters/filter-anchor-noref.js';
 import {filter_pings} from '/src/lib/filters/filter-pings.js';
-import {is_external_url} from '/src/lib/net/is-external-url.js';
 
 // The lonestar filter is tasked with jamming radars. A guide to anti-telemetry
 // can be found here: https://youtu.be/rGvblGCD7qM
+
+const EXTERNAL_MODE_UNKNOWN = 0;
+const EXTERNAL_MODE_STRICT = 2;
+const EXTERNAL_MODE_SLOPPY = 1;
 
 // Raspberry expressions for radars
 const telemetry_host_patterns = [
@@ -211,4 +214,76 @@ function image_has_telemetry_source(image, document_url) {
 
   // Nothing indicated telemetry.
   return false;
+}
+
+// Returns true when the other_url is external to the document_url. A url is
+// external when it comes from a different website. This could be because the
+// origin is different. However, this may allow for ignoring differences in the
+// subdomain, by only looking at the top domain.
+// WARNING: this is insecure because it is approximate
+function is_external_url(
+    document_url, other_url, mode = EXTERNAL_MODE_UNKNOWN) {
+  if (!(document_url instanceof URL)) {
+    throw new TypeError('document_url is not a URL');
+  }
+
+  if (!(other_url instanceof URL)) {
+    throw new TypeError('other_url is not a URL');
+  }
+
+  const local_protocols = ['data:', 'mailto:', 'tel:', 'javascript:'];
+  if (local_protocols.includes(other_url.protocol)) {
+    return false;
+  }
+
+  if (mode === EXTERNAL_MODE_STRICT) {
+    return document_url.origin !== other_url.origin;
+  }
+
+  const doc_domain = url_get_upper_domain(document_url);
+  const other_domain = url_get_upper_domain(other_url);
+  return doc_domain !== other_domain;
+}
+
+// NOTE: ignores port
+function url_get_upper_domain(url) {
+  if (hostname_is_ipv4(url.hostname) || hostname_is_ipv6(url.hostname)) {
+    return url.hostname;
+  }
+
+  const levels = url.hostname.split('.');
+
+  // Handle the simple case of localhost or example.com
+  if (levels.length < 3) {
+    return url.hostname;
+  }
+
+  // Using a full geo-suffix list is overkill so use tld length to guess
+  const top_level = levels[levels.length - 1];
+  const reverse_offset = top_level.length === 2 ? -3 : -2;
+  return levels.slice(reverse_offset).join('.');
+}
+
+function hostname_is_ipv4(string) {
+  if (typeof string !== 'string') {
+    return false;
+  }
+
+  const parts = string.split('.');
+  if (parts.length !== 4) {
+    return false;
+  }
+
+  for (const part of parts) {
+    const digit = parseInt(part, 10);
+    if (isNaN(digit) || digit < 0 || digit > 255) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function hostname_is_ipv6(value) {
+  return typeof value === 'string' && value.includes(':');
 }
