f87a3bcf4abdbd6ba4a3c6083a25f0f4fdecc306
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-06-15 10:04:45 -0400

    [fetch-image-element] deprecate lib, fold into set-image-sizes

--- src/lib/filters/set-image-sizes.js
+++ src/lib/filters/set-image-sizes.js
@@ -1,5 +1,3 @@
-import {fetch_image_element} from '/src/lib/net/fetch-image-element.js';
-
 // Scans the images of a document and ensures the width and height attributes
 // are set. If images are missing dimensions then this fetches the dimensions
 // and modifies each image element's attributes.
@@ -145,4 +143,52 @@ function get_style_dims(element) {
       }
     }
   }
+}
+
+// TODO: avoid sending cookies, probably need to use fetch api
+async function fetch_image_element(url, timeout, is_allowed_request) {
+  assert(
+      typeof timeout === 'undefined' || timeout === null ||
+      (Number.isInteger(timeout) && timeout >= 0));
+  const fetch_promise = fetch_exec(url, is_allowed_request);
+  const contestants =
+      timeout ? [fetch_promise, sleep(timeout)] : [fetch_promise];
+  const image = await Promise.race(contestants);
+  assert(image, 'Timed out fetching image url ' + url.href);
+  return image;
+}
+
+function fetch_exec(url, is_allowed_request) {
+  return new Promise((resolve, reject) => {
+    assert(url instanceof URL);
+    assert(is_allowed_request('get', url));
+
+    // Create a proxy element within this script's document
+    const proxy = new Image();
+    // Set the proxy's source to trigger the fetch
+    proxy.src = url.href;
+
+    // If cached then resolve immediately
+    if (proxy.complete) {
+      return resolve(proxy);
+    }
+
+    proxy.onload = () => resolve(proxy);
+    proxy.onerror = (event) => {
+      // NOTE: the event does not contain a useful error object, or any error
+      // information at all really, so create our own error
+      reject(new Error('Unknown error fetching image ' + url.href));
+    };
+  });
+}
+
+// Returns a promise that resolves to undefined after a given amount of time (in
+// milliseconds). By racing this promise against another promise, this is useful
+// for imposing a timeout on the other operation.
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function assert(value, message) {
+  if (!value) throw new Error(message || 'Assertion error');
 }
