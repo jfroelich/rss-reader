06117b29962814a26617b916b040fae0e24ecef4
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-16 11:07:37 -0400

    [count-unread-slides] factor in read-pending in unread count

--- src/slideshow-page/count-unread-slides.js
+++ src/slideshow-page/count-unread-slides.js
@@ -1,8 +1,6 @@
 // Returns the number of unread slide elements present in the view
 export function count_unread_slides() {
-  // Find all slide elements within the slideshow container element that do not
-  // have the read attribute
-  const selector = '#slideshow-container > slide:not([read])';
+  const selector = 'slide:not([read]):not([read-pending])';
   const slides = document.body.querySelectorAll(selector);
   return slides.length;
 }
--- src/slideshow-page/slide-nav.js
+++ src/slideshow-page/slide-nav.js
@@ -7,87 +7,47 @@ import {remove_slide} from '/src/slideshow-page/remove-slide.js';
 import * as slideshow_state from '/src/slideshow-page/slideshow-state.js';
 
 export async function show_next_slide() {
+  if (slideshow_state.get_active_transition_count()) {
+    return;
+  }
+
   const current_slide = slideshow_state.get_current_slide();
-  // There may not be a current slide (this is routine)
   if (!current_slide) {
     return;
   }
 
-  // NOTE: the current slide contributes to the unread count
-  let slide_unread_count = count_unread_slides();
-
-  // TEMP: monitoring buggy functionality in console
-  console.debug('Found %d unread slides before marking', slide_unread_count);
-
   const conn = await db.open_db();
   await mark_slide_read_start(conn, current_slide);
 
+  const slide_unread_count = count_unread_slides();
   let entries = [];
-
-  // TODO: this condition needs some tweaking to reflect the new offset
-  // calculation. The current behavior is harmless, but I would rather not even
-  // try to load entries. basically this condition should consider read-pending
-  // along with read. slide_unread_count only considers read at the moment.
-  // TODO: furthermore, it may be just that slide_unread_count should be
-  // considering read-pending, or that I should use some separate function that
-  // makes it clear it considers both states. There is no other use of
-  // slide_unread_count in this function so it kind of makes sense. I would not
-  // need a separate offset calculation here either.
   if (slide_unread_count < 3) {
-    const selector = 'slide:not([read]):not([read-pending])';
-    const slides = document.body.querySelectorAll(selector);
-    const offset = slides.length;
-
-    // TEMP: monitoring new offset calculation
-    console.debug(
-        'Maybe loading entries, offset %d, unread %d', offset,
-        slide_unread_count);
-
-    entries = await load_entries(conn, offset);
+    // TODO: cleanup this "|| 3" crap
+    const limit = localstorage.read_int('initial_entry_load_limit') || 3;
+    const mode = 'viewable';
+    entries = await db.get_entries(conn, mode, slide_unread_count, limit);
   }
   conn.close();
 
-  // If we loaded some more entries, append them as slides. Do this prior to
-  // transitioning to allow for dynamic loading.
-  // TODO: if things work, this condition isn't needed, as the loop noops
-  if (entries.length) {
-    // TEMP: monitoring recent changes
-    console.debug('Appending %d slides', entries.length);
-    for (const entry of entries) {
-      // TEMP: investigating sporadic error. This condition should not be needed
-      // but it occassionally is at the moment. Although I may have fixed it
-      // with changes to offset calculation above.
-      if (document.querySelector('slide[entry="' + entry.id + '"]')) {
-        console.warn('Entry already loaded, not appending again', entry.id);
-        continue;
-      }
-
+  for (const entry of entries) {
+    if (!document.querySelector('slide[entry="' + entry.id + '"]')) {
       append_slide(entry);
+    } else {
+      console.debug('Entry already loaded', entry.id);
     }
   }
 
-  if (!slideshow_state.get_active_transition_count()) {
-    const next_slide = current_slide.nextElementSibling;
-    if (next_slide) {
-      slideshow_state.increment_active_transition_count();
-      current_slide.style.left = '-100%';  // hide old
-      slideshow_state.increment_active_transition_count();
-      next_slide.style.left = '0';  // show new
-      slideshow_state.set_current_slide(next_slide);
-    }
+  const next_slide = current_slide.nextElementSibling;
+  if (next_slide) {
+    slideshow_state.increment_active_transition_count();
+    current_slide.style.left = '-100%';
+    next_slide.style.left = '0';
+    slideshow_state.set_current_slide(next_slide);
   }
 
   if (entries.length) {
     compact_slides();
   }
-}
-
-// Returns a promise
-function load_entries(conn, offset) {
-  // TODO: cleanup this "|| 3" crap
-  const limit = localstorage.read_int('initial_entry_load_limit') || 3;
-  const mode = 'viewable';
-  return db.get_entries(conn, mode, offset, limit);
 }
 
 function compact_slides() {
@@ -109,27 +69,23 @@ function compact_slides() {
 }
 
 export function show_prev_slide() {
-  if (slideshow_state.get_active_transition_count() > 0) {
-    console.debug('Canceling previous navigation, too many transitions');
+  if (slideshow_state.get_active_transition_count()) {
+    console.debug('Canceling previous navigation');
     return;
   }
 
-  if (!slideshow_state.get_current_slide()) {
+  const current_slide = slideshow_state.get_current_slide();
+  if (!current_slide) {
     return;
   }
 
-  const previous_slide =
-      slideshow_state.get_current_slide().previousElementSibling;
+  const previous_slide = current_slide.previousElementSibling;
   if (!previous_slide) {
     return;
   }
 
   slideshow_state.increment_active_transition_count();
-  slideshow_state.get_current_slide().style.left = '100%';
-
-  // TEMP: testing proper transition count
-  slideshow_state.increment_active_transition_count();
-
+  current_slide.style.left = '100%';
   previous_slide.style.left = '0';
   slideshow_state.set_current_slide(previous_slide);
 }
