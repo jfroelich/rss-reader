8d90b4e25925af9fa270e0969ada674de864c783
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-16 10:12:52 -0400

    [onkeydown] simplify
    Inline the edit intent helper, the condition speaks for itself and is sufficiently idiomatic. Switch from switch to if syntax.

--- src/slideshow-page/onkeydown.js
+++ src/slideshow-page/onkeydown.js
@@ -3,38 +3,30 @@ import {show_next_slide, show_prev_slide} from '/src/slideshow-page/slide-nav.js
 export function onkeydown(event) {
   const code = event.keyCode;
 
-  if (is_edit_intent(event)) {
-    console.debug('Ignoring key down while in input', event.target.localName);
+  if (event.target.localName === 'input' ||
+      event.target.localName === 'textarea') {
+    console.debug(
+        'Ignoring key down while in editable', event.target.localName);
     return;
   }
 
-  const LEFT = 37;
-  const RIGHT = 39;
-  const N = 78;
-  const P = 80;
+  if (is_edit_intent(event)) {
+    return;
+  }
+
+  const LEFT = 37, RIGHT = 39;
+  const N = 78, P = 80;
   const SPACE = 32;
 
-  switch (code) {
-    case RIGHT:
-    case N:
-    case SPACE: {
-      event.preventDefault();
-      show_next_slide();
-      break;
-    }
-
-    case LEFT:
-    case P: {
-      event.preventDefault();
-      show_prev_slide();
-      break;
-    }
-
-    default: { break; }
+  if (code === RIGHT || code === N || code === SPACE) {
+    event.preventDefault();
+    show_next_slide();
+    return;
   }
-}
 
-function is_edit_intent(event) {
-  return event.target.localName === 'input' ||
-      event.target.localName === 'textarea';
+  if (code === LEFT || code === P) {
+    event.preventDefault();
+    show_prev_slide();
+    return;
+  }
 }
