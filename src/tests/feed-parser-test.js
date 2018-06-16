e29208719830ab6ed1e8151fa4bd9e74b50c0cc2
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-16 17:58:59 -0400

    [feed-parser-test] remove unused import, fix comment

--- src/tests/feed-parser-test.js
+++ src/tests/feed-parser-test.js
@@ -1,11 +1,9 @@
-import {fetch_feed} from '/src/fetch-feed.js';
 import {parse_feed} from '/src/lib/parse-feed.js';
 import {assert} from '/src/tests/assert.js';
 import {register_test} from '/src/tests/test-registry.js';
 
-// TODO: this needs to be run on a local resource instead of fetching a remote
-// one
-// TODO: this cannot accept parameters
+// TODO: implement. note that this should run by loading a local resource, or
+// create a document in memory during the test
 
 async function feed_parser_test() {
   return true;
