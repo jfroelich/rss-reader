34dfeb63f9b150c025a09d78bee6139fc31d3744
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-06-20 23:07:38 -0400

    [entry-control] first draft
    Move delete-entry out of db.js

--- src/db/db.js
+++ src/db/db.js
@@ -10,22 +10,6 @@ import {filter_unprintable_characters} from '/src/lang/filter-unprintable-charac
 import * as Entry from '/src/model/entry.js';
 import * as Feed from '/src/model/feed.js';
 
-// Deletes an entry from the database. Unlike delete_feed this does not expose
-// feed id in its message because it would require an extra lookup.
-export function delete_entry(conn, channel, id, reason) {
-  return new Promise((resolve, reject) => {
-    assert(Entry.is_valid_entry_id(id));  // prevent fake noops
-    const txn = conn.transaction('entry', 'readwrite');
-    txn.oncomplete = _ => {
-      const msg = {type: 'entry-deleted', id: id, reason: reason};
-      channel.postMessage(msg);
-      resolve();
-    };
-    txn.onerror = _ => reject(txn.error);
-    txn.objectStore('entry').delete(id);
-  });
-}
-
 // Load an array of entries from the database
 export function get_entries(conn, mode = 'all', offset = 0, limit = 0) {
   return new Promise((resolve, reject) => {
