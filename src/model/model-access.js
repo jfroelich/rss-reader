3c2c33b0c98a5e465c99fdc7034917dbdcc2824c
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-06-29 09:41:44 -0400

    [model-access] cleanup and commentary

--- src/model/model-access.js
+++ src/model/model-access.js
@@ -1,9 +1,30 @@
 import assert from '/src/lib/assert.js';
 import * as indexeddb from '/src/lib/indexeddb.js';
-import * as ls from '/src/lib/ls.js';
 import * as object from '/src/lib/object.js';
 import * as Model from '/src/model/model.js';
 import sizeof from '/src/lib/sizeof.js';
+
+// Connects to the model. Instantiates and returns a wrapper class that serves
+// as a data access layer for reading from and writing to app persistent storage
+//
+// @param is_channeled {Boolean} optional, if true then this establishes a
+// connection to the app's broadcast channel that will potentially be used by
+// methods that modify database state. Generally, use true when reading and
+// writing, and use false when only reading.
+// @param name {String} optional, database name
+// @param version {Number} optional, database version
+// @param timeout {Number} optional, ms before considering opening attempt a
+// failure
+// @throws {DOMException} some kind of database error, failed to connect,
+// failed to apply upgrades to new version, etc.
+// @throws {Error} invalid arguments
+// @throws {Error} failed to create channel
+// @return {Promise} resolves to a ModelAccess object
+export async function openModelAccess(is_channeled, name, version, timeout) {
+  const ma = new ModelAccess();
+  await ma.connect(is_channeled, name, version, timeout);
+  return ma;
+}
 
 // Provides a data access layer for interacting with the reader database
 export function ModelAccess() {
@@ -164,16 +185,9 @@ ModelAccess.prototype.close = function() {
   this.conn.close();
 };
 
-export async function openModelAccess(writable, name, version, timeout) {
-  const ma = new ModelAccess();
-  await ma.connect(writable, name, version, timeout);
-  return ma;
-}
-
-ModelAccess.prototype.connect = async function(
-    writable = false, name = 'reader', version = 24, timeout = 500) {
-  // We only need a channel in write mode
-  if (writable) {
+ModelAccess.prototype.connect =
+    async function(is_channeled, name = 'reader', version = 24, timeout = 500) {
+  if (is_channeled) {
     this.channel = new BroadcastChannel('reader');
   }
 
