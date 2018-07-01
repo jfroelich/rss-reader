61817dc1a9e668862848414af4ff0bfe125e5873
Joshua Froelich <joshuafroelich@Joshuas-MacBook-Pro.local>
2018-07-01 17:51:21 -0400

    [test] Shifted some tests from model-access to idb-model

--- test/model/idb-model-test.js
+++ test/model/idb-model-test.js
@@ -30,5 +30,100 @@ async function create_feed_test() {
   await indexeddb.remove(conn.name);
 }
 
-register_test(create_feed_test);
+// Test that double url insert fails, it is expected to throw a
+// DOMException like the following: "Unable to add key to index 'urls': at least
+// one key does not satisfy the uniqueness requirements."
+async function create_feed_url_constraint_test() {
+  const conn = await idbmodel.open('create-feed-url-constraint-test');
+  const feed1 = Model.create_feed();
+  Model.append_feed_url(feed1, new URL('http://www.example.com/example.rss'));
+  await idbmodel.create_feed(conn, feed1);
+
+  const feed2 = Model.create_feed();
+  Model.append_feed_url(feed2, new URL('http://www.example.com/example.rss'));
+
+  let create_error;
+  try {
+    await idbmodel.create_feed(conn, feed2);
+  } catch (error) {
+    create_error = error;
+  }
+  assert(create_error instanceof DOMException);
+
+  conn.close();
+  await indexeddb.remove(conn.name);
+}
+
+
+// Exercise the typical createFeeds case
+async function create_feeds_test() {
+  const conn = await idbmodel.open('create-feeds-test');
+
+  const num_feeds = 3;
+  const feeds = [];
+  for (let i = 0; i < num_feeds; i++) {
+    const feed = Model.create_feed();
+    Model.append_feed_url(feed, new URL('a://b.c' + i));
+    feeds.push(feed);
+  }
+
+  const ids = await idbmodel.create_feeds(conn, feeds);
+  assert(ids.length === num_feeds, '' + ids);
+
+  const stored_feeds = await idbmodel.get_feeds(conn, 'all');
+  assert(stored_feeds.length === num_feeds);
+
+  // Exercise the id check
+  const get_proms = ids.map(id => idbmodel.get_feed(conn, 'id', id));
+  const feeds_by_id = await Promise.all(get_proms);
+  for (const feed of feeds_by_id) {
+    assert(Model.is_feed(feed));
+    assert(Model.is_valid_feed_id(feed.id));
+  }
+
+  conn.close();
+  await indexeddb.remove(conn.name);
+}
+
+
+async function count_unread_entries_test() {
+  const conn = await idbmodel.open('count-unread-entries-test');
+  let count = await idbmodel.count_unread_entries(conn);
+  assert(count === 0);
+
+  const insert_unread_count = 3;
+  const entries_to_insert = [];
+  for (let i = 0; i < insert_unread_count; i++) {
+    const entry = Model.create_entry();
+    entry.readState = Model.ENTRY_STATE_UNREAD;
+    Model.append_entry_url(entry, new URL('a://b.c' + i));
+    entries_to_insert.push(entry);
+  }
+
+  const insert_read_count = 5;
+  for (let i = 0; i < insert_read_count; i++) {
+    const entry = Model.create_entry();
+    entry.readState = Model.ENTRY_STATE_READ;
+    Model.append_entry_url(entry, new URL('d://e.f' + i));
+    entries_to_insert.push(entry);
+  }
+
+  const insert_promises = [];
+  for (const entry of entries_to_insert) {
+    const promise = idbmodel.create_entry(conn, entry);
+    insert_promises.push(promise);
+  }
+  await Promise.all(insert_promises);
+
+  count = await idbmodel.count_unread_entries(conn);
+  assert(count === insert_unread_count);
+
+  conn.close();
+  await indexeddb.remove(conn.name);
+}
+
 register_test(archive_entries_test);
+register_test(create_feed_test);
+register_test(create_feed_url_constraint_test);
+register_test(create_feeds_test);
+register_test(count_unread_entries_test);
--- test/model/model-access-test.js
+++ test/model/model-access-test.js
@@ -4,85 +4,9 @@ import {openModelAccess} from '/src/model/model-access.js';
 import * as Model from '/src/model/model.js';
 import {register_test} from '/test/test-registry.js';
 
-// Test that double url insert fails, it is expected to throw a
-// DOMException with a message about a constraint error because of the unique
-// constraint on the url index of the feed store
-async function create_feed_url_constraint_test() {
-  const feed1 = Model.create_feed();
-  Model.append_feed_url(feed1, new URL('http://www.example.com/example.rss'));
-
-  const feed2 = Model.create_feed();
-  Model.append_feed_url(feed2, new URL('http://www.example.com/example.rss'));
-
-  const ma = await openModelAccess(
-      /* channeled */ false, 'create-feed-url-constraint-test');
-  ma.channel = {name: 'stub', postMessage: noop, close: noop};
-
-  // Store the first feed
-  await ma.createFeed(feed1);
-
-  // Now attempt to store the second feed, trapping the error
-  let create_error = null;
-  try {
-    await ma.createFeed(feed2);
-  } catch (error) {
-    create_error = error;
-  }
-
-  // Assert that storing the second feed failed. The error message is something
-  // like the following: "Unable to add key to index 'urls': at least one key
-  // does not satisfy the uniqueness requirements."
-  assert(create_error instanceof DOMException);
-
-  // Teardown the test
-  ma.channel.close();
-  ma.close();
-  await indexeddb.remove(ma.conn.name);
-}
-
-// Exercise the typical createFeeds case
-async function create_feeds_test() {
-  const msgs = [];
-  const ma = await openModelAccess(/* channeled */ false, 'create-feeds-test');
-  ma.channel = {name: 'stub', postMessage: m => msgs.push(m), close: noop};
-
-  const num_feeds = 3;
-  const feeds = [];
-  for (let i = 0; i < num_feeds; i++) {
-    const feed = Model.create_feed();
-    Model.append_feed_url(feed, new URL('a://b.c' + i));
-    feeds.push(feed);
-  }
-
-  const ids = await ma.createFeeds(feeds);
-  assert(ids.length === num_feeds, '' + ids);
-
-  const stored_feeds = await ma.getFeeds('all', /* title-sort */ false);
-  assert(stored_feeds.length === num_feeds, '' + stored_feeds);
-
-  // Load the feeds individually by id, albeit concurrently. This exercises the
-  // id check, which getFeeds above does not.
-  const feeds_by_id = await Promise.all(ids.map(id => ma.getFeed('id', id)));
-
-  for (const feed of feeds_by_id) {
-    assert(Model.is_feed(feed));
-    assert(Model.is_valid_feed_id(feed.id));
-    assert(feed.active);
-    assert(feed.dateCreated);
-    assert(feed.dateUpdated === undefined);
-  }
-
-  assert(msgs.length === num_feeds);
-  assert(msgs[0].type === 'feed-created');
-  assert(msgs[0].id === feeds_by_id[0].id);
-
-  ma.close();
-  await indexeddb.remove(ma.conn.name);
-}
-
 async function activate_feed_test() {
   const msgs = [];
-  const ma = await openModelAccess(/* channeled */ false, 'activate-feed-test');
+  const ma = await openModelAccess(false, 'activate-feed-test');
   ma.channel = {name: 'stub', postMessage: msg => msgs.push(msg), close: noop};
 
   const feed = Model.create_feed();
@@ -102,82 +26,18 @@ async function activate_feed_test() {
   assert(msgs[1].id === stored_feed.id);
 
   // Activating a feed that is already active should fail
-  // NOTE: so I sort of learned an important lesson about the leaky abstraction
-  // that is promises. My original understanding was that when an exception is
-  // thrown inside of a promise, it is translated into a rejection. That
-  // understanding is incorrect. There is more nuance. In particular, only
-  // immediately-thrown exceptions are translated automatically into rejections.
-  // If an error is thrown in a later event loop iteration (e.g. next tick)
-  // while within the executor of the promise, such as after setTimeout
-  // resolves, or after an indexedDB request event occurs, then the error is NOT
-  // transformed into a rejection. It remains an uncaught exception that leaves
-  // the promise in an unsettled state (the promise never rejects). Therefore,
-  // all promises that do work that occurs after the current event loop epoch
-  // has elapsed must use try/catch in non-immediate contexts or somehow avoid
-  // throwing errors entirely. All code that occurs in a later tick from within
-  // a promise is untrustworthy.
-
-  // TODO: convert model-access to reject instead of assert in the above
-  // situation, as a partial solution.
-  // TODO: perform a thorough review of all promises that do deferred work.
-
   let activation_error;
   try {
     await ma.activateFeed(id);
   } catch (error) {
     activation_error = error;
   }
-
-  // Assert that the activation attempt failed as expected
   assert(activation_error);
 
   ma.close();
   await indexeddb.remove(ma.conn.name);
 }
 
-async function count_unread_entries_test() {
-  const ma =
-      await openModelAccess(/* channeled */ false, 'count-unread-entries-test');
-  ma.channel = {name: 'stub', postMessage: noop, close: noop};
-
-  const empty_count = await ma.countUnreadEntries();
-  assert(empty_count === 0);
-
-  // Create a few unread entries, a few read entries, then ensure the
-  // count is correct
-
-  const insert_unread_count = 3;
-  const entries_to_insert = [];
-  for (let i = 0; i < insert_unread_count; i++) {
-    const entry = Model.create_entry();
-    entry.readState = Model.ENTRY_STATE_UNREAD;
-    Model.append_entry_url(entry, new URL('a://b.c' + i));
-    entries_to_insert.push(entry);
-  }
-
-  const insert_read_count = 5;
-  for (let i = 0; i < insert_read_count; i++) {
-    const entry = Model.create_entry();
-    entry.readState = Model.ENTRY_STATE_READ;
-    Model.append_entry_url(entry, new URL('d://e.f' + i));
-    entries_to_insert.push(entry);
-  }
-
-  const insert_promises = entries_to_insert.map(ma.createEntry, ma);
-  await Promise.all(insert_promises);
-
-  const unread_count = await ma.countUnreadEntries();
-  console.debug('Counted %d unread entries', unread_count);
-  assert(unread_count === insert_unread_count);
-
-  ma.channel.close();
-  ma.close();
-  await indexeddb.remove(ma.conn.name);
-}
-
 function noop() {}
 
-register_test(create_feed_url_constraint_test);
-register_test(create_feeds_test);
 register_test(activate_feed_test);
-register_test(count_unread_entries_test);
