60c90075c02a8c9ee5ebd6b4b58a45d6c19084aa
Joshua Froelich <joshuafroelich@joshuas-mbp.mi.local>
2018-07-09 12:11:56 -0400

    [tools] cleanup sd-explorer a bit
    Cleanup some of the comments and debug messages

--- tools/sd-explorer.js
+++ tools/sd-explorer.js
@@ -3,33 +3,19 @@ import {set_base_uri} from '/src/lib/html-document.js';
 import {parse_html} from '/src/lib/html.js';
 import {fetch_html} from '/src/lib/net/fetch-html.js';
 
+// TODO: i need to be able to discover problems with sanitization here. Like
+// instead of pruning, this annotates with removal-reason or something
+
 async function explore(url_string) {
   document.body.innerHTML = 'Loading ' + url_string + ' ...';
-
   const url = new URL(url_string);
   const response = await fetch_html(url, 5000);
   const response_text = await response.text();
-
-  console.debug('Content-Length:', response_text.length);
-
   document.body.innerHTML = 'Parsing ' + response_text.length + ' characters';
   const doc = parse_html(response_text);
-
-  let element_count = doc.documentElement.getElementsByTagName('*').length;
-  console.debug('Element count:', element_count);
-
   document.body.innerHTML = 'Filtering document';
-
   set_base_uri(doc, new URL(response.url));
-
-  // TODO: i need to be able to discover problems with sanitization here. Like
-  // instead of pruning, this annotates with removal-reason or something
-
   await sanitize_document(doc);
-
-  element_count = doc.documentElement.getElementsByTagName('*').length;
-  console.debug('After filter element count:', element_count);
-
   document.body.innerHTML = 'Rendering';
   document.body.innerHTML = doc.body.innerHTML;
 }
