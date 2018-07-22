004aa498701fd4418f9c33c7e98023e1a9f2fe3b
Joshua Froelich <joshua.froelich@gmail.com>
2018-07-22 09:29:35 -0400

    [string] alternate pattern for multiple whitespace
    I think \s\s+ might be more idiomatic than \s{2,}. Maybe it is pedantic

--- src/string.js
+++ src/string.js
@@ -1,11 +1,10 @@
 // Returns a new string where sequences of two or more whitespace characters are
-// replaced with a single space character (ascii 32).
-//
-// Throws if value is undefined or does not support the replace method.
-// Assumes value is a DOMString, meaning that when the regex is applied, the
-// \s pattern will match html entities like &nbsp;.
+// replaced with a single space character (ascii 32). Throws if value is
+// undefined or does not support the replace method. Assumes value is a
+// DOMString, meaning that when the regex is applied, the \s pattern will also
+// consider html whitespace entities.
 export function condense_whitespace(value) {
-  return value.replace(/\s{2,}/g, ' ');
+  return value.replace(/\s\s+/g, ' ');
 }
 
 // Returns a new string where Unicode Cc-class characters have been removed.
