cfdb455ba7e36894c4350674c7f177cb0ad09e13
Joshua Froelich <joshua.froelich@gmail.com>
2018-07-28 10:38:35 -0400

    [fetch-html] allow text/plain, optionally

--- src/net/fetch-html.js
+++ src/net/fetch-html.js
@@ -1,8 +1,13 @@
 import {is_allowed_request} from '/src/net/fetch-policy.js';
 import {fetch2} from '/src/net/fetch2.js';
 
-export function fetch_html(url, timeout) {
+export function fetch_html(url, timeout, allow_text) {
   const html_mime_types = ['text/html'];
+
+  if (allow_text) {
+    html_mime_types.push('text/plain');
+  }
+
   const options = {timeout: timeout, types: html_mime_types};
   return fetch2(url, options, is_allowed_request);
 }
