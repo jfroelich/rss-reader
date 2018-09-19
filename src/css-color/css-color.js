e8bf0d870c834be41d869c08a93ea760999d96d2
Joshua Froelich <joshua.froelich@gmail.com>
2018-09-18 23:19:15 -0400

    [css-color] correct bad import
    In prior move of third party helper tinycolor-min I messed up the import path

--- src/css-color/css-color.js
+++ src/css-color/css-color.js
@@ -1,4 +1,4 @@
-import '/src/css-color/third-party/tinycolor-min.js';
+import '/src/css-color/tinycolor-min.js';
 import * as color from '/src/color/color.js';
 
 // Parses a css color value into a color
