d4a8a08bfb676185fdda3672c3bc6008f0028c8f
Joshua Froelich <joshua.froelich@gmail.com>
2018-10-03 22:14:57 -0400

    [update-feed] throw specific error types

--- src/db/update-feed.js
+++ src/db/update-feed.js
@@ -1,7 +1,11 @@
 import assert from '/src/assert/assert.js';
-import * as feed_utils from '/src/db/feed-utils.js';
-import * as object from '/src/db/object-utils.js';
-import * as types from '/src/db/types.js';
+import * as errors from 'errors.js';
+import * as feed_utils from 'feed-utils.js';
+import * as object from 'object-utils.js';
+import * as types from 'types.js';
+
+// TODO: throw the proper errors here
+// This is also causing activate-feed-test to fail
 
 export async function update_feed(session, feed, overwrite) {
   // If overwriting, the new feed must be valid. If partial update, the new
@@ -75,14 +79,14 @@ function find_feed_onsuccess(props, reject, event) {
 
   if (!feed) {
     const message = 'Failed to find feed to update for id ' + props.id;
-    const error = new Error(message);
+    const error = new errors.NotFoundError(message);
     reject(error);
     return;
   }
 
   if (!types.is_feed(feed)) {
     const message = 'Matched object is not of type feed for id ' + props.id;
-    const error = new Error(message);
+    const error = new errors.InvalidStateError(message);
     reject(error);
     return;
   }
@@ -96,7 +100,7 @@ function find_feed_onsuccess(props, reject, event) {
   // property in the partial use case.
   if (props.active === true && feed.active === true) {
     const message = 'Cannot activate already active feed with id ' + feed.id;
-    const error = new Error(message);
+    const error = new errors.InvalidStateError(message);
     reject(error);
     return;
   }
@@ -104,7 +108,7 @@ function find_feed_onsuccess(props, reject, event) {
   // Similarly, should not try to deactivate an inactive feed.
   if (props.active === false && feed.active === false) {
     const message = 'Cannot deactivate inactive feed with id ' + feed.id;
-    const error = new Error(message);
+    const error = new errors.InvalidStateError(message);
     reject(error);
     return;
   }
@@ -153,88 +157,3 @@ function find_feed_onsuccess(props, reject, event) {
 
   event.target.source.put(feed);
 }
-
-
-
-/*
-export async function update_feed(session, feed, transition) {
-  // TODO: I have temporarily disabled this assertion. The issue is that callers
-  // use fake feed objects in a few places and that needs to work at the moment.
-  // So until the callers use real feed objects this cannot exist.
-  // assert(types.is_feed(feed));
-
-  assert(feed_utils.is_valid_feed_id(feed.id));
-
-  // TODO: define has-url in feed-utils, use that here instead
-  // TODO: I have temporarily disabled this assertion. The issue is that callers
-  // use fake feed objects in a few places and do not always provide urls.
-  // assert(feed.urls && feed.urls.length);
-
-  assert(transition === undefined || typeof transition === 'function');
-
-  object.filter_empty_properties(feed);
-  feed.dateUpdated = new Date();
-
-  const ufeb = update_feed_executor.bind(null, session.conn, feed, transition);
-  const ufp = new Promise(ufeb);
-  await ufp;
-
-  if (session.channel) {
-    const message = {type: 'feed-updated', id: feed.id};
-    session.channel.postMessage(message);
-  }
-}
-
-function update_feed_executor(conn, feed, transition, resolve, reject) {
-  const txn = conn.transaction('feed', 'readwrite');
-  txn.onerror = event => reject(event.target.error);
-  txn.oncomplete = resolve;
-  const store = txn.objectStore('feed');
-
-  if (!transition) {
-    store.put(feed);
-    return;
-  }
-
-  // Read, transition, then overwrite
-  const find_request = store.get(feed.id);
-  find_request.onsuccess = event => {
-    const old_feed = event.target.result;
-    if (!old_feed) {
-      const msg = 'Failed to find feed to update for id ' + feed.id;
-      const err = new Error(msg);
-      reject(err);
-      return;
-    }
-
-    if (!types.is_feed(old_feed)) {
-      const msg = 'Matched object is not of type feed for id ' + feed.id;
-      const err = new Error(msg);
-      reject(err);
-      return;
-    }
-
-    // NOTE: the reason for the try/catch is that this exception otherwise
-    // occurs in a deferred setting and is not automatically converted into a
-    // rejection
-
-    let new_feed;
-    try {
-      new_feed = transition(old_feed);
-    } catch (error) {
-      reject(error);
-      return;
-    }
-
-    if (!types.is_feed(new_feed)) {
-      const msg =
-          'Transitioning feed did not produce a valid feed object for id ' +
-          feed.id;
-      const err = new Error(msg);
-      reject(err);
-      return;
-    }
-
-    store.put(new_feed);
-  };
-}*/
