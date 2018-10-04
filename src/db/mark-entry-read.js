779c054d8b5917b91333917710a0a3b7dd19f989
Joshua Froelich <joshua.froelich@gmail.com>
2018-10-04 11:16:56 -0400

    [mark-entry-read] internal code improvements
    Use better naming, remove some hoops

--- src/db/mark-entry-read.js
+++ src/db/mark-entry-read.js
@@ -2,52 +2,62 @@ import assert from '/src/assert/assert.js';
 import * as entry_utils from './entry-utils.js';
 import * as types from './types.js';
 
-export async function mark_entry_read(session, entry_id) {
-  assert(entry_utils.is_valid_entry_id(entry_id));
-  await mark_entry_read_internal(session.conn, entry_id);
+// TODO: revise update-entry in the style of update-feed, then change this
+// function to simply wrap a call to update-entry, and then possibly just
+// deprecate this function
+
+export async function mark_entry_read(session, id) {
+  assert(entry_utils.is_valid_entry_id(id));
+
+  const partial_executor = executor.bind(null, session.conn, id);
+  await new Promise(partial_executor);
 
   if (session.channel) {
-    const message = {type: 'entry-read', id: entry_id};
+    const message = {type: 'entry-read', id: id};
     session.channel.postMessage(message);
   }
 }
 
-function mark_entry_read_internal(conn, entry_id) {
-  return new Promise(mark_entry_read_executor.bind(null, conn, entry_id));
-}
-
-function mark_entry_read_executor(conn, entry_id, resolve, reject) {
+function executor(conn, id, resolve, reject) {
   const txn = conn.transaction('entry', 'readwrite');
   txn.onerror = event => reject(event.target.error);
   txn.oncomplete = resolve;
 
   const store = txn.objectStore('entry');
-  const request = store.get(entry_id);
+  const request = store.get(id);
+  request.onsuccess = request_onsuccess.bind(request, id);
+}
 
-  // TODO: move function definition to top level
-  request.onsuccess = _ => {
-    const entry = request.result;
+function request_onsuccess(id, event) {
+  const entry = event.target.result;
 
-    if (!types.is_entry(entry)) {
-      reject(new Error('Loaded object is not an entry ' + entry_id));
-      return;
-    }
+  // TODO: use specific error types from ./errors.js
+  // TODO: be more verbose so this is simpler to read
 
-    if (entry.archiveState === entry_utils.ENTRY_STATE_ARCHIVED) {
-      reject(new Error('Cannot mark archived entry as read ' + entry_id));
-      return;
-    }
+  if (!entry) {
+    reject(new Error('No entry found with id ' + id));
+    return;
+  }
 
-    if (entry.readState === entry_utils.ENTRY_STATE_READ) {
-      reject(new Error('Cannot mark read entry as read ' + entry_id));
-      return;
-    }
+  if (!types.is_entry(entry)) {
+    reject(new Error('Loaded object is not an entry ' + id));
+    return;
+  }
 
-    entry.readState = entry_utils.ENTRY_STATE_READ;
-    const currentDate = new Date();
-    entry.dateUpdated = currentDate;
-    entry.dateRead = currentDate;
+  if (entry.archiveState === entry_utils.ENTRY_STATE_ARCHIVED) {
+    reject(new Error('Cannot mark archived entry as read ' + id));
+    return;
+  }
 
-    request.source.put(entry);
-  };
+  if (entry.readState === entry_utils.ENTRY_STATE_READ) {
+    reject(new Error('Cannot mark read entry as read ' + id));
+    return;
+  }
+
+  entry.readState = entry_utils.ENTRY_STATE_READ;
+  const currentDate = new Date();
+  entry.dateUpdated = currentDate;
+  entry.dateRead = currentDate;
+
+  event.target.source.put(entry);
 }
