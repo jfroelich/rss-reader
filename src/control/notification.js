abad76604ea0de45b51e321b484109d6f41147ae
Joshua Froelich <joshua.froelich@gmail.com>
2018-10-06 22:23:16 -0400

    [notification] minor comment changes

--- src/control/notification.js
+++ src/control/notification.js
@@ -1,11 +1,30 @@
 import * as local_storage from '/src/base/localstorage.js';
 import * as extension_tab from '/src/control/extension-tab.js';
 
+// TODO: i should rename this file to just show-notification.js. It is a single
+// function. I do not see the benefit of being more abstract. In fact I should
+// maybe even rename it to show-desktop-notification.js or something that really
+// emphasizes what it does, and distinguishes it from something like an html
+// element that pops up in some view.
+
 // TODO: i am placing this in the control layer for now. however, i think this
 // somehow belongs in the base layer, but in the app-specific part, so that it
 // is located beneath the database/model layer, so that the model layer can also
 // consider using notification functionality. or maybe it is located in the
 // right place. need to think about it more.
+
+// TODO: this should not be directly interacting with local storage module.
+// this should be interacting only with the config module, that wraps the
+// local storage module in some way. the config module abstracts away how
+// configuration data is persisted.
+
+// TODO: separate to the issue with config, is that this relies on config.
+// if i want to move this to a lower layer, that cannot happen. it is the
+// single check at the start of the function that causes the early exit that
+// ruins the ability to make this lower level. Well, a second issue is that
+// the default icon should be coming from configuration, and that also should
+// be read from config like the show_notifications setting, instead of being
+// hardcoded here.
 
 export function show(title, message, icon_url_string) {
   if (!local_storage.read_boolean('show_notifications')) {
