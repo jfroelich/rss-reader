ad1a493f0a263580248cbe45349c97f0c8d00d61
Joshua Froelich <joshua.froelich@gmail.com>
2018-10-13 09:39:53 -0400

    [filter-empahsis] allow for 0 length param
    Do not necessarily throw an exception when the input param is undefined or 0. Just noop.

--- src/base/sandoc/filter-emphasis.js
+++ src/base/sandoc/filter-emphasis.js
@@ -1,15 +1,29 @@
 import assert from '/src/base/assert.js';
 import {unwrap_element} from '/src/base/unwrap-element.js';
 
-export function filter_emphasis(document, text_length_max) {
-  assert(Number.isInteger(text_length_max) && text_length_max > 0);
-  if (document.body) {
-    const elements =
-        document.body.querySelectorAll('b, big, em, i, strong, mark, u');
-    for (const element of elements) {
-      if (get_emphasis_length(element) > text_length_max) {
-        unwrap_element(element);
-      }
+// NOTE: whitespace is completely stripped prior to analysis. This threshold
+// parameter applies to the non-whitespace length.
+
+export function filter_emphasis(document, text_length_max = 0) {
+  assert(Number.isInteger(text_length_max) && text_length_max >= 0);
+
+  // 0 means indefinite emphasis is allowed, which means no filtering should
+  // occur at all. Technically this function should not have been called because
+  // it was pointless but this should not cause an exception.
+  if (text_length_max === 0) {
+    return;
+  }
+
+  // Analysis is restricted to elements within body.
+  if (!document.body) {
+    return;
+  }
+
+  const selector = 'b, big, em, i, strong, mark, u';
+  const elements = document.body.querySelectorAll(selector);
+  for (const element of elements) {
+    if (get_emphasis_length(element) > text_length_max) {
+      unwrap_element(element);
     }
   }
 }
