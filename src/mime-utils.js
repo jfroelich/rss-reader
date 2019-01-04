d1cf491d7b6a4da382a88f82c41839a8ed97982f
jfroelich <joshua.froelich@gmail.com>
2019-01-04 08:09:33 -0500

    [mime-utils] Move issues to github and cleanup

--- src/mime-utils.js
+++ src/mime-utils.js
@@ -3,51 +3,35 @@ import * as string from '/src/string-utils.js';
 export const MIME_TYPE_MIN_LENGTH = 7;
 export const MIME_TYPE_MAX_LENGTH = 100;
 
-// Tries to get the mime type corresponding to an HTTP Content-Type header
-// value. Returns undefined on any failure.
+// Extract the mime type from an HTTP Content-Type header value
 export function parse_content_type(value) {
-  // For convenience, allow the caller to call this without checking the header
-  // value
   if (typeof value !== 'string') {
     return;
   }
 
-  // Try and avoid the cost of trim
-  // TODO: profile whether this is worth it, may be premature optimization
   if (value.length < MIME_TYPE_MIN_LENGTH) {
     return;
   }
 
   value = value.trim();
-
-  // Try and avoid the cost of search
-  // TODO: profile whether this is worth it, may be premature optimization
   if (value.length < MIME_TYPE_MIN_LENGTH) {
     return;
   }
 
-  // Chop of the trailing encoding, if it exists
-  const scpos = value.indexOf(';');
-  let mime_type = scpos > -1 ? value.substring(0, scpos) : value;
-
-  // Try and avoid the cost of normalization
-  // TODO: profile, may be premature optimization
+  let mime_type = strip_encoding(value);
   if (mime_type.length < MIME_TYPE_MIN_LENGTH) {
     return;
   }
 
-  // Normalize the value
   mime_type = string.filter_whitespace(mime_type).toLowerCase();
-
-  // Provide minimal guarantee the output is valid
-  if (!is_mime_type(mime_type)) {
-    return;
-  }
-
-  return mime_type;
+  return is_mime_type(mime_type) ? mime_type : undefined;
 }
 
-// Returns whether a string loosely approximates a mime type
+export function strip_encoding(value) {
+  const idx = value.indexOf(';');
+  let mime_type = idx > -1 ? value.substring(0, idx) : value;
+}
+
 export function is_mime_type(value) {
   return typeof value === 'string' && value.length > MIME_TYPE_MIN_LENGTH &&
       value.length < MIME_TYPE_MAX_LENGTH && value.includes('/') &&
