334ffbd36b8833d1bbba8eaee7d6eb0b10c6706f
Josh Froelich <joshua.froelich@gmail.com>
2019-02-23 10:50:01 -0800

    [dom-utils] inline node-is-leaf helpers
    The helpers did not add a great deal of value. I believe leftover from a very old attempt at perf opt. No obvious benefit to creating idioms here. So just inline. Part of task of simplifying and/or removing dom-utils.

--- src/core/dom-utils.js
+++ src/core/dom-utils.js
@@ -1,17 +1,17 @@
 import '/third-party/parse-srcset.js';
 import {unwrap_element} from '/src/lib/unwrap-element.js';
 
-const leaf_exception_element_names = [
-  'area', 'audio',  'base', 'col',      'command', 'br',    'canvas', 'col',
-  'hr',   'iframe', 'img',  'input',    'keygen',  'meta',  'nobr',   'param',
-  'path', 'source', 'sbg',  'textarea', 'track',   'video', 'wbr'
-];
-
 // Returns whether the input node is a 'leaf'
 export function node_is_leaf(node) {
+  const exception_names = [
+    'area', 'audio',  'base', 'col',      'command', 'br',    'canvas', 'col',
+    'hr',   'iframe', 'img',  'input',    'keygen',  'meta',  'nobr',   'param',
+    'path', 'source', 'sbg',  'textarea', 'track',   'video', 'wbr'
+  ];
+
   switch (node.nodeType) {
     case Node.ELEMENT_NODE: {
-      if (is_element_leaf_exception(node)) {
+      if (exception_names.includes(node.localName)) {
         return false;
       }
 
@@ -32,10 +32,6 @@ export function node_is_leaf(node) {
   }
 
   return true;
-}
-
-function is_element_leaf_exception(element) {
-  return leaf_exception_element_names.includes(element.localName);
 }
 
 // Returns true if the image element has at least one source
