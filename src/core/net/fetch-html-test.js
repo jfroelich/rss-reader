5e3f3694fed9504bacbb43bedb2f593e4f4ad61f
Josh Froelich <joshua.froelich@gmail.com>
2019-02-24 15:59:16 -0800

    [fetch-html] isolate test
    fetch-html is not part of net module, so its tests should not be part of net tests

---
+++ src/core/net/fetch-html-test.js
@@ -0,0 +1,17 @@
+import {fetch_html} from '/src/core/net/fetch-html.js';
+import {assert} from '/src/lib/assert.js';
+
+// TODO: run on a local resource
+// TODO: cannot accept param
+// TODO: assert something, do not just log
+
+export async function fetch_html_test() {
+  // let url_string = undefined;
+  // let timeout = undefined;
+  // const request_url = new URL(url_string);
+  // const response = await fetch_html(request_url, {timeout: timeout});
+  // console.dir(response);
+  // const response_text = await response.text();
+  // console.log(response_text);
+  // return response;
+}
--- src/core/net/net-tests.js
+++ src/core/net/net-tests.js
@@ -1,4 +1,3 @@
-import {fetch_html} from '/src/core/net/fetch-html.js';
 import * as net from '/src/core/net/net.js';
 import {assert} from '/src/lib/assert.js';
 import {Deadline, INDEFINITE} from '/src/lib/deadline.js';
@@ -43,21 +42,4 @@ export async function better_fetch_test() {
 
 export async function fetch_feed_test() {
   // TODO: implement me
-}
-
-// TODO: move to separate test file
-export async function fetch_html_test() {
-  // TODO: run on a local resource
-  // TODO: cannot accept param
-  // TODO: assert something, do not just log
-
-  // OLD:
-  // let url_string = undefined;
-  // let timeout = undefined;
-  // const request_url = new URL(url_string);
-  // const response = await fetch_html(request_url, {timeout: timeout});
-  // console.dir(response);
-  // const response_text = await response.text();
-  // console.log(response_text);
-  // return response;
 }
