6a1af7df53a0cc72574487e950de6f7423ae8144
Josh Froelich <joshua.froelich@gmail.com>
2019-02-25 07:10:22 -0800

    [desknote] some cleanup
    Cleanup comments and a few minor code changes

--- src/core/desknote.js
+++ src/core/desknote.js
@@ -2,25 +2,32 @@ import * as extension from '/src/core/extension.js';
 import * as platform from '/src/lib/platform.js';
 import * as tls from '/src/lib/tls.js';
 
-// If notifications are enabled, generate and show a desktop notification.
-// |note| has optional properties name, message, and url (string). Defaults are
-// provided for missing properties.
-export function show(note) {
+// Show a desktop notification with optional title, message, and icon. |note|
+// properties include title (string), message (string), and url (string).
+//
+// This wraps the generic notification feature with app-specific behavior
+// including:
+// * Canceling the notification if notifications are disabled in app settings.
+// Callers do not need to be concerned with whether notifications are enabled.
+// * Providing default values for title, message, and icon. Callers only need
+// to supply the values they want, and this substitutes in the rest.
+// * More gracefully handling an error situation when clicking on a notification
+// while the browser window is closed.
+// * Linking the notification click handler to the app's show view function.
+export function show(note = {}) {
   if (!tls.read_boolean('show_notifications')) {
     return;
   }
 
-  const title = note.title || 'Untitled';
-  const message = note.message || '';
-
-  const details = {};
-  details.body = message || '';
-
   const default_icon =
       platform.extension.get_url_string('/images/rss_icon_trans.gif');
 
+  const title = note.title || 'Untitled';
+  const details = {};
+  details.body = note.message || '';
   details.icon = note.url || default_icon;
 
+  // Instantiating the object also shows it
   const notification = new Notification(title, details);
   notification.addEventListener('click', event => {
     try {
