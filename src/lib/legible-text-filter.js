1931acb9dae15a995d26c2aae9943ac18735ecee
Josh Froelich <joshua.froelich@gmail.com>
2019-03-03 09:15:31 -0800

    [legible-text-filter] misc improvements
    Check for whitespace. Use CSSUnitValue throughout. Improve averaging accuracy. Cleanup comments. Condense some draft code. Remove some unclear code.

--- src/lib/legible-text-filter.js
+++ src/lib/legible-text-filter.js
@@ -4,31 +4,13 @@ import {assert} from '/src/lib/assert.js';
 // getComputedStyle availability it is horribly inaccurate and could
 // unintentionally remove desired content.
 
-// The legible_text_filter removes text nodes from the input document that the
-// filter considers illegible due to having too small of a font size.
+// The filter removes text nodes from the input document that the filter
+// considers illegible due to having too small of a font size.
 //
 // This filter makes decisions based on CSS values. For better fidelity to
 // author intent, this should be run before CSS values are filtered by any other
 // filters, such as a by a filter that removes link/style elements, or a filter
 // that removes style attributes.
-//
-// The filter accepts a required |options| parameter with a required property
-// |min_font_size| that must be a CSSUnitValue, or an assertion error is thrown.
-
-// NOTE: this is a very raw first draft, probably several things wrong, I am
-// trying to proceed despite not having a clear vision for the implementation.
-// NOTE: unless otherwise specified, all units are in pixels
-
-// TODO: does style.fontSize yield pts or px?
-
-// TODO: this is another opportunity to learn about CSSOM, which may be relevant
-
-// TODO: this initial impl calls getComputedStyle twice. I am unsure if
-// computation is memoized or how browsers implement getComputedStyle, I just
-// know that it tends to have horrible performance. I think I need a different
-// approach where I call getComputedStyle only once per element. In addition, I
-// do not love how this iterates twice when it could be done in a single
-// iteration.
 
 // TODO: consider mark-sweep approach, where I break up the classification step
 // and the prune step. Similar to what I started doing in the boilerplate lib.
@@ -42,59 +24,60 @@ import {assert} from '/src/lib/assert.js';
 // sizes for whitespace only text, it would probably be better to leave that
 // text untouched.
 
-// TODO: I am unsure if I am averaging the right thing. Not sure if total text
-// node count is what I want. Maybe text length ignoring whitespace is a more
-// accurate metric? I need to clarify what I am trying to measure. Basically I
-// want to remove tiny text, unless most of the document is tiny. So I want to
-// remove relatively tiny text.
+
 
 // TODO: maybe I also want to enforce a minimum content length ratio similar to
 // what was done in the boilerplate filter, so that the filter never removes too
 // much text.
 
-export function legible_text_filter(doc, options) {
-  const min_font_size = options.min_font_size;
+// |doc| required, must be of type Document. |min_font_size| required, must be
+// of type CSSUnitValue, must use a CSS unit type such as 'px' or 'in' or 'mm'
+// or 'cm'.
+export function legible_text_filter(doc, min_font_size) {
   assert(min_font_size instanceof CSSUnitValue);
 
-  // TODO: to simplify and make the comparison of min-font-size to
-  // avg-font-size, both should be CSSUnitValue types of the same unit.
-
-  const avg_font_size = calc_avg_font_size(doc);
-  if (isNaN(avg_font_size)) {
-    console.debug('Failed to calc average font size', doc.baseURI);
+  // We are only investigating nodes within body. Without a body, there is no
+  // benefit to analysis, so exit.
+  if (!doc.body) {
     return;
   }
 
-  if (avg_font_size < 1) {
-    console.debug('Average font size is < 1', avg_font_size);
+  // Calculate the average font size using the same units as the minimum font
+  // size, for simple comparison. If indeterminate, it is unsafe to prune, so
+  // exit early.
+  const avg_font_size = calc_avg_font_size(doc, min_font_size.unit);
+  if (!avg_font_size) {
     return;
   }
 
-  if (avg_font_size < min_font_size.value) {
-    console.debug(
-        'Average font size is smaller than minimum', avg_font_size,
-        min_font_size);
+  // If the average font size is an outlier, it indicates something unexpected,
+  // so it is unsafe to prune, so give up.
+  if (avg_font_size.to('px').value < 1) {
     return;
   }
 
-  const it = doc.createNodeIterator(doc.documentElement, NodeFilter.SHOW_TEXT);
+  // If the document, on average, has a small font size, then we should not do
+  // any pruning so as to avoid blanking the document.
+  if (avg_font_size.value < min_font_size.value) {
+    return;
+  }
+
+  // Iterate over text nodes within the body and remove text nodes with a small
+  // font size.
+  const it = doc.createNodeIterator(doc.body, NodeFilter.SHOW_TEXT);
   for (let node = it.nextNode(); node; node = it.nextNode()) {
-    const parent = node.parentNode;
-    const font_size = get_element_font_size(parent);
-    if (isNaN(font_size)) {
-      console.debug('Could not determine text font size', node.nodeValue);
-      continue;
-    }
-
-    if (font_size < min_font_size.value) {
-      console.debug(
-          'Removing text node with small font', node.nodeValue, font_size);
-      parent.removeChild(node);
+    const font_size =
+        get_element_font_size(node.parentNode, min_font_size.unit);
+    if (font_size && font_size.value < min_font_size.value) {
+      console.debug('Removing node', node.nodeValue);
+      node.parentNode.removeChild(node);
     }
   }
 }
 
-// Returns an element's font size
+// Returns an element's font size as a CSSUnitValue or undefined if there is
+// any problem. Currently this only examines inline CSS. Note this assumes the
+// browser supports Element.attributeStyleMap.
 export function get_element_font_size(element, units = 'px') {
   // NOTE: in order to support processing of inert documents, we cannot use
   // getComputedStyle. Using element.style.fontSize is fickle because it means
@@ -102,7 +85,6 @@ export function get_element_font_size(element, units = 'px') {
   // now, experiment with the new typed CSS object model approach and only
   // support pixel-based font sizes.
   // https://developers.google.com/web/updates/2018/03/cssom
-
   // TODO: somehow support relative size like percent and EM and smaller
   // TODO: support inherited CSS styles
   // TODO: support other CSS properties or html attribtues that affect font
@@ -111,59 +93,45 @@ export function get_element_font_size(element, units = 'px') {
   // filters and modules that want to use this functionality.
   // TODO: consider returning a default value?
 
-  // If map size is 0 then there are no inline properties, so exit. We could
-  // skip this step, this may be a premature optimization, leaving it here for
-  // now as a reference.
-  const inline_property_count = element.attributeStyleMap.size;
-  if (inline_property_count < 1) {
-    return NaN;
+  const font_size = element.attributeStyleMap.get('font-size');
+  if (font_size) {
+    // This can throw if the conversion is not supported, this only supports
+    // physical metric conversion like in/cm/mm/px/pt. For example, the value
+    // 100%, with unit percent, will cause an error.
+    try {
+      return font_size.to(units);
+    } catch (error) {
+    }
   }
-
-  let font_size_unit_value = element.attributeStyleMap.get('font-size');
-  if (!font_size_unit_value) {
-    return NaN;
-  }
-
-  // This can throw if the conversion is not supported, this only supports
-  // physical metric conversion like in/cm/mm/px/pt
-  try {
-    font_size_unit_value = font_size_unit_value.to(units);
-  } catch (error) {
-    console.debug(error);
-    return NaN;
-  }
-  return font_size_unit_value.value;
 }
 
-// Returns the average font size of text in the document
-export function calc_avg_font_size(doc) {
-  // TODO: consider imputing font size for those nodes where font size is
-  // unknown. For example, consider a fallback to a default font size. However,
-  // the problem then is how to choose a default font size. This is tricky if
-  // the code is intended to support multiple devices where the default font
-  // size varies by device.
+// Returns the average font size of involved text in the document as a
+// CSSUnitValue, or undefined if there was a problem. Throws an error if doc is
+// not a Document or units is not a valid value for CSSUnitValue units.
+// TODO: consider imputing unknown font size
+export function calc_avg_font_size(doc, units = 'px') {
+  if (!doc.body) {
+    return;
+  }
 
-  let total_font_size = 0;
-  let text_node_count = 0;
-  const it = doc.createNodeIterator(doc.documentElement, NodeFilter.SHOW_TEXT);
+  let total_size = 0, node_count = 0;
+  const it = doc.createNodeIterator(doc.body, NodeFilter.SHOW_TEXT);
   for (let node = it.nextNode(); node; node = it.nextNode()) {
-    text_node_count++;
-
-    const parent = node.parentNode;
-    const font_size = get_element_font_size(parent);
-    if (!isNaN(font_size)) {
-      total_font_size += font_size;
+    if (node_has_content(node)) {
+      const font_size = get_element_font_size(node.parentNode, units);
+      if (font_size) {
+        total_size += font_size.value;
+        node_count++;
+      }
     }
   }
 
-  if (text_node_count < 1) {
-    return NaN;
+  if (node_count) {
+    return new CSSUnitValue(total_size / node_count, units);
   }
+}
 
-  // TEMP: initial tracing, will remove
-  console.debug(
-      '%d text nodes, total font size %d, average font size %d',
-      text_node_count, total_font_size, total_font_size / text_node_count);
-
-  return total_font_size / text_node_count;
+// TODO: consider explicit visibility check? for now rely on vis filter.
+export function node_has_content(node) {
+  return node.nodeValue.trim() ? true : false;
 }
