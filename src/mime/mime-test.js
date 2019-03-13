34ba7b071b2304af59a8f7bd971e5423cf7a3f2e
Josh Froelich <joshua.froelich@gmail.com>
2019-03-13 07:53:24 -0700

    [mime][test] trivial changes

--- src/mime/mime-test.js
+++ src/mime/mime-test.js
@@ -3,60 +3,69 @@ import * as mime from '/src/mime/mime.js';
 
 export async function mime_test() {
   const a = assert;
+  const pct = mime.parse_content_type;
+
   // constants tests
+  // (compensate for lack of static assertions in js)
   a(mime.MIN_LENGTH < mime.MAX_LENGTH);
   a(mime.MIN_LENGTH >= 0);
   a(mime.MAX_LENGTH >= 0);
 
-  // parse no input
-  a(mime.parse_content_type() === undefined);
+  // no input
+  a(!pct());
 
-  // parse bad input
-  a(mime.parse_content_type(1234) === undefined);
-  a(mime.parse_content_type(false) === undefined);
-  a(mime.parse_content_type(null) === undefined);
-  a(mime.parse_content_type([]) === undefined);
-  a(mime.parse_content_type({}) === undefined);
+  // unsupported types
+  a(!pct(1234));
+  a(!pct(false));
+  a(!pct(null));
+  a(!pct([]));
+  a(!pct({}));
 
+  // short input
+  a(!pct(''));
+  a(!pct('a'));
 
-  // parse short input
-  a(mime.parse_content_type('') === undefined);
-  a(mime.parse_content_type('a') === undefined);
-
-  // parse long input
+  // long input
   let long_string = '';
   for (let i = 0; i < 100; i++) {
     long_string += 'abc';
   }
-  a(mime.parse_content_type(long_string) === undefined);
+  a(!pct(long_string));
 
-  // parse uppercase
-  a(mime.parse_content_type('TEXT/HTML') === 'text/html');
+  // case normalization
+  a(pct('TEXT/HTML') === 'text/html');
+  a(pct('TeXt/HTmL') === 'text/html');
 
-  // parse mixed case
-  a(mime.parse_content_type('TeXt/HTmL') === 'text/html');
+  // no semicolon, no character encoding
+  a(pct('text/html') === 'text/html');
 
-  // parse no semicolon, no character encoding
-  a(mime.parse_content_type('text/html') === 'text/html');
-  // parse extra trimmable whitespace
-  a(mime.parse_content_type(' \t\ntext/html  \n\t  ') === 'text/html');
-  // parse typical input
-  a(mime.parse_content_type('text/html;charset=UTF-8') === 'text/html');
-  // parse space leading character encoding
-  a(mime.parse_content_type('text/html; charset=UTF-8') === 'text/html');
+  // TODO: if header values in http responses are supposed to be a single line,
+  // then maybe it is more correct to not support line breaks
 
-  // parse extra whitespace
-  a(mime.parse_content_type('text / html') === 'text/html');
-  a(mime.parse_content_type('   text / html   ') === 'text/html');
+  // extra trimmable whitespace
+  a(pct(' \t\ntext/html  \n\t  ') === 'text/html');
+  // typical input with character encoding without leading space
+  a(pct('text/html;charset=UTF-8') === 'text/html');
+  // typical input with space leading character encoding
+  a(pct('text/html; charset=UTF-8') === 'text/html');
 
-  // fictional mime type with correct formatting and sufficient length
-  a(mime.parse_content_type('foofoo/barbar') === 'foofoo/barbar');
+  // extra intermediate whitespace
+  a(pct('text / html') === 'text/html');
+  // extra intermediate and wrapping whitespace
+  a(pct('   text / html   ') === 'text/html');
+
+  // fictional mime type
+  a(pct('foofoo/barbar') === 'foofoo/barbar');
 
   // duplicate slash
-  a(mime.parse_content_type('text/lib/html/foo') === 'text/lib/html/foo');
+  // TODO: eventually the parse function should be revised to return undefined
+  // for now this test just documents this pathological case
+  a(pct('text/lib/html/foo') === 'text/lib/html/foo');
 
   // duplicate semicolon
-  a(mime.parse_content_type('text/html;;charset=UTF-8') === 'text/html');
+  // TODO: eventually the parse function should be revised to return undefined
+  // for now just document this case
+  a(pct('text/html;;charset=UTF-8') === 'text/html');
 
   // is_valid tests
   a(mime.is_valid('text/html'));
