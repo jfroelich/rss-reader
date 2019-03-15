9fb52331a3d14584bf3ccc2faff4d2da91598e6c
Josh Froelich <joshua.froelich@gmail.com>
2019-03-14 21:03:33 -0700

    [db][magic] move comment to github
    See #771

--- src/db/types/magic.js
+++ src/db/types/magic.js
@@ -9,13 +9,5 @@
 // its corresponding type(s). By storing a magic value, it is easy to check if
 // the magic value loaded from the database corresponds to the expected type.
 
-// TODO: I am now more comfortable with incurring deserialization cost and
-// performing explicit deserialization at the model layer. I have also
-// introduced, long after beginning to use magic, explicit entity classes like
-// Entry and Feed. Therefore, there is less of a need to use magic. Instead, I
-// can use simple instanceof type checks in the various sanity checks.
-// Therefore, I think I would like to eventually deprecate the use of magic
-// fields.
-
 export const ENTRY_MAGIC = 0xdeadbeef;
 export const FEED_MAGIC = 0xfeedfeed;
