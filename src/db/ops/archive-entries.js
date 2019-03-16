2d78d9390db29a0f7dfdf6f4ebc7c1b3848586f7
Josh Froelich <joshua.froelich@gmail.com>
2019-03-16 11:08:04 -0700

    [db][archive-entries] increase readability

--- src/db/ops/archive-entries.js
+++ src/db/ops/archive-entries.js
@@ -7,62 +7,71 @@ const TWO_DAYS_MS = 1000 * 60 * 60 * 24 * 2;
 export default function archive_entries(conn, channel, max_age = TWO_DAYS_MS) {
   return new Promise((resolve, reject) => {
     assert(conn instanceof IDBDatabase);
+    assert(max_age >= 0);
 
-    const entry_ids = [];
+    const ids = [];
+    const transaction = conn.transaction('entry', 'readwrite');
+    transaction.oncomplete =
+        transaction_oncomplete.bind(transaction, channel, ids, resolve);
+    transaction.onerror = event => reject(event.target.error);
 
-    const txn = conn.transaction('entry', 'readwrite');
-    txn.oncomplete = event => {
-      if (channel) {
-        for (const id of entry_ids) {
-          channel.postMessage({type: 'entry-archived', id: id});
-        }
-      }
-
-      resolve(entry_ids);
-    };
-    txn.onerror = event => reject(event.target.error);
-
-    const store = txn.objectStore('entry');
-    const index = store.index('archiveState-readState');
-    const key_path = [Entry.UNARCHIVED, Entry.READ];
-    const request = index.openCursor(key_path);
-    request.onsuccess = event => {
-      const cursor = event.target.result;
-      if (!cursor) {
-        return;
-      }
-
-      const entry = cursor.value;
-      if (!is_entry(entry)) {
-        console.warn('Not an entry', entry);
-        cursor.continue();
-        return;
-      }
-
-      if (!entry.dateCreated) {
-        console.warn('No date created', entry);
-        cursor.continue();
-        return;
-      }
-
-      const current_date = new Date();
-      const age = current_date - entry.dateCreated;
-
-      if (age < 0) {
-        console.warn('Future entry', entry);
-        cursor.continue();
-        return;
-      }
-
-      if (age > max_age) {
-        const ae = archive_entry(entry);
-        cursor.update(ae);
-        entry_ids.push(ae.id);
-      }
-
-      cursor.continue();
-    };
+    const request = create_archivable_entries_cursor_request(transaction);
+    request.onsuccess = request_onsuccess.bind(request, ids, max_age);
   });
+}
+
+function create_archivable_entries_cursor_request(transaction) {
+  const store = transaction.objectStore('entry');
+  const index = store.index('archiveState-readState');
+  const key_path = [Entry.UNARCHIVED, Entry.READ];
+  return index.openCursor(key_path);
+}
+
+function request_onsuccess(ids, max_age, event) {
+  const cursor = event.target.result;
+  if (!cursor) {
+    return;
+  }
+
+  const entry = cursor.value;
+  if (!is_entry(entry)) {
+    console.warn('Skipping non-entry object', entry);
+    cursor.continue();
+    return;
+  }
+
+  if (!entry.dateCreated) {
+    console.warn('Skiping entry missing date created', entry);
+    cursor.continue();
+    return;
+  }
+
+  const current_date = new Date();
+  const age = current_date - entry.dateCreated;
+
+  if (age < 0) {
+    console.warn('Skipping entry created in the future', entry);
+    cursor.continue();
+    return;
+  }
+
+  if (age > max_age) {
+    const archived_entry = archive_entry(entry);
+    cursor.update(archived_entry);
+    ids.push(archived_entry.id);
+  }
+
+  cursor.continue();
+}
+
+function transaction_oncomplete(channel, ids, callback, event) {
+  if (channel) {
+    for (const id of ids) {
+      channel.postMessage({type: 'entry-archived', id: id});
+    }
+  }
+
+  callback(ids);
 }
 
 function archive_entry(entry) {
