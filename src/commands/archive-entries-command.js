939cb9434f35b34fb935c1b61cfeff4585bd5808
Josh Froelich <joshua.froelich@gmail.com>
2019-03-16 10:52:31 -0700

    [archive-entries-command] implement
    Get the basic command drafted and working.
    
    Test: ran archive entries, observed some entries were archived.

--- src/commands/archive-entries-command.js
+++ src/commands/archive-entries-command.js
@@ -1,5 +1,14 @@
-// TODO: implement
+import archive_entries from '/src/db/ops/archive-entries.js';
+import db_open from '/src/db/ops/db-open.js';
 
-export default function archive_entries_command() {
-  throw new Error('Not yet implemented');
+export default async function archive_entries_command() {
+  console.log('Archiving entries...');
+
+  const conn = await db_open();
+  const channel = new BroadcastChannel('reader');
+  const entry_ids = await archive_entries(conn, channel);
+  conn.close();
+  channel.close();
+
+  console.debug('Archived %d entries', entry_ids.length);
 }
