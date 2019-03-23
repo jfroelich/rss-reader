e2fb1546b7d1b7f9e9b7e03bdd328deda1d59001
Josh Froelich <joshua.froelich@gmail.com>
2019-03-23 00:28:43 -0700

    [get-path-extension] logic improvements
    handle period before final slash correctly

---
+++ src/lib/get-path-extension-test.js
@@ -0,0 +1,34 @@
+import assert from '/src/lib/assert.js';
+import get_path_extension from '/src/lib/get-path-extension.js';
+
+export default function get_path_extension_test() {
+  // Exercise the normal case
+  let result = get_path_extension('/b.html');
+  assert(result === 'html', 'result: ' + result);
+
+  // invalid path (missing leading slash) should fail
+  result = get_path_extension('foo');
+  assert(!result);
+
+  // no extension should fail
+  result = get_path_extension('/foo');
+  assert(!result);
+
+  // Should fail without error when there is a trailing period
+  result = get_path_extension('/b.');
+  assert(!result);
+
+  result = get_path_extension('/.htaccess');
+  assert(result === 'htaccess');
+
+  // Should fail without error when the extension has too many characters,
+  // here we use the explicit value despite it being equal to the default for
+  // the moment, to insulate against change to the default and use express
+  // expression
+  result = get_path_extension('/b.01234567890123456789asdf', 10);
+  assert(!result);
+
+  // period in path not filename
+  result = get_path_extension('/a.b/c');
+  assert(!result, 'result: ' + result);
+}
--- src/lib/get-path-extension.js
+++ src/lib/get-path-extension.js
@@ -1,12 +1,66 @@
-export default function get_path_extension(path) {
-  if (path.length > 2) {
-    const last_dot_pos_p1 = path.lastIndexOf('.') + 1;
-    if (last_dot_pos_p1 > 0 && last_dot_pos_p1 < path.length) {
-      const ext = path.substring(last_dot_pos_p1);
-      if (ext.length < 5 && is_alphanumeric(ext)) {
-        return ext;
-      }
-    }
+// Find and return the extension in a path. |max_extension_length| is optional
+// integer that defaults to 10. Return undefined if unable to find extension or
+// found extension is too long or not alphanumeric. Undefined behavior for
+// invalid input.
+export default function get_path_extension(path, max_extension_length = 10) {
+  // all (valid) paths start with a /. the shortest valid path that has an
+  // extension is "/.a", which is 3 characters. therefore, bail if too short.
+  // better to explicitly check here then start doing character searches.
+  //
+  // this also implicitly bails on empty string case. note it is also important
+  // to bail on the empty string case to deal with lastIndexOf returning its
+  // second argument when the first is empty, which is undesired. although
+  // note that this issue is not currently encountered given the filename
+  // workaround
+  if (path.length < 3) {
+    return;
+  }
+
+  // we do not skip the leading slash as tempting as that is, we start from
+  // the entire string, the leading may be the only slash in the string
+  const slash_index = path.lastIndexOf('/');
+
+  // so, we should not have to do this test if we have a valid path, because
+  // all paths start with a leading slash, however we do not know we have a
+  // valid path, and still have to avoid unexpected behavior. ideally this
+  // represents programmer error but this might be user-input and i'd rather
+  // not have the caller worry.
+  if (slash_index < 0) {
+    return;
+  }
+
+  // I ran into unexplainable behavior with using fromIndex parameter as
+  // documented on MDB, such as '/b.html'.lastIndexOf('.', 1). To work around
+  // this, get the substring and work with the substring from here on
+  const filename = path.substring(slash_index + 1);
+
+  const dot_index = filename.lastIndexOf('.');
+
+  // bail when no dot found
+  if (dot_index < 0) {
+    return;
+  }
+
+  // before actually grabbing the string, calculate its length, the -1 is to
+  // account for the period itself.
+  const extension_length = filename.length - dot_index - 1;
+
+  // bail when final path character is a dot. do this before the substring
+  // call below to avoid it.
+  if (!extension_length) {
+    return;
+  }
+
+  // bail if extension has too many characters, do this before the substring
+  // call below to avoid it.
+  if (extension_length > max_extension_length) {
+    return;
+  }
+
+  // Get the extension, excluding its leading "." character
+  const extension = filename.substring(dot_index + 1);
+  if (is_alphanumeric(extension)) {
+    return extension;
   }
 }
 
