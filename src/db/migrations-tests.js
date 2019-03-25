78683c91653265cbb745b985e1015561968e7039
Josh Froelich <joshua.froelich@gmail.com>
2019-03-25 09:35:42 -0700

    [db][migrations][test] add 20-21 test
    Confirm that migrations, at least for to-20 then to-21, are working.
    
    Test: ran the test, it passed

--- src/db/migrations-tests.js
+++ src/db/migrations-tests.js
@@ -1,4 +1,5 @@
 import * as migrations from '/src/db/migrations.js';
+import * as types from '/src/db/types.js';
 import assert from '/src/lib/assert.js';
 import * as indexeddb_utils from '/src/lib/indexeddb-utils.js';
 import RecordingChannel from '/src/lib/recording-channel.js';
@@ -7,7 +8,7 @@ export async function migrations_tests_20() {
   let database_name = 'migrations-test-20-database';
   await indexeddb_utils.remove(database_name);
 
-  const channel = new RecordingChannel('migrations-test-20-channel');
+  const channel = new RecordingChannel();
   let version = 20;
   let timeout = undefined;
 
@@ -38,6 +39,59 @@ export async function migrations_tests_20() {
   await indexeddb_utils.remove(conn.name);
 }
 
-// TODO: test creating database (old version 0) to 21. open to 20, insert an
-// entry without magic, close, open to 21, verify that 21 added the magic prop
-// to the test entry
+// Test upgrading to 20, inserting an entry, upgrading to 21, and then verify
+// that an entry inserted in 20 was modified after upgrading to 21, and that the
+// migration to 21 dispatched a message to the channel.
+export async function migrations_tests_20_21() {
+  const database_name = 'migrations-tests-20-21';
+  await indexeddb_utils.remove(database_name);
+
+  const handler =
+      event => {
+        migrations.migrate20(event, channel);
+        migrations.migrate21(event, channel);
+      }
+
+  // Step 1: create a database in version 20
+  const channel = new RecordingChannel();
+  channel.name = 'migrations-tests-20-21';
+  let conn = await indexeddb_utils.open(database_name, 20, handler);
+
+  // Step 2: insert a test entry that is missing magic props
+  let id = await new Promise((resolve, reject) => {
+    let id = undefined;
+    const transaction = conn.transaction('entry', 'readwrite');
+    transaction.oncomplete = event => resolve(id);
+    transaction.onerror = event => reject(event.target.error);
+
+    const store = transaction.objectStore('entry');
+    const request = store.put({title: 'test entry created in version 20'});
+    request.onsuccess = event => id = request.result;
+  });
+
+  // close the connection to 20
+  conn.close();
+
+  // Step 3: connect to 21
+  conn = await indexeddb_utils.open(database_name, 21, handler);
+
+  // Step 4: verify that entry now has magic prop
+  let has_magic = await new Promise((resolve, reject) => {
+    const transaction = conn.transaction('entry');
+    const store = transaction.objectStore('entry');
+    const request = store.get(id);
+    request.onerror = event => reject(request.error);
+    request.onsuccess = event => resolve(types.is_entry(request.result));
+  });
+
+  // (explicit to also verify resolve value is boolean)
+  assert(has_magic === true);
+
+  // Step 5: verify that the 20 to 21 migration produced a message
+  assert(channel.messages.length);
+  assert(channel.messages[0].type === 'entry-updated');
+  assert(channel.messages[0].id === id);
+
+  conn.close();
+  await indexeddb_utils.remove(conn.name);
+}
