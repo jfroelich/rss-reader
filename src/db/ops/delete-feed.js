8cedaa7f1bfde3902a63e9b38f2a3c7e6fe6f557
Josh Froelich <joshua.froelich@gmail.com>
2019-03-28 21:49:41 -0700

    [db][delete-feed] stop using feed index
    Revise delete-feed so that iteration over the entries store looking for 
    entries belonging to the feed no longer makes use of the feed index on 
    the entry store. delete-feed was the sole user of the feed index. Using 
    the index did speed up the operation, and it did avoid loading full 
    entry data into memory. However, there is no need to be fast here. This 
    is a rarely used operation where there is no expectation of speed. 
    Because this is the sole client of the feed index, it should now be 
    possible in a subsequent commit to simply remove the feed index, which 
    should improve the performance of other operations that need good 
    performance.
    
    I also took this opportunity to break up the function a bit so that it 
    is more readable.
    
    Progress on #651.
    
    Test: delete-feed test passes

--- src/db/ops/delete-feed.js
+++ src/db/ops/delete-feed.js
@@ -12,36 +12,42 @@ export default function delete_feed(conn, feed_id, reason) {
 
     const txn = conn.conn.transaction(['feeds', 'entries'], 'readwrite');
     txn.onerror = event => reject(event.target.error);
-    txn.oncomplete = _ => {
-      if (conn.channel) {
-        conn.channel.postMessage(
-            {type: 'feed-deleted', id: feed_id, reason: reason});
-        for (const id of entry_ids) {
-          conn.channel.postMessage({
-            type: 'entry-deleted',
-            id: id,
-            reason: reason,
-            feed_id: feed_id
-          });
-        }
-      }
+    txn.oncomplete = transaction_oncomplete.bind(
+        txn, conn.channel, feed_id, reason, entry_ids, resolve);
 
-      resolve(entry_ids);
-    };
+    txn.objectStore('feeds').delete(feed_id);
 
-    const feed_store = txn.objectStore('feeds');
-    feed_store.delete(feed_id);
-
-    const entry_store = txn.objectStore('entries');
-    const feed_index = entry_store.index('feed');
-    // Avoid loading full entry data
-    const request = feed_index.getAllKeys(feed_id);
-    request.onsucess = function(event) {
-      const keys = event.target.result;
-      for (const id of keys) {
-        entry_ids.push(id);
-        entry_store.delete(id);
-      }
-    };
+    const entry_cursor_request = txn.objectStore('entries').openCursor();
+    entry_cursor_request.onsuccess = entry_cursor_request_onsuccess.bind(
+        entry_cursor_request, feed_id, entry_ids);
   });
+}
+
+// Walk the entries store looking for entries that match the given feed id
+// and enqueue delete requests on those entries. The ids of deleted entries are
+// appended into the entry_ids array.
+function entry_cursor_request_onsuccess(feed_id, entry_ids, event) {
+  const cursor = event.target.result;
+  if (cursor) {
+    const entry = cursor.value;
+    if (entry.feed === feed_id) {
+      entry_ids.push(entry.id);
+      cursor.delete();
+    }
+
+    cursor.continue();
+  }
+}
+
+function transaction_oncomplete(
+    channel, feed_id, reason, entry_ids, callback, event) {
+  if (channel) {
+    channel.postMessage({type: 'feed-deleted', id: feed_id, reason: reason});
+    for (const id of entry_ids) {
+      channel.postMessage(
+          {type: 'entry-deleted', id: id, reason: reason, feed_id: feed_id});
+    }
+  }
+
+  callback(entry_ids);
 }
