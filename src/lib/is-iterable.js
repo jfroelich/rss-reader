c64bb04e16e67c53e0cd95d09c3ee55f7fd21475
Josh Froelich <joshua.froelich@gmail.com>
2019-04-07 08:51:55 -0700

    [is-iterable] deprecate
    This lib was not in use

--- src/lib/is-iterable.js
+++
@@ -1,5 +0,0 @@
-// Return whether a value is iterable
-export default function is_iterable(value) {
-  // See https://stackoverflow.com/questions/18884249
-  return value !== null && Symbol.iterator in Object(value);
-}
