26ea9bb06c6f7ca48ba7660a5f4f6a1fe11fcf8a
Josh Froelich <joshua.froelich@gmail.com>
2019-04-09 07:08:30 -0700

    [filter-unprintables] require valid input
    Remove the check that tolerates a non-string input, allowing the 
    function to throw on invalid input.

--- src/db/filter-unprintables-tests.js
+++ src/db/filter-unprintables-tests.js
@@ -2,35 +2,25 @@ import assert from '/src/assert.js';
 import filter_unprintables from '/src/db/filter-unprintables.js';
 
 export function filter_unprintables_test() {
-  const f = filter_unprintables;
-
   for (let i = 0; i < 9; i++) {
-    assert(f(String.fromCharCode(i)).length === 0);
+    assert(filter_unprintables(String.fromCharCode(i)).length === 0);
   }
 
-  assert(f('\t').length === 1);  // 9
-  assert(f('\n').length === 1);  // 10
-  assert(f(String.fromCharCode(11)).length === 0);
-  assert(f('\f').length === 1);  // 12
-  assert(f('\r').length === 1);  // 13
+  assert(filter_unprintables('\t').length === 1);  // 9
+  assert(filter_unprintables('\n').length === 1);  // 10
+  assert(filter_unprintables(String.fromCharCode(11)).length === 0);
+  assert(filter_unprintables('\f').length === 1);  // 12
+  assert(filter_unprintables('\r').length === 1);  // 13
 
   const space_code = ' '.charCodeAt(0);
   for (let i = 14; i < space_code; i++) {
-    assert(f(String.fromCharCode(i)).length === 0);
+    assert(filter_unprintables(String.fromCharCode(i)).length === 0);
   }
 
-  assert(f(' ').length === 1);
-  assert(f('Hello').length === 5);
-  assert(f('World').length === 5);
-  assert(f('Hello\nWorld').length === 11);
-  assert(f('Hello\u0000World').length === 10);
-  assert(f('<tag>text</t\u0005ag>').length === 15);
-
-  assert(f('').length === 0);
-  assert(f(null) === null);
-  assert(f(void 0) === void 0);
-  assert(f(true) === true);
-  assert(f(false) === false);
-  assert(isNaN(f(NaN)));
-  assert(f(0) === 0);
+  assert(filter_unprintables(' ').length === 1);
+  assert(filter_unprintables('Hello').length === 5);
+  assert(filter_unprintables('World').length === 5);
+  assert(filter_unprintables('Hello\nWorld').length === 11);
+  assert(filter_unprintables('Hello\u0000World').length === 10);
+  assert(filter_unprintables('<tag>text</t\u0005ag>').length === 15);
 }
