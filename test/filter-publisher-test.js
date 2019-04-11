b6d21670e3b8780264c4ca3b4531b7f797862824
Josh Froelich <joshua.froelich@gmail.com>
2019-04-11 11:49:26 -0700

    [filter-publisher] improve tests
    Revise the test to use pairs
    
    #611
    
    Tests: the revised filter-publisher test passes

--- test/filter-publisher-test.js
+++ test/filter-publisher-test.js
@@ -1,53 +1,66 @@
 import assert from '/lib/assert.js';
 import filter_publisher from '/lib/filter-publisher.js';
 
-// TODO: store input-ouput pairs then simply iterate
-// TODO: store input/output in a json file, then change this test to load the
-// local json file data and run against that, instead of hardcoding?
-// TODO: test various failure cases, e.g. bad inputs
-// TODO: more tests, e.g. short title
 export function filter_publisher_test() {
-  // no delimiters found
-  assert(filter_publisher('Hello World') === 'Hello World');
-  // starts with delim
-  assert(filter_publisher(' - Hello World') === ' - Hello World');
-  // ends with delim
-  assert(filter_publisher('Hello World - ') === 'Hello World - ');
-  // non-default delim
-  assert(filter_publisher('Hello ; World') === 'Hello ; World');
-  // double delim
-  assert(
-      filter_publisher('Hello - World Hello abcd - World') ===
-      'Hello - World Hello abcd');
-  // mixed double delim
-  assert(
-      filter_publisher('Hello : World Hello abcd - World') ===
-      'Hello : World Hello abcd');
-  // input too short retains input
-  assert(
-      filter_publisher('Hello World - Big News Org') ===
-      'Hello World - Big News Org');
-  // really short
-  assert(filter_publisher('a - Big News Org') === 'a - Big News Org');
+  const pairs = [];
+
+  // normal use
+  pairs.push({
+    input: 'Hello World Hello World - Big News Org',
+    output: 'Hello World Hello World'
+  });
+
+  // title without delimiter
+  pairs.push({input: 'Hello World', output: 'Hello World'});
+  // starting with delimiter
+  pairs.push({input: ' - Hello World', output: ' - Hello World'});
+  // ending with delimiter
+  pairs.push({input: 'Hello World - ', output: 'Hello World - '});
+  // non-default delimiter
+  pairs.push({input: 'Hello ; World', output: 'Hello ; World'});
+  // double delimiter
+  pairs.push({
+    input: 'Hello - World Hello abcd - World',
+    output: 'Hello - World Hello abcd'
+  });
+  // mixed double delimiter
+  pairs.push({
+    input: 'Hello : World Hello abcd - World',
+    output: 'Hello : World Hello abcd'
+  });
+  // short title
+  pairs.push({
+    input: 'Hello World - Big News Org',
+    output: 'Hello World - Big News Org'
+  });
+  // even shorter title
+  pairs.push({input: 'a - Big News Org', output: 'a - Big News Org'});
+
   // short title long publisher
-  assert(
-      filter_publisher('a - BigNewsOrgBigNewsOrgBigNewsOrg') ===
-      'a - BigNewsOrgBigNewsOrgBigNewsOrg');
+  pairs.push({
+    input: 'a - BigNewsOrgBigNewsOrgBigNewsOrg',
+    output: 'a - BigNewsOrgBigNewsOrgBigNewsOrg'
+  });
+
   // short title long publisher multiword
-  assert(
-      filter_publisher('a - BBBBBBBig NNNNNNNews OOOOOOOrg') ===
-      'a - BBBBBBBig NNNNNNNews OOOOOOOrg');
+  pairs.push({
+    input: 'a - BBBBBBBig NNNNNNNews OOOOOOOrg',
+    output: 'a - BBBBBBBig NNNNNNNews OOOOOOOrg'
+  });
+
   // long title short publisher
-  assert(
-      filter_publisher('AAAAAAAAAAAAAABBBBBBBBBBCCCCCCCCCCCCCCC - D') ===
-      'AAAAAAAAAAAAAABBBBBBBBBBCCCCCCCCCCCCCCC - D');
+  pairs.push({
+    input: 'AAAAAAAAAAAAAABBBBBBBBBBCCCCCCCCCCCCCCC - D',
+    output: 'AAAAAAAAAAAAAABBBBBBBBBBCCCCCCCCCCCCCCC - D'
+  });
+
   // too many words after delim
-  assert(
-      filter_publisher(
-          'Hello World Hello World - Too Many Words In Tail Found') ===
-      'Hello World Hello World - Too Many Words In Tail Found');
-  // basic positive case
-  assert(
-      filter_publisher('Hello World Hello World - Big News Org') ===
-      'Hello World Hello World');
+  pairs.push({
+    input: 'Hello World Hello World - Too Many Words In Tail Found',
+    output: 'Hello World Hello World - Too Many Words In Tail Found'
+  });
+
+  for (const {input, output} of pairs) {
+    assert(filter_publisher(input) === output);
+  }
 }
